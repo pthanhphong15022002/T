385a92e2ca0c5221fb0d31b734e5e24d906dedd8
thuantv1996 <tuvanthuan1996@gmail.com>
2022-04-19 15:41:24 +0700

    up

---
+++ src/app/modules/wp/components/chat-list/chat-list.component.ts
@@ -0,0 +1,13 @@
+@import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'chat-list',
+    templateUrl: 'chat-list.component.html',
+    
+})
+
+export class ChatListComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
