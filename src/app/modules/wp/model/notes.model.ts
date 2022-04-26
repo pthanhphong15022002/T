cc82f4f2564613b4710b6350d1cbb83c82cc1bfa
LeTrongNguyen2000 <letrongnguyen29112000@gmail.com>
2022-04-26 19:05:30 +0700

    ..

---
+++ src/app/modules/wp/model/notes.model.ts
@@ -0,0 +1,19 @@
+import { StatusNote } from './enum/enum';
+
+export class Notes {
+    recID: string;
+    transID: string;
+    noteType: string;
+    memo: string;
+    pictures: object;
+    checkList: object;
+    isPin: boolean;
+    showCalendar: boolean;
+    createdOn: string;
+    createdBy: string;   
+}
+
+export class NoteGoal {
+    status: StatusNote;
+    listNote: any;
+}
