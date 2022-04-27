266b46fdd22a17f955bf4609c60b1834951d7747
Cao Binh <ctbinh@lacviet.com.vn>
2022-04-27 10:58:45 +0700

    ..

--- src/app/pages/_layout/layout.modules.ts
+++ src/app/pages/_layout/layout.modules.ts
@@ -3,10 +3,16 @@ import { RouterModule } from '@angular/router';
 import { PagesRoutingModule } from '../pages-routing.module';
 import { SharedModule } from 'src/shared';
 import { LayoutComponent } from './layout.component';
+import { NotesHomeComponent } from '@pages/home/notes-home/notes-home.component';
+import { AddNoteComponent } from '@pages/home/notes-home/add-note/add-note.component';
+import { HomeComponent } from '@pages/home/home.component';
 
 @NgModule({
   declarations: [
     LayoutComponent,
+    NotesHomeComponent,
+    AddNoteComponent,
+    HomeComponent
   ],
   imports: [
     PagesRoutingModule,
