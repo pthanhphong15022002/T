fa7a02369158e5a71e0826522272ab8284578290
Hao Nguyen <nvhaohg95@gmail.com>
2022-04-27 11:37:24 +0700

    ...

--- src/app/pages/_layout/layout.modules.ts
+++ src/app/pages/_layout/layout.modules.ts
@@ -3,10 +3,15 @@ import { RouterModule } from '@angular/router';
 import { PagesRoutingModule } from '../pages-routing.module';
 import { SharedModule } from 'src/shared';
 import { LayoutComponent } from './layout.component';
-
+import { HomeComponent } from '@pages/home/home.component';
+import { NotesHomeComponent } from '@pages/home/notes-home/notes-home.component';
+import { AddNoteComponent } from '@pages/home/notes-home/add-note/add-note.component';
 @NgModule({
   declarations: [
     LayoutComponent,
+    HomeComponent,
+    NotesHomeComponent,
+    AddNoteComponent
   ],
   imports: [
     PagesRoutingModule,
