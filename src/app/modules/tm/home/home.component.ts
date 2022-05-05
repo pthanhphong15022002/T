bcf4fa71f68c285b0ce6a10a505c4c7d43e1f3c8
Vo Thao <vthao@lacviet.com.vn>
2022-05-05 17:45:44 +0700

    create_task_form

--- src/app/modules/tm/home/home.component.ts
+++ src/app/modules/tm/home/home.component.ts
@@ -6,9 +6,11 @@ import {
   AfterViewInit,
   ChangeDetectorRef,
 } from '@angular/core';
+import { SidebarComponent } from '@syncfusion/ej2-angular-navigations';
 import { ButtonModel } from 'codx-core/lib/layout/toolbar/tool-model';
 import { ViewBaseComponent } from 'codx-core/lib/layout/views/view-base/view-base.component';
 import { ViewModel } from 'codx-core/lib/layout/views/view-model';
+import { TaskInfoComponent } from '../controls/task-info/task-info.component';
 
 @Component({
   selector: 'app-home',
@@ -24,6 +26,15 @@ export class HomeComponent implements OnInit, AfterViewInit {
   @ViewChild('listTasks') listTasks: TemplateRef<any>;
   @ViewChild('schedule') schedule: TemplateRef<any>;
   @ViewChild('itemTemplate') itemTemplate: TemplateRef<any> | null;
+  // @ViewChild("sidebar") sidebar :TaskInfoComponent ;
+  @ViewChild("task-info") taskInfo : TaskInfoComponent;
+
+  @ViewChild('sidebar') sidebar: SidebarComponent;
+  public showBackdrop: boolean = true;
+  public type: string = 'Push';
+  public width: string = '600px';
+  public closeOnDocumentClick: boolean = true;
+ 
 
   views: Array<ViewModel> = [];
   buttons: Array<ButtonModel> = [];
@@ -71,7 +82,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
     },
     {
       id: '3',
-      type: 'listdetail',
+      type: 'kanban',
       icon: 'icon-chrome_reader_mode1',
       text: 'List-details',
       active: false,
@@ -110,5 +121,15 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   click(evt: any) {
     console.log(evt);
+    // alert("bắt sự kiện click")
+    this.sidebar.show();
+  }
+
+  public onCreated(args: any) {
+    this.sidebar.element.style.visibility = '';
+    this.sidebar.position ="Right";
+  }
+  closeClick(): void {
+    this.sidebar.hide();
   }
 }
