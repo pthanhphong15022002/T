b1ca819557f763a68dbddc539cfb5c207ced2aca
Nguyễn Ngọc Phú Sỹ <phusy9a11516@gmail.com>
2022-05-05 15:02:18 +0700

    Update kanban

--- src/app/modules/tm/home/home.component.ts
+++ src/app/modules/tm/home/home.component.ts
@@ -29,81 +29,90 @@ export class HomeComponent implements OnInit, AfterViewInit {
   buttons: Array<ButtonModel> = [];
   moreFunc: Array<ButtonModel> = [];
 
-  constructor(private cf: ChangeDetectorRef) { }
+  constructor(private cf: ChangeDetectorRef) {}
 
   ngOnInit(): void {
-    this.buttons = [{
-      id: '1',
-      icon: 'icon-list-checkbox',
-      text: 'button 1',
-    },
-    {
-      id: '2',
-      icon: 'icon-list-checkbox',
-      text: 'button 2',
-    }]
+    this.buttons = [
+      {
+        id: '1',
+        icon: 'icon-list-checkbox',
+        text: 'button 1',
+      },
+      {
+        id: '2',
+        icon: 'icon-list-checkbox',
+        text: 'button 2',
+      },
+    ];
 
-    this.moreFunc = [{
-      id: '1',
-      icon: 'icon-list-checkbox',
-      text: 'more 1',
-    },
-    {
-      id: '2',
-      icon: 'icon-list-checkbox',
-      text: 'more 2',
-    }]
+    this.moreFunc = [
+      {
+        id: '1',
+        icon: 'icon-list-checkbox',
+        text: 'more 1',
+      },
+      {
+        id: '2',
+        icon: 'icon-list-checkbox',
+        text: 'more 2',
+      },
+    ];
   }
 
   ngAfterViewInit(): void {
-    this.views = [{
-      id: '1',
-      type: 'content',
-      active: true,
-      model: {
-        panelLeftRef: this.panelLeftRef
-      }
-    },
-    {
-      id: '2',
-      type: 'kanban',
-      active: false,
-    },
-    {
-      id: '3',
-      type: 'listdetail',
-      icon: 'icon-chrome_reader_mode1',
-      text: 'List-details',
-      active: false,
-      model: {
-        panelLeftRef: this.listDetails,
-        sideBarLeftRef: this.asideLeft,
-        //       itemTemplate: this.itemTemplate,
-      }
-    },
-    {
-      id: '4',
-      type: 'list',
-      icon: 'icon-format_list_bulleted',
-      text: 'List-tasks',
-      active: false,
-      model: {
-        panelLeftRef: this.listTasks,
-        sideBarLeftRef: this.asideLeft,
-        // itemTemplate: this.itemTemplate,
-      }
-    },
-    {
-      id: '5',
-      type: 'schedule',
-      text: 'schedule',
-      active: false,
-      model: {
-        panelLeftRef: this.schedule,
-        sideBarLeftRef: this.asideLeft,
-        // itemTemplate: this.itemTemplate,
-      }
-    },
+    this.views = [
+      {
+        id: '1',
+        type: 'content',
+        active: true,
+        model: {
+          panelLeftRef: this.panelLeftRef,
+        },
+      },
+      {
+        id: '2',
+        type: 'kanban',
+        active: false,
+        model: {
+          panelLeftRef: this.kanban,
+          sideBarLeftRef: this.asideLeft,
+        },
+      },
+      {
+        id: '3',
+        type: 'listdetail',
+        icon: 'icon-chrome_reader_mode1',
+        text: 'List-details',
+        active: false,
+        model: {
+          panelLeftRef: this.listDetails,
+          sideBarLeftRef: this.asideLeft,
+          //       itemTemplate: this.itemTemplate,
+        },
+      },
+      {
+        id: '4',
+        type: 'list',
+        icon: 'icon-format_list_bulleted',
+        text: 'List-tasks',
+        active: false,
+        model: {
+          panelLeftRef: this.listTasks,
+          sideBarLeftRef: this.asideLeft,
+          // itemTemplate: this.itemTemplate,
+        },
+      },
+      {
+        id: '5',
+        type: 'schedule',
+        text: 'schedule',
+        active: false,
+        model: {
+          panelLeftRef: this.schedule,
+          sideBarLeftRef: this.asideLeft,
+          // itemTemplate: this.itemTemplate,
+        },
+      },
     ];
     this.cf.detectChanges();
   }
