f4b5cee3ff0ac6f8e95e0428dfccd46f49cce283
Vo Thao <vthao@lacviet.com.vn>
2022-05-23 16:43:50 +0700

    mwp_add_task MWP

--- src/app/modules/tm/mwp/_layout/layout.modules.ts
+++ src/app/modules/tm/mwp/_layout/layout.modules.ts
@@ -1,9 +1,9 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AssignModule } from '@modules/tm/assign/assign-routing.component';
+import { ControlsModule } from '@modules/tm/controls/controls.module';
 import { SharedModule } from 'src/shared';
-import { MwpModule } from '../mwp-routing.component';
+import { MwpModule } from '../mwp-routing.module';
 import { LayoutComponent } from './layout.component';
 
 @NgModule({
@@ -13,6 +13,7 @@ import { LayoutComponent } from './layout.component';
   imports: [
     MwpModule,
     SharedModule,
+    ControlsModule
   ],
   exports: [RouterModule],
 })
