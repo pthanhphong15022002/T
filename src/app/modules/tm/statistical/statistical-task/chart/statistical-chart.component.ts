3ea8a5bb683bf362c35b7c77933780fb0013b89d
Phúc <tdnphuck42@gmail.com>
2022-06-10 16:32:41 +0700

    statistical-chart

--- src/app/modules/tm/statistical/statistical-task/chart/statistical-chart.component.ts
+++ src/app/modules/tm/statistical/statistical-task/chart/statistical-chart.component.ts
@@ -1,7 +1,9 @@
 import { AfterViewInit, ChangeDetectorRef, Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ChartTaskRemind, TaskRemind } from '@modules/tm/models/dashboard.model';
-import { ApiHttpService, AuthStore, DataRequest } from 'codx-core';
+import { ChartTaskRemind, RemiderOnDay, TaskRemind } from '@modules/tm/models/dashboard.model';
+import { SelectweekComponent } from '@shared/components/selectweek/selectweek.component';
+import { AccPoints, AccumulationChart, AccumulationChartComponent, AnimationModel, IAccAnimationCompleteEventArgs, ILoadedEventArgs } from '@syncfusion/ej2-angular-charts';
+import { ApiHttpService, AuthStore, DataRequest, UserModel } from 'codx-core';
 import { ViewModel } from 'codx-core/lib/layout/views/view-model';
 import { Subject, takeUntil } from 'rxjs';
 
@@ -12,61 +14,239 @@ import { Subject, takeUntil } from 'rxjs';
 })
 export class StatisticalChartComponent implements OnInit, AfterViewInit {
   @Input() data = [];
+  ngUnsubscribe = new Subject<void>();
+  user: UserModel;
+  model: DataRequest;
+  fromDate: Date;
+  toDate: Date;
+  daySelected: Date;
+  daySelectedFrom: Date;
+  daySelectedTo: Date;
+  week: number;
+  monthSelected: number;
+  rateDoneOnTime: number;
+  rate: number;
+  rateTotalChange: string;
+  positive: boolean;
+  rateTotalChangeValue: number = 0;
+  remiderOnDay: RemiderOnDay[] = [];
+  taskCount: number;
+  //#region chartline
+  dataLineTrend: Object[] = [];
+  lineXAxis: Object = {
+    valueType: 'Category',
+    labelFormat: 'y',
+    rangePadding: 'None',
+    majorGridLines: { width: 0 },
+    majorTickLines: { width: 0 },
+    lineStyle: { width: 0 },
+    labelStyle: {
+      color: 'transparent',
+    },
+  };
 
+  lineYAxis: Object = {
+    lineStyle: { width: 0 },
+    majorTickLines: { width: 0 },
+    majorGridLines: { width: 0 },
+    labelStyle: {
+      color: 'transparent',
+    },
+  };
+
+  markerLine: Object = {
+    visible: false,
+    height: 5,
+    width: 5,
+  };
+
+  tooltip: Object = {
+    enable: false,
+  };
+
+  legendLine: Object = {
+    visible: false,
+  };
+  //#endregion chartline
+
+  //#region proccess bar
+  animation: AnimationModel = { enable: true, duration: 2000, delay: 0 };
+  valPcb: number = 100;
+  //#endregion proccess bar
+
+  //#region chartcolumn
+  dataColumn: Object[] = [];
+  dataLine: Object[] = [];
+  columnXAxis: Object = {
+    interval: 1,
+    valueType: 'Category',
+    rangePadding: 'None',
+    majorGridLines: { width: 0 },
+    majorTickLines: { width: 0 },
+    lineStyle: { width: 0 },
+    labelStyle: {
+      color: 'dark',
+    },
+  };
+
+  columnYAxis: Object = {
+    minimum: 0,
+    interval: 10,
+    labelStyle: {
+      color: 'gray',
+    },
+  };
+
+  chartArea: Object = {
+    border: {
+      width: 0,
+    },
+  };
+
+  radius: Object = {
+    topLeft: 10,
+    topRight: 10,
+  };
+
+  legendSettings: Object = { visible: true, position: 'Top', alignment: 'Far' };
+  //#endregion chartcolumn
+
+  //#region donut
+  pie: AccumulationChartComponent | AccumulationChart;
+  execute = false;
+  count = 0;
+  startAngle: number = 0;
+  endAngle: number = 360;
+  doughnutData = [{ label: '', value: 100 }];
+  palettes: string[] = ['#005DC7', '#06DDB8'];
+
+  //Initializing Datalabel
+  dataLabel: Object = {
+    visible: true,
+    position: 'Inside',
+    name: '${point.y}',
+    font: {
+      color: 'white',
+      fontWeight: 'Bold',
+      size: '14px',
+    },
+  };
+
+  onAnimationComplete(args: IAccAnimationCompleteEventArgs): void {
+    let centerTitle: HTMLDivElement = document.getElementById(
+      'center_title'
+    ) as HTMLDivElement;
+    centerTitle.style.fontSize = this.getFontSize(
+      args.accumulation.initialClipRect.width
+    );
+    let rect: ClientRect = centerTitle.getBoundingClientRect();
+    centerTitle.style.top =
+      args.accumulation.origin.y +
+      args.accumulation.element.offsetTop -
+      rect.height / 2 +
+      'px';
+    centerTitle.style.left =
+      args.accumulation.origin.x +
+      args.accumulation.element.offsetLeft -
+      rect.width / 2 +
+      'px';
+    centerTitle.style.visibility = 'visible';
+    let points: AccPoints[] = args.accumulation.visibleSeries[0].points;
+    for (let point of points) {
+      if (point.labelPosition === 'Outside' && point.labelVisible) {
+        let label: Element = document.getElementById(
+          'doughnut-container_datalabel_Series_0_text_' + point.index
+        );
+        label.setAttribute('fill', 'black');
+      }
+    }
+  }
+
+  getFontSize(width: number): string {
+    if (width > 300) {
+      return '13px';
+    } else if (width > 250) {
+      return '8px';
+    } else {
+      return '6px';
+    }
+  }
+  loaded(args: ILoadedEventArgs): void {
+    args.chart.refresh();
+  }
+  //#endregion doughnut
+
+  @ViewChild('selectweek') selectweekComponent: SelectweekComponent;
   chartTaskRemind: ChartTaskRemind = new ChartTaskRemind();
   @ViewChild('dashboard') dashboard: TemplateRef<any>;
 
   taskRemind: TaskRemind = new TaskRemind();
   views: Array<ViewModel> = [];
 
-  model= new DataRequest();
-  user: any;
-  doughnutData = [];
   doughnutEmpty = [
     { label: '', value: 100 },
   ];
-  public ngUnsubscribe = new Subject<void>();
+
 
   constructor(private api: ApiHttpService, private changeDetectorRef: ChangeDetectorRef, private authStore: AuthStore, private route: ActivatedRoute) { }
   ngAfterViewInit(): void {
-    this.views = [{
-      id: '1',
-      type: 'content',
-      active: true,
-      model: {
-        panelLeftRef: this.dashboard
-      }
-    }];
-    this.getGenaralData();
-
+    // this.week = this.selectweekComponent.week;
+    // this.fromDate = this.selectweekComponent.fromDate;
+    // this.toDate = this.selectweekComponent.toDate;
+    // this.daySelected = this.selectweekComponent.daySelected;
+    // this.daySelectedFrom = this.selectweekComponent.daySelectedFrom;
+    // this.daySelectedTo = this.selectweekComponent.daySelectedTo;
+    // this.monthSelected = this.selectweekComponent.month + 1;
+    this.getInitData();
   }
 
   ngOnInit(): void {
-    console.log(this.route.snapshot.params["funcID"]);
     this.user = this.authStore.get();
     this.model = new DataRequest();
-    this.model.formName = "Tasks";
-    this.model.gridViewName = "grvTasks";
-    this.model.entityName = "TM_Tasks";
+    this.model.formName = 'Tasks';
+    this.model.gridViewName = 'grvTasks';
+    this.model.entityName = 'TM_Tasks';
     this.model.pageLoading = false;
-    this.doughnutData = this.doughnutEmpty;
   }
 
-  getGenaralData() {
-    this.api
-      .exec("TM", "ReportBusiness", "GetGenaralDataAsync", [
-        this.model,
-      ])
+  private getInitData() {
+    this.api.exec("TM","ReportBusiness","GetGeneralDataAsync", [this.model,'4/20/2022','6/10/2022','4/20/2022','6/10/2022','4/20/2022','6/10/2022'])
       .pipe(takeUntil(this.ngUnsubscribe))
-      .subscribe((data: TaskRemind) => {
-        this.taskRemind = data;
-        //this.setDataChart(data.chartData);
-
+      .subscribe((task: TaskRemind) => {
+        this.taskRemind = task;
+        this.remiderOnDay = task.listTaskByDay['result'];
+        this.setDataRateDoneOnTime(task.rateDoneOnTime);
+        this.setDataProgressBar(task.rateDoneAllTime);
+        this.setDataDonutChart(task.donutChart);
+        this.setDataTrendChart(task.trendChart);
       });
   }
 
-  setDataChart(data: any) {
-    this.chartTaskRemind = data.chartTaskRemind;
+  private setDataTrendChart(data) {
+    this.rateDoneOnTime = data.rateDoneOnTime;
+    this.dataLineTrend = data.result;
+  }
 
+  private setDataProgressBar(data) {
+    this.taskRemind.rateDoneAllTime = data;
+  }
+  private setDataRateDoneOnTime(data) {
+    this.taskRemind.rateDoneOnTime = data;
+  }
+  private setDataDonutChart(data) {
+    if (data.chartPerformance && data.chartPerformance.doughnutData == 0) {
+      this.setTitleRateChange(data.rateTotalChange);
+    } else {
+      this.doughnutData = data.chartPerformance;
+      this.setTitleRateChange(data.rateTotalChange);
+      this.taskRemind.totalTaskInWeek = data.count;
+      this.changeDetectorRef.detectChanges();
+    }
+  }
+
+  private setTitleRateChange(data) {
+    this.positive = data.positive;
+    this.count = data.count;
+    this.rateTotalChangeValue = data.rate;
   }
 }
