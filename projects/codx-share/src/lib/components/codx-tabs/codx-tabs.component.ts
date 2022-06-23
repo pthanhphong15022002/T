c39bc8d7efeed46cc8111b393afcc8a3a8d52f81
vvquang <vvquanglacviet.com.vn>
2022-06-23 16:19:34 +0700

    ..

--- projects/codx-share/src/lib/components/codx-tabs/codx-tabs.component.ts
+++ projects/codx-share/src/lib/components/codx-tabs/codx-tabs.component.ts
@@ -5,6 +5,7 @@ import {
   Input,
   OnInit,
 } from '@angular/core';
+import { TabModel } from './model/tabControl.model';
 
 @Component({
   selector: 'codx-tabs',
@@ -15,14 +16,27 @@ export class CodxTabsComponent implements OnInit {
   @Input() active = 1;
   @Input() entityName!: string;
   @Input() objectID!: any;
-  @Input() TabControl: any[];
+  @Input() TabControl: TabModel[] = [];
+  private all = ['attachment', 'history', 'comment', 'reference', 'approve'];
   constructor(
     injector: Injector,
     private changeDetectorRef: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
-    this.active = this.TabControl.findIndex((x) => x.isAcive == true);
+    if (this.TabControl.length == 0) {
+      this.all.forEach((res, index) => {
+        var tabModel = new TabModel();
+        tabModel.name = tabModel.textDefault = res;
+        if (index == 0) tabModel.isActive = true;
+        else tabModel.isActive = false;
+        this.TabControl.push(tabModel);
+      });
+    } else {
+      this.active = this.TabControl.findIndex(
+        (x: TabModel) => x.isActive == true
+      );
+    }
     this.changeDetectorRef.detectChanges();
   }
 }
