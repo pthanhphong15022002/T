aec21403d14887b4112464109db282f7e8129ae6
Nguyễn Ngọc Phú Sỹ <phusy9a11516@gmail.com>
2022-06-30 15:55:35 +0700

    add mydashboard

--- projects/codx-tm/src/lib/reports/teamdashboard/teamdashboard.component.ts
+++ projects/codx-tm/src/lib/reports/teamdashboard/teamdashboard.component.ts
@@ -1,13 +1,34 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { ApiHttpService, AuthStore, DataRequest } from 'codx-core';
 
 @Component({
-    selector: 'team-dashboard',
-    templateUrl: './teamdashboard.component.html',
-    styleUrls: ['./teamdashboard.component.scss']
+  selector: 'team-dashboard',
+  templateUrl: './teamdashboard.component.html',
+  styleUrls: ['./teamdashboard.component.scss'],
 })
 export class TeamDashboardComponent implements OnInit {
-    ngOnInit(): void {
-    
-    }
+  model: DataRequest;
 
+  constructor(private api: ApiHttpService, private auth: AuthStore) {}
+
+  ngOnInit(): void {
+    this.model = new DataRequest();
+    this.model.formName = 'Tasks';
+    this.model.funcID = 'TMT0102';
+    this.model.gridViewName = 'grvTasks';
+    this.model.entityName = 'TM_Tasks';
+    this.model.pageLoading = false;
+
+    this.api
+      .execSv(
+        'TM',
+        'TM',
+        'ReportBusiness',
+        'GetDataTeamDashboardAsync',
+        this.model
+      )
+      .subscribe((res) => {
+        console.log('TeamDashboard', res);
+      });
+  }
 }
