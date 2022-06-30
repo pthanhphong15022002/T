03c423341dab50929b2082c736318a9b7c68ee48
tmCuong <tmcuong@lacviet.com.vn>
2022-06-30 13:36:34 +0700

    u

--- projects/codx-dm/src/lib/codx-dm.module.ts
+++ projects/codx-dm/src/lib/codx-dm.module.ts
@@ -15,6 +15,7 @@ import { DatePickerModule } from '@syncfusion/ej2-angular-calendars';
 import { BreadcrumbComponent, TabModule } from '@syncfusion/ej2-angular-navigations';
 import { HomeComponent } from './home/home.component';
 import { CardComponent } from './views/card/card.component';
+import { AttachmentComponent } from 'projects/codx-share/src/lib/components/attachment/attachment.component';
 
 export const routes: Routes = [
   {
@@ -45,7 +46,7 @@ export const routes: Routes = [
 const T_Component: Type<any>[] = [
   LayoutComponent,
   HomeComponent,
-  CardComponent
+  CardComponent  
 ]
 @NgModule({
   imports: [
