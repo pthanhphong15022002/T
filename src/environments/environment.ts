2613256d1d564e641f4c441e463fb82a37cee451
ntbvan <ntbvan@lacviet.com.vn>
2022-07-05 12:06:08 +0700

    ...

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,17 +5,10 @@ export const environment = {
   dfPe: '',
   appVersion: 'v101lv',
   TNDATA_KEY: 'tnf649fc9a5f55',
-<<<<<<< HEAD
-  // apiUrl: 'http://localhost:8011',
-  //shopping: 'https://localhost:44317/Shopping',
-  shopping: 'https://172.16.7.34:8111/Shopping',
-  apiUrl: 'http://172.16.7.34:8011',
-=======
   apiUrl: 'http://localhost:8011',
   shopping: 'https://localhost:44317/Shopping',
   // shopping: 'https://172.16.7.34:8111/Shopping',
   // apiUrl: 'http://172.16.7.34:8011',
->>>>>>> ae68ccec00d350fdb0c50977f4b10d292467fc3e
   librOfficeUrl: 'http://172.16.7.33:9980/loleaflet/2afbc52/loleaflet.html',
   StoreNames: {
     FormLabels: 'FormLabels',
