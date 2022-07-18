4766b75f96e9cd551cb7d87d396fd913565161ab
Vo Thao <vthao@lacviet.com.vn>
2022-07-18 15:49:06 +0700

    .....

--- projects/codx-tm/src/lib/tasks/popup-add/popup-add.component.ts
+++ projects/codx-tm/src/lib/tasks/popup-add/popup-add.component.ts
@@ -68,6 +68,8 @@ export class PopupAddComponent implements OnInit, AfterViewInit {
   isAdd = false;
   crrEstimated: any;
   isHaveFile = false;
+  index :number
+  popover: any;
 
   @ViewChild('contentAddUser') contentAddUser;
   @ViewChild('contentListTask') contentListTask;
@@ -159,7 +161,12 @@ export class PopupAddComponent implements OnInit, AfterViewInit {
   }
 
   onAddToDo(evt: any) {
-    if (!this.todoAddText) return;
+    if (!this.todoAddText || this.todoAddText.trim()=='' ) {
+       this.todoAddText = '';
+      // evt.focus();
+      return;
+    }
+   
     if (this.listTodo == null) this.listTodo = [];
     var todo = new TaskGoal();
     todo.status = this.STATUS_TASK_GOAL.NotChecked;
@@ -190,6 +197,9 @@ export class PopupAddComponent implements OnInit, AfterViewInit {
   }
 
   updateTodoList(content: string) {
+    if(content == null || content.trim() == ''){
+      return
+    }  ;
     if (this.indexEditTodo >= 0) {
       this.listTodo[this.indexEditTodo].text = content;
     }
@@ -295,7 +305,6 @@ export class PopupAddComponent implements OnInit, AfterViewInit {
 
     this.checkLogicTime();
     if (!this.isCheckTime) {
-      // this.notiService.notifyCode('TM002');
       return;
     }
     if (this.task.startDate == null && this.task.endDate == null) {
@@ -431,6 +440,7 @@ export class PopupAddComponent implements OnInit, AfterViewInit {
         case 'U':
           listUserID += obj.id + ';';
           break;
+        case 'O':
         case 'D':
           listDepartmentID += obj.id + ';';
           break;
@@ -739,5 +749,13 @@ export class PopupAddComponent implements OnInit, AfterViewInit {
   }
   getfileCount(e) {
     if (e.data.length > 0) this.isHaveFile = true;else this.isHaveFile = false ;
+  }
+  showPoppoverDelete(p, i){
+    if(this.popover)
+    this.popover.close() ;
+    if(i)
+    this.index = i;
+    p.open();
+    this.popover = p ;
   }
 }
