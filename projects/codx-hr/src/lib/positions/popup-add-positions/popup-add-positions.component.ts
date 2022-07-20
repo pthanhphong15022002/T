faccc0eb23d763ed29232fa2157b5ad645d8435e
Cao Binh <ctbinh@lacviet.com.vn>
2022-07-20 17:54:04 +0700

    .

--- projects/codx-hr/src/lib/positions/popup-add-positions/popup-add-positions.component.ts
+++ projects/codx-hr/src/lib/positions/popup-add-positions/popup-add-positions.component.ts
@@ -14,9 +14,9 @@ export class PopupAddPositionsComponent implements OnInit {
   user: any;
   functionID: string;
   action = '';
-
-  // dataBind: any = {};
   position: HR_Positions = new HR_Positions();
+  data: any;
+
   constructor(
     private df: ChangeDetectorRef,
     private notiService: NotificationsService,
@@ -24,10 +24,12 @@ export class PopupAddPositionsComponent implements OnInit {
     @Optional() dialog?: DialogRef,
     @Optional() dt?: DialogData,
   ) {
-    this.action = dt.data[1];
+    this.action = dt.data;
     this.dialog = dialog;
     this.user = this.authStore.get();
     this.functionID = this.dialog.formModel.funcID;
+    this.data = dialog.dataService!.dataSelected;
+    this.position = this.data;
    }
 
   ngOnInit(): void {
@@ -40,38 +42,48 @@ export class PopupAddPositionsComponent implements OnInit {
     }
   }
 
-  valueChange(e) {
-    switch (e.field) {
-      case "orgUnitID":
-        var value = e.data?.OrgUnitID;
-        this.position[e.field] = value;
-        this.position["departmentID"] = e.data?.OrgUnitType;
-        this.df.detectChanges();
-        break;
-      case "departmentID":
-        value = e.data?.DepartmentID;
-        break;
-      case "divisionID":
-        value = e.data?.DivisionID;
-        break;
-      case "companyID":
-        value = e.data?.CompanyID;
-        break;
-      case "reportTo":
-        value = e.data?.ReportTo;
-        break;
-      case "reportTo2":
-        value = e.data?.eportTo2;
-        break;
-      case "jobID":
-        value = e.data?.JobID;
-        break;
-      case "jobGroup":
-        value = e.data?.JobGroup;
-        break;
+  // valueChange(e) {
+  //   switch (e.field) {
+  //     case "orgUnitID":
+  //       var value = e.data?.OrgUnitID;
+  //       this.position[e.field] = value;
+  //       this.position["departmentID"] = e.data?.OrgUnitType;
+  //       this.df.detectChanges();
+  //       break;
+  //     case "departmentID":
+  //       value = e.data?.DepartmentID;
+  //       break;
+  //     case "divisionID":
+  //       value = e.data?.DivisionID;
+  //       break;
+  //     case "companyID":
+  //       value = e.data?.CompanyID;
+  //       break;
+  //     case "reportTo":
+  //       value = e.data?.ReportTo;
+  //       break;
+  //     case "reportTo2":
+  //       value = e.data?.eportTo2;
+  //       break;
+  //     case "jobID":
+  //       value = e.data?.JobID;
+  //       break;
+  //     case "jobGroup":
+  //       value = e.data?.JobGroup;
+  //       break;
 
+  //   }
+  //   this.position[e.field] = value;
+  // }
+
+  dataChange(e: any, field: string) {
+    if (e) {
+      if (e?.length == undefined) {
+        this.position[field] = e?.data;
+      } else {
+        this.position[field] = e[0];
+      }
     }
-    this.position[e.field] = value;
   }
 
   beforeSave(op: any) {
