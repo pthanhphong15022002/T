d5397745ddd013947b7517d20a9bb2099270e0ef
Nguyễn Ngọc Phú Sỹ <phusy9a11516@gmail.com>
2022-07-26 18:02:19 +0700

    setting calendar

--- projects/codx-tm/src/lib/setting/calendar/popup-add-dayoffs/popup-add-dayoffs.component.ts
+++ projects/codx-tm/src/lib/setting/calendar/popup-add-dayoffs/popup-add-dayoffs.component.ts
@@ -1,70 +1,104 @@
-import { AfterViewInit, Component, OnInit, Optional } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Injector,
+  OnInit,
+  Optional,
+} from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { APICONSTANT } from '@shared/constant/api-const';
-import { ApiHttpService, DialogData, DialogRef, FormModel } from 'codx-core';
+import {
+  ApiHttpService,
+  AuthStore,
+  DialogData,
+  DialogRef,
+  FormModel,
+  UIComponent,
+} from 'codx-core';
+import { CodxTMService } from '../../../codx-tm.service';
 
 @Component({
   selector: 'popup-add-dayoffs',
   templateUrl: './popup-add-dayoffs.component.html',
   styleUrls: ['./popup-add-dayoffs.component.scss'],
 })
-export class PopupAddDayoffsComponent implements OnInit, AfterViewInit {
+export class PopupAddDayoffsComponent extends UIComponent implements OnInit {
   dialogAddDayoffs: FormGroup;
   formModel: FormModel;
   dialog!: DialogRef;
-  calendateDate: any;
-  evtCDDate: any;
-  data:any;
-  
+  isAfterRender: boolean = false;
+  user: any;
+  funcID: string;
+  data: any;
+ 
   constructor(
-    private api: ApiHttpService,
+    private injector: Injector,
+    private tmService: CodxTMService,
+    private authService: AuthStore,
     @Optional() dt?: DialogData,
     @Optional() dialog?: DialogRef
   ) {
+    super(injector);
+    this.user = this.authService.get();
+    this.funcID = this.router.snapshot.params['funcID'];
     this.dialog = dialog;
     this.data = dt?.data;
   }
 
-  ngOnInit(): void {
-    console.log(this.data)
+  onInit(): void {
+    this.cache.functionList('TMS021').subscribe((res) => {
+      this.formModel = new FormModel();
+      this.formModel = res;
+      this.initForm();
+    });
   }
 
-  ngAfterViewInit(): void {}
-
-  saveCalendarDate() {
-    const t = this;
-    this.api
-      .execSv<any>(
-        APICONSTANT.SERVICES.BS,
-        APICONSTANT.ASSEMBLY.BS,
-        APICONSTANT.BUSINESS.BS.CalendarDate,
-        'SaveCalendarDateAsync',
-        this.evtCDDate
-      )
-      .subscribe((res) => {
+  initForm() {
+    this.tmService
+      .getFormGroup(this.formModel.formName, this.formModel.gridViewName)
+      .then((res) => {
         if (res) {
-          if (res.isAdd) {
-            t.calendateDate.push(res.data);
-          } else {
-            var index = t.calendateDate.findIndex(
-              (p) => p.recID == t.evtCDDate.recID
-            );
-            t.calendateDate[index] = t.evtCDDate;
-          }
-          this.dialog.close();
+          console.log(res)
+          this.dialogAddDayoffs = res;
+          this.isAfterRender = true;
         }
       });
   }
 
-  changeTime(e, entity) {
-    if (e.field == 'calendarDate') this.evtCDDate.calendarDate = e.data;
-  }
-
-  valueChange(event) {
-    if (event?.field) {
+  valueChange(event: any, field: string = '') {
+    if (!field) field = event?.field;
+    if (field && event.component) {
       if (event?.data === Object(event?.data))
-        this.dialogAddDayoffs.patchValue({ [event['field']]: event.data.value });
+        this.dialogAddDayoffs.patchValue({
+          [event['field']]: event.data.value,
+        });
       else this.dialogAddDayoffs.patchValue({ [event['field']]: event.data });
     }
+  }
+
+  saveCalendarDate() {
+    // const t = this;
+    // this.api
+    //   .execSv<any>(
+    //     APICONSTANT.SERVICES.BS,
+    //     APICONSTANT.ASSEMBLY.BS,
+    //     APICONSTANT.BUSINESS.BS.CalendarDate,
+    //     'SaveCalendarDateAsync',
+    //     this.evtCDDate
+    //   )
+    //   .subscribe((res) => {
+    //     if (res) {
+    //       if (res.isAdd) {
+    //         t.calendateDate.push(res.data);
+    //       } else {
+    //         var index = t.calendateDate.findIndex(
+    //           (p) => p.recID == t.evtCDDate.recID
+    //         );
+    //         t.calendateDate[index] = t.evtCDDate;
+    //       }
+    //       this.dialog.close();
+    //     }
+    //   });
+    console.log(this.dialogAddDayoffs.value)
   }
 }
