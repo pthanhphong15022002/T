b97ba20984e58b9f9ea2fc9e22272ded5a690b3e
Vo Thao <vthao@lacviet.com.vn>
2022-08-03 15:36:34 +0700

    cap_nhat_tien_do

---
+++ projects/codx-tm/src/lib/tasks/popup-update-progress/popup-update-progress.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-popup-update-progress',
+  templateUrl: './popup-update-progress.component.html',
+  styleUrls: ['./popup-update-progress.component.css']
+})
+export class PopupUpdateProgressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
