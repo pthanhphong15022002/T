498c07e66a1499463d15e69a8487969ae1368417
TrongNguyen <76871869+LeTrongNguyen2000@users.noreply.github.com>
2022-08-24 14:55:33 +0700

    css

--- projects/codx-share/src/lib/components/codx-note/codx-note.component.ts
+++ projects/codx-share/src/lib/components/codx-note/codx-note.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { ApiHttpService, CacheService } from 'codx-core';
+import { CO_Contents } from './model/CO_Contents.model';
 
 @Component({
   selector: 'codx-note',
@@ -7,11 +8,9 @@ import { ApiHttpService, CacheService } from 'codx-core';
   styleUrls: ['./codx-note.component.css'],
 })
 export class CodxNoteComponent implements OnInit {
-
   message: any;
   showEmojiPicker = false;
-  lineType = 'text';
-  empty = '';
+  font: any;
   gridViewSetup: any;
   sets = [
     'native',
@@ -23,6 +22,10 @@ export class CodxNoteComponent implements OnInit {
     'messenger',
   ];
   set = 'apple';
+  lineType = 'TEXT';
+  empty = '';
+
+  co_content: CO_Contents = new CO_Contents();
 
   @Input() showMenu = true;
   @Input() showMF = true;
@@ -47,6 +50,28 @@ export class CodxNoteComponent implements OnInit {
     this.dt.detectChanges();
   }
 
+  countBOLD = 0;
+  countITALIC = 0;
+  countUNDERLINE = 0;
+  chooseFont(font) {
+    if (font) {
+      if (font == 'BOLD') {
+        this.countBOLD++;
+        document.getElementById('font').style.fontWeight = 'bolder';
+        if(this.countBOLD > 1) {
+        document.getElementById('font').style.fontWeight = '';
+        }
+      } else if (font == 'ITALIC') {
+        this.countITALIC++;
+        document.getElementById('font').style.fontStyle = 'italic';
+      } else if (font == 'UNDERLINE') {
+        this.countUNDERLINE++;
+        document.getElementById('font').style.textDecoration = 'underline';
+      }
+      this.dt.detectChanges();
+    }
+  }
+
   popupFile() {}
 
   addEmoji(event) {
@@ -61,7 +86,18 @@ export class CodxNoteComponent implements OnInit {
 
   valueChange(event, item = null, i = null) {
     if (event?.data) {
-      // this.addContent(event?.data);
+      var dt = event?.data;
+      var field = event?.field;
+      this.co_content.lineType = this.lineType;
+      this.co_content[field] = dt;
+      if (this.co_content.lineType == 'CHECK') this.addContent(this.co_content);
+    }
+  }
+
+  valueChangeStatus(event) {
+    if (event?.data != null) {
+      this.co_content[event?.field] = event?.data;
+      if (this.co_content.memo != null) this.updateContent(this.co_content);
     }
   }
 
@@ -72,17 +108,21 @@ export class CodxNoteComponent implements OnInit {
   }
 
   addContent(data) {
+    data;
     debugger;
-    this.api
-      .execSv('CO', 'ERM.Business.CO', 'ContentsBusiness', 'SaveAsync', data)
-      .subscribe((res) => {
-        if (res) {
+    // this.api
+    //   .execSv('CO', 'ERM.Business.CO', 'ContentsBusiness', 'SaveAsync', data)
+    //   .subscribe((res) => {
+    //     if (res) {
 
-        }
-      });
+    //     }
+    //   });
   }
 
-  delete() {
-    
+  updateContent(data) {
+    data;
+    debugger;
   }
+
+  delete() {}
 }
