9be4059653fbb6c09ac86baff80c1b22d22e5c35
TrongNguyen <76871869+LeTrongNguyen2000@users.noreply.github.com>
2022-08-31 10:33:25 +0700

    css

--- projects/codx-tm/src/lib/tmmeetings/template/template.component.ts
+++ projects/codx-tm/src/lib/tmmeetings/template/template.component.ts
@@ -1,99 +1,117 @@
 import { ActivatedRoute } from '@angular/router';
 import { DataRequest } from './../../../../../../src/shared/models/data.request';
-import { CO_MeetingTemplates, CO_Content } from './../../models/CO_MeetingTemplates.model';
+import {
+  CO_MeetingTemplates,
+  CO_Content,
+} from './../../models/CO_MeetingTemplates.model';
 import { Component, Injector, Input, OnInit, Optional } from '@angular/core';
-import { DialogData, DialogRef, ApiHttpService, UIComponent, AlertConfirmInputConfig, NotificationsService } from 'codx-core';
+import {
+  DialogData,
+  DialogRef,
+  ApiHttpService,
+  UIComponent,
+  AlertConfirmInputConfig,
+  NotificationsService,
+} from 'codx-core';
 
 @Component({
   selector: 'lib-template',
   templateUrl: './template.component.html',
-  styleUrls: ['./template.component.css']
+  styleUrls: ['./template.component.css'],
 })
 export class TemplateComponent extends UIComponent implements OnInit {
-
-  @Input() template : CO_MeetingTemplates[];
-  options =new DataRequest();
+  @Input() template: CO_MeetingTemplates[];
+  options = new DataRequest();
   dialog: any;
   data: any;
-  title= 'Chọn template';
+  title = 'Chọn template';
   funcID: any;
   show: boolean = false;
   displayMembers: any;
   viewMember: any;
   valueMember: any;
   tempID: any;
-  content:  CO_Content[] = [];
+  content: CO_Content[] = [];
   constructor(
     private injector: Injector,
     private activedRouter: ActivatedRoute,
     private notificationsService: NotificationsService,
 
     @Optional() dt?: DialogData,
-    @Optional() dialog?: DialogRef,
+    @Optional() dialog?: DialogRef
   ) {
     super(injector);
 
     this.dialog = dialog;
     this.data = dt?.data;
     this.funcID = this.activedRouter.snapshot.params['funcID'];
-
   }
 
   onInit(): void {
     this.loadData();
   }
 
-
-  loadData(){
+  loadData() {
     let data = new DataRequest();
     data.page = 1;
     data.pageSize = 10;
     data.comboboxName = 'TM_MeetingTemplates';
-    this.api.execSv<any>('CO',
-    'ERM.Business.CM',
-    'DataBusiness',
-    'LoadDataCbxAsync', data).subscribe(res=>{
-      if(res){
-        console.log(res);
-        var item = JSON.parse(res[0]);
-        var result = [];
-        item.forEach(element => {
-          var obj =
-          {
-            recID : element['RecID'],
-            templateName : element['TemplateName'],
-            templateID : element['TemplateID'],
-            descriptions : element['Descriptions'],
-            content: element['Content']
-          }
-          result.push(obj);
-        });
-        this.template = result;
-      }
-    })
+    this.api
+      .execSv<any>(
+        'CO',
+        'ERM.Business.CM',
+        'DataBusiness',
+        'LoadDataCbxAsync',
+        data
+      )
+      .subscribe((res) => {
+        if (res) {
+          console.log(res);
+          var item = JSON.parse(res[0]);
+          var result = [];
+          item.forEach((element) => {
+            var obj = {
+              recID: element['RecID'],
+              templateName: element['TemplateName'],
+              templateID: element['TemplateID'],
+              descriptions: element['Descriptions'],
+              content: element['Content'],
+            };
+            result.push(obj);
+          });
+          this.template = result;
+        }
+      });
   }
 
-  search(e){
+  search(e) {}
 
-  }
-
-  toggleDocuments(item){
+  toggleDocuments(item) {
     console.log(item);
     this.tempID = item.templateID;
   }
 
-  onSave(){
-    if(this.data == null){
+  onSave() {
+    if (this.data == null) {
       this.dialog.close(this.tempID);
-    }else{
+    } else {
       var config = new AlertConfirmInputConfig();
-      config.type = "YesNo";
-      this.notificationsService.alert('Thông báo','Đã có dữ liệu cũ bạn có chắc chắn muốn chỉnh sửa?',config).closed
-      .subscribe(x=>{
-        if(x.event.status == 'Y'){
-          this.dialog.close(this.tempID);
-        }
-      })
+      config.type = 'YesNo';
+      this.notificationsService
+        .alert(
+          'Thông báo',
+          'Đã có dữ liệu cũ bạn có chắc chắn muốn chỉnh sửa?',
+          config
+        )
+        .closed.subscribe((x) => {
+          if (x.event.status == 'Y') {
+            this.dialog.close(this.tempID);
+          }
+        });
     }
+  }
+
+  getContent(event) {
+    console.log('check getContent', event);
   }
 }
