cf99fae3bd4efffce90fd37e247710690bc8e0c1
Vo Thao <vthao@lacviet.com.vn>
2022-09-09 14:28:13 +0700

    ....

--- projects/codx-tm/src/lib/tmmeetings/meeting-detail/meeting-detail.component.ts
+++ projects/codx-tm/src/lib/tmmeetings/meeting-detail/meeting-detail.component.ts
@@ -239,9 +239,9 @@ export class MeetingDetailComponent extends UIComponent {
        // if(data.recID !='')
         this.listRecID.push(data.recID);
       });
-      var listRecID =   this.listRecID.length > 0 ? this.listRecID.join(";") : '';
-      this.dataObj = { listRecID: listRecID };
     }
+    var listRecID =   this.listRecID.length > 0 ? this.listRecID.join(";") : 'DON';
+    this.dataObj = { listRecID: listRecID };
   }
   //#region end
 }
