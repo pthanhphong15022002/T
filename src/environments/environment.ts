49d3c90207135bddff1c33576f457306d84dd660
ntbvan <ntbvan@lacviet.com.vn>
2022-09-09 18:07:36 +0700

    cdd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,8 @@ export const environment = {
   dfPe: '',
   appVersion: 'v101lv',
   TNDATA_KEY: 'tnf649fc9a5f55',
-<<<<<<< HEAD
   //apiUrl: 'http://localhost:8011',
   ///apiUrl: 'http://172.16.7.34:8011',
-=======
-  apiUrl: 'http://localhost:8011',
-  //  apiUrl: 'http://172.16.7.34:8011',
->>>>>>> f4e7d4e56564827bad5185c53562ba2eef0a4ffa
   //shopping: 'https://localhost:44317/Shopping',
   shopping: 'https://172.16.7.34:8111/Shopping',
   apiUrl: 'http://172.16.7.34:8011',
