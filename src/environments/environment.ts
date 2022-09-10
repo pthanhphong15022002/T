618e92cb0bbf99b662abb39e85526efb6743aeca
TrongNguyen <76871869+LeTrongNguyen2000@users.noreply.github.com>
2022-09-10 10:36:46 +0700

    conflix enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,8 @@ export const environment = {
   appVersion: 'v101lv',
   TNDATA_KEY: 'tnf649fc9a5f55',
   apiUrl: 'http://localhost:8011',
-<<<<<<< HEAD
-  //apiUrl: 'http://172.16.7.34:8011',
-=======
-  //  apiUrl: 'http://172.16.7.34:8011',
->>>>>>> f4e7d4e56564827bad5185c53562ba2eef0a4ffa
   //shopping: 'https://localhost:44317/Shopping',
-   shopping: 'https://172.16.7.34:8111/Shopping',
+  shopping: 'https://172.16.7.34:8111/Shopping',
   // apiUrl: 'http://172.16.7.34:8011',
   pdfUrl: 'http://localhost:8015/api/pdf',
   librOfficeUrl: 'http://172.16.7.33:9980/loleaflet/2afbc52/loleaflet.html',
