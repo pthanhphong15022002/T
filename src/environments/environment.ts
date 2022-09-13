c2cf3002986a8c2e12dbac9b03f70edf5f85ae3d
vvquang <vvquanglacviet.com.vn>
2022-09-13 16:31:05 +0700

    ..

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,8 @@ export const environment = {
   dfPe: '',
   appVersion: 'v101lv',
   TNDATA_KEY: 'tnf649fc9a5f55',
-<<<<<<< HEAD
-  apiUrl: 'http://localhost:8011',
-=======
-  apiUrl: 'http://172.16.7.34:8011',
->>>>>>> fcaf52910bab8101c7b38da2a5084842784e70b0
-  //apiUrl: 'http://172.16.12.86:8011',
+  //apiUrl: 'http://localhost:8011',
+  apiUrl: 'http://172.16.12.86:8011',
   //shopping: 'https://localhost:44317/Shopping',
   shopping: 'https://172.16.7.34:8111/Shopping',
   //apiUrl: 'http://172.16.7.34:8011',
