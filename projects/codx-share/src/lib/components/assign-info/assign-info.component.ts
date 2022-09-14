05417c5540468b936cdaa53d3814a2fabd64da09
Vo Thao <vthao@lacviet.com.vn>
2022-09-14 09:51:28 +0700

    ..

--- projects/codx-share/src/lib/components/assign-info/assign-info.component.ts
+++ projects/codx-share/src/lib/components/assign-info/assign-info.component.ts
@@ -343,7 +343,7 @@ export class AssignInfoComponent implements OnInit, AfterViewInit {
           //lưu his giao việc
           var objectType = this.formModel.entityName 
           var objectID = this.task.refID
-          var objectName = this.formModel.entityName 
+          var objectName = "TM_Tasks" 
           var dataObj = { objectType: objectType, objectID: objectID, objectName: objectName }
           
            var tmpHistorry = {
