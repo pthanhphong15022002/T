1639e5a19177908bae9f4e47c731b90a45f420ce
vvquang <vvquanglacviet.com.vn>
2022-09-14 16:17:08 +0700

    fix scroll Lộc

--- projects/codx-ep/src/lib/settings/cars/popup-add-cars/popup-add-cars.component.ts
+++ projects/codx-ep/src/lib/settings/cars/popup-add-cars/popup-add-cars.component.ts
@@ -1,6 +1,13 @@
 import {
   AfterViewInit,
-  ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Optional, Output, ViewChild,
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Optional,
+  Output,
+  ViewChild,
 } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
@@ -18,7 +25,6 @@ import {
 import { AttachmentComponent } from 'projects/codx-share/src/lib/components/attachment/attachment.component';
 import { Device } from '../../../booking/car/popup-add-booking-car/popup-add-booking-car.component';
 
-
 import { CodxEpService } from '../../../codx-ep.service';
 import { Equipments } from '../../../models/equipments.model';
 
@@ -28,31 +34,31 @@ import { Equipments } from '../../../models/equipments.model';
   styleUrls: ['popup-add-cars.component.scss'],
 })
 export class PopupAddCarsComponent implements OnInit, AfterViewInit {
-  @ViewChild('attachment') attachment : AttachmentComponent; 
+  @ViewChild('attachment') attachment: AttachmentComponent;
   @ViewChild('imageUpLoad') imageUpload: ImageViewerComponent;
   @Input() editResources: any;
   @Input() isAdd = true;
   @Input() data!: any;
   @Output() closeEdit = new EventEmitter();
-  @Output() onDone = new EventEmitter();  
+  @Output() onDone = new EventEmitter();
   @Output() loadData = new EventEmitter();
-  
-  headerText='';
+
+  headerText = '';
   subHeaderText = '';
 
   fGroupAddCar: FormGroup;
   formModel: FormModel;
   dialogRef: DialogRef;
-  lstEquipment=[];
+  lstEquipment = [];
   CbxName: any;
-  isAfterRender = false;  
-  gviewCar:any;
+  isAfterRender = false;
+  gviewCar: any;
   vllDevices = [];
   lstDeviceCar = [];
   tmplstDevice = [];
-  avatarID:any=null;
+  avatarID: any = null;
   notificationsService: any;
-  constructor(    
+  constructor(
     private authService: AuthService,
     private callFuncService: CallFuncService,
     private cacheService: CacheService,
@@ -64,33 +70,31 @@ export class PopupAddCarsComponent implements OnInit, AfterViewInit {
     this.data = dialogRef?.dataService?.dataSelected;
     this.isAdd = dialogData?.data[1];
     this.dialogRef = dialogRef;
-    this.formModel = this.dialogRef.formModel;    
+    this.formModel = this.dialogRef.formModel;
   }
 
-  ngAfterViewInit(): void {
-  }
+  ngAfterViewInit(): void {}
 
-  ngOnInit(): void {    
+  ngOnInit(): void {
     this.initForm();
-    debugger;
-    this.cacheService.valueList('EP012').subscribe((res) => {      
-      this.vllDevices = res.datas;      
+
+    this.cacheService.valueList('EP012').subscribe((res) => {
+      this.vllDevices = res.datas;
       this.vllDevices.forEach((item) => {
         let device = new Device();
         device.id = item.value;
         device.text = item.text;
-        if(!this.isAdd)
-        {          
-          this.data.equipments.forEach((item)=>{
-            if(item.equipmentID == device.id){
+        if (!this.isAdd) {
+          this.data.equipments.forEach((item) => {
+            if (item.equipmentID == device.id) {
               device.isSelected = true;
             }
-          }); 
+          });
         }
-        this.lstDeviceCar.push(device);          
+        this.lstDeviceCar.push(device);
         this.tmplstDevice = JSON.parse(JSON.stringify(this.lstDeviceCar));
-      });       
-    }); 
+      });
+    });
 
     this.codxEpService
       .getComboboxName(
@@ -104,13 +108,12 @@ export class PopupAddCarsComponent implements OnInit, AfterViewInit {
   }
 
   initForm() {
-    if(this.isAdd){      
-      this.headerText = "Thêm mới xe"
+    if (this.isAdd) {
+      this.headerText = 'Thêm mới xe';
       //this.fGroupAddCar.value.resourceName=null;
-    }
-    else{
-      this.headerText = "Sửa thông tin xe"
-      this.avatarID= this.data.recID;
+    } else {
+      this.headerText = 'Sửa thông tin xe';
+      this.avatarID = this.data.recID;
     }
     this.codxEpService
       .getFormGroup(this.formModel.formName, this.formModel.gridViewName)
@@ -118,19 +121,19 @@ export class PopupAddCarsComponent implements OnInit, AfterViewInit {
         this.fGroupAddCar = item;
         if (this.data) {
           this.fGroupAddCar.patchValue(this.data);
-        }    
+        }
         this.fGroupAddCar.patchValue({
-          resourceType : '2',
-          linkType : '3',
-        });    
+          resourceType: '2',
+          linkType: '3',
+        });
         this.isAfterRender = true;
       });
     this.cacheService
-        .gridViewSetup(this.formModel.formName, this.formModel.gridViewName)
-        .subscribe((res) => {
-          this.gviewCar = res;
-          console.log('grvEPT', this.gviewCar);
-        });
+      .gridViewSetup(this.formModel.formName, this.formModel.gridViewName)
+      .subscribe((res) => {
+        this.gviewCar = res;
+        console.log('grvEPT', this.gviewCar);
+      });
   }
   checkedChange(event: any, device: any) {
     let index = this.tmplstDevice.indexOf(device);
@@ -163,7 +166,7 @@ export class PopupAddCarsComponent implements OnInit, AfterViewInit {
       }
     }
   }
-  
+
   beforeSave(option: RequestOption) {
     let itemData = this.fGroupAddCar.value;
     option.methodName = 'AddEditItemAsync';
@@ -183,33 +186,37 @@ export class PopupAddCarsComponent implements OnInit, AfterViewInit {
       this.codxEpService.notifyInvalid(this.fGroupAddCar, this.formModel);
       return;
     }
-   
+
     let equipments = '';
     this.tmplstDevice.forEach((element) => {
-      if (element.isSelected) {  
+      if (element.isSelected) {
         let tempEquip = new Equipments();
-        tempEquip.equipmentID=element.id;
-        tempEquip.createBy=this.authService.userValue.userID;
+        tempEquip.equipmentID = element.id;
+        tempEquip.createBy = this.authService.userValue.userID;
         this.lstEquipment.push(tempEquip);
-        
       }
     });
-    if(this.fGroupAddCar.value.category!=1){
-      this.fGroupAddCar.value.companyID=null;
-    }else{      
-      if(this.fGroupAddCar.value.companyID instanceof Object){
-        this.fGroupAddCar.patchValue({companyID:this.fGroupAddCar.value.companyID[0]})
+    if (this.fGroupAddCar.value.category != 1) {
+      this.fGroupAddCar.value.companyID = null;
+    } else {
+      if (this.fGroupAddCar.value.companyID instanceof Object) {
+        this.fGroupAddCar.patchValue({
+          companyID: this.fGroupAddCar.value.companyID[0],
+        });
       }
     }
-    if(this.fGroupAddCar.value.owner instanceof Object){
-      this.fGroupAddCar.patchValue({owner:this.fGroupAddCar.value.owner[0]})
-    }if(this.fGroupAddCar.value.linkID instanceof Object){
-      this.fGroupAddCar.patchValue({linkID:this.fGroupAddCar.value.linkID[0]})
+    if (this.fGroupAddCar.value.owner instanceof Object) {
+      this.fGroupAddCar.patchValue({ owner: this.fGroupAddCar.value.owner[0] });
+    }
+    if (this.fGroupAddCar.value.linkID instanceof Object) {
+      this.fGroupAddCar.patchValue({
+        linkID: this.fGroupAddCar.value.linkID[0],
+      });
     }
     this.fGroupAddCar.patchValue({
-      resourceType : '2',
-      linkType : '3',
-      equipments:this.lstEquipment,
+      resourceType: '2',
+      linkType: '3',
+      equipments: this.lstEquipment,
     });
 
     this.dialogRef.dataService
@@ -224,34 +231,30 @@ export class PopupAddCarsComponent implements OnInit, AfterViewInit {
               }
             });
           this.dialogRef.close();
-        }        
+        }
         return;
       });
-    
   }
 
-  fileCount(event){
-    this.fGroupAddCar.value.icon= event.data[0].data;    
+  fileCount(event) {
+    this.fGroupAddCar.value.icon = event.data[0].data;
   }
-  fileAdded(event){debugger}
-  
+  fileAdded(event) {
+    debugger;
+  }
+
   popupUploadFile() {
     this.attachment.uploadFile();
-  } 
+  }
 
   closeFormEdit(data) {
     this.initForm();
     this.closeEdit.emit(data);
   }
   dataValid() {
-    var data = this.fGroupAddCar.value;   
-    var result= true; 
-    var requiredControlName = [
-      'resourceName',
-      'owner',
-      'capacity',
-      'category',
-    ];
+    var data = this.fGroupAddCar.value;
+    var result = true;
+    var requiredControlName = ['resourceName', 'owner', 'capacity', 'category'];
     requiredControlName.forEach((item) => {
       var x = data[item];
       if (!data[item]) {
