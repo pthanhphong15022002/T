2467271499ccebb836c8dc578957887730676603
TrongNguyen <76871869+LeTrongNguyen2000@users.noreply.github.com>
2022-09-20 17:51:06 +0700

    Fix bug: recID bị null khi giao việc trong controlNote

--- projects/codx-share/src/lib/components/dynamic-setting/dynamic-setting.component.ts
+++ projects/codx-share/src/lib/components/dynamic-setting/dynamic-setting.component.ts
@@ -37,9 +37,9 @@ export class DynamicSettingComponent implements OnInit {
     private api: ApiHttpService,
     private changeDetectorRef: ChangeDetectorRef,
     private codxService: CodxService
-  ) { }
+  ) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
     this.views = [
@@ -128,7 +128,7 @@ export class DynamicSettingComponent implements OnInit {
                 '.aside-menu .menu-item[data-id]'
               );
               var item = items[0] as HTMLElement;
-              item.click();
+              if (item) item.click();
             }
           }
         });
