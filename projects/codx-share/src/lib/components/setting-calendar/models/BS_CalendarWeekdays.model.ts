61195b8641a8f41a8604a86bc104fbb4fe1c7a75
Nguyễn Ngọc Phú Sỹ <phusy9a11516@gmail.com>
2022-09-26 18:03:28 +0700

    ...

---
+++ projects/codx-share/src/lib/components/setting-calendar/models/BS_CalendarWeekdays.model.ts
@@ -0,0 +1,14 @@
+export class BS_CalendarWeekdays {
+    wKTemplateID: string;
+    weekday: string;
+    shiftType: string;
+    hours: any;
+    productivity: any;
+    dayOff: any;
+    dayoffCode: any;
+    note: any;
+    startTime: any;
+    endTime: any;
+    data: Array<any>;
+
+}
