7c6bdcb1b65f65e3c892fe7ddfff4c8a22f38bee
Vo Thao <vthao@lacviet.com.vn>
2022-10-07 16:29:42 +0700

    ....................

--- projects/codx-bp/src/lib/processsteps/popup-send-email/popup-send-email.component.ts
+++ projects/codx-bp/src/lib/processsteps/popup-send-email/popup-send-email.component.ts
@@ -1,34 +1,42 @@
 import { Component, OnInit, Optional, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { ApiHttpService, AuthStore, DialogData, DialogRef, NotificationsService } from 'codx-core';
-import { DispatchService } from 'projects/codx-od/src/lib/services/dispatch.service';
+import {
+  ApiHttpService,
+  AuthStore,
+  DialogData,
+  DialogRef,
+  FormModel,
+  NotificationsService,
+} from 'codx-core';
 import { AttachmentComponent } from 'projects/codx-share/src/lib/components/attachment/attachment.component';
 import { CodxBpService } from '../../codx-bp.service';
 
 @Component({
   selector: 'lib-popup-send-email',
   templateUrl: './popup-send-email.component.html',
-  styleUrls: ['./popup-send-email.component.css']
+  styleUrls: ['./popup-send-email.component.css'],
 })
 export class PopupSendEmailComponent implements OnInit {
-
+  @ViewChild('attachment') attachment: AttachmentComponent;
+  headerText: string = '';
+  subHeaderText: string = '';
+  dialog: DialogRef;
+  formModel: FormModel;
   user: any;
-  dialog: any;
   data: any;
   gridViewSetup: any;
-  job:"";
-  @ViewChild('attachment') attachment: AttachmentComponent;
-  // formatBytes = formatBytes;
-  sendEmailForm = new FormGroup({
-    from: new FormControl(),
-    to: new FormControl(),
-    subject: new FormControl(),
-    content: new FormControl(),
-    tenant: new FormControl(),
-    saveTemplate: new FormControl(),
-  });
+  lstFrom = [];
+  lstTo = [];
+  lstBcc = [];
+  lstCc = []
+  showCC = true;
+  showBCC=true ;
+  vllShare = 'TM003';
+  message ='tesstttttttttttttttttttttttttttttttttt'
+  objectID:any
+  //
   constructor(
-    private bpService : CodxBpService,
+    private bpService: CodxBpService,
     private authStore: AuthStore,
     private notifySvr: NotificationsService,
     private api: ApiHttpService,
@@ -40,49 +48,35 @@ export class PopupSendEmailComponent implements OnInit {
   }
   ngOnInit(): void {
     this.user = this.authStore.get();
-    this.gridViewSetup = this.data['gridViewSetup'];
-    this.getDataUser();
   }
-  changeValueOwner(event: any) {
-    //this.dispatch.owner = event.data?.value[0];
-    this.sendEmailForm.controls['to'].setValue([event.data?.value[0]]);
+
+  deleteUser(item, index) {}
+
+  eventApply(event) {}
+
+  changeSendType(e) {}
+
+  valueChange(e){
+
   }
-  onSave() {
-    /*  
-   this.dataAssign.recID = this.data.recID;
-    this.inforEmail.from = this.user?.userID;
-    if(this.inforEmail.to.length>0) this.inforEmail.to = this.inforEmail.to.toString().replace(",",";");
-    this.odService.sendMail(this.data.recID, this.inforEmail).subscribe((item)=>
-    {
-      if(item.status == 0) 
-      {
-        this.close();
-      }
-      this.notifySvr.notify(item.message);
-    }) */
-    this.sendEmailForm.value.from = this.user?.userID;
-    this.sendEmailForm.value.to = this.sendEmailForm.value.to.join(';');
-    this.sendEmailForm.value.saveTemplate =
-      this.sendEmailForm.value.saveTemplate == null
-        ? false
-        : this.sendEmailForm.value.saveTemplate;
-    this.bpService
-      .sendMail(
-        this.dialog.dataService.dataSelected.recID,
-        this.sendEmailForm.value
-      )
-      .subscribe((item) => {
-        if (item.status == 0) this.dialog.close();
-        this.notifySvr.notify(item.message);
-      });
+
+  fileAdded(e){
+
   }
-  getDataUser()
-  {
-    this.api.execSv("HR", 'ERM.Business.HR', "HRBusiness" , "GetDataJoinUserAsync" ,this.user?.userID).subscribe((item:any)=>{
-      if(item)
-      {
-        this.job = item?.jobName
-      }
-    })
+
+  getfileCount(e){
+
+  }
+
+  openFormUploadFile(){
+
+  }
+
+  sendMail(){
+
+  }
+
+  click(){
+    
   }
 }
