976bd739d3a5dd1bcafa677d66f3e611636002b2
TrongNguyen <76871869+LeTrongNguyen2000@users.noreply.github.com>
2022-10-10 00:05:11 +0700

    95% thiết lập fd

--- projects/codx-fd/src/lib/setting/dedication-rank/add-dedication-rank/add-dedication-rank.component.ts
+++ projects/codx-fd/src/lib/setting/dedication-rank/add-dedication-rank/add-dedication-rank.component.ts
@@ -1,48 +1,38 @@
-import {
-  Component,
-  OnInit,
-  Injector,
-  Optional,
-  ViewChild,
-} from '@angular/core';
-import {
-  DialogRef,
-  UIComponent,
-  DialogData,
-  CodxFormComponent,
-  NotificationsService,
-} from 'codx-core';
-import { CodxFdService } from 'projects/codx-fd/src/public-api';
+import { Component, OnInit, Injector, Optional, ViewChild } from '@angular/core';
+import { AuthStore, DialogData, DialogRef, NotificationsService, UIComponent, CodxFormComponent } from 'codx-core';
+import { CodxFdService } from '../../../codx-fd.service';
 
 @Component({
-  selector: 'lib-add-behavior',
-  templateUrl: './add-behavior.component.html',
-  styleUrls: ['./add-behavior.component.scss'],
+  selector: 'lib-add-dedication-rank',
+  templateUrl: './add-dedication-rank.component.html',
+  styleUrls: ['./add-dedication-rank.component.scss']
 })
-export class AddBehaviorComponent extends UIComponent implements OnInit {
+export class AddDedicationRankComponent extends UIComponent implements OnInit {
+
   header = '';
   title = '';
   dialog: DialogRef;
   formModel: any;
   dataUpdate: any;
   isModeAdd = true;
+  user: any;
 
   @ViewChild('form') form: CodxFormComponent;
 
-  constructor(
-    private injector: Injector,
+  constructor(private injector: Injector,
     private notification: NotificationsService,
+    private authStore: AuthStore,
     private fdSV: CodxFdService,
     @Optional() dt: DialogData,
-    @Optional() dialog: DialogRef
-  ) {
+    @Optional() dialog: DialogRef) {
     super(injector);
+    this.user = this.authStore.get();
     this.dialog = dialog;
     this.dataUpdate = JSON.parse(
       JSON.stringify(dialog.dataService.dataSelected)
     );
-    this.isModeAdd = dt.data?.isModeAdd;
     this.formModel = dialog.formModel;
+    this.isModeAdd = dt.data?.isModeAdd;
     this.title = dt.data?.headerText;
     this.cache.functionList(this.formModel.funcID).subscribe((res) => {
       if (res) {
@@ -53,7 +43,7 @@ export class AddBehaviorComponent extends UIComponent implements OnInit {
           res?.customName.slice(1);
       }
     });
-  }
+   }
 
   onInit(): void {
   }
@@ -64,28 +54,35 @@ export class AddBehaviorComponent extends UIComponent implements OnInit {
     }
   }
 
+  valueChangeColor(e) {
+
+  }
+
   onSave() {
     var formGroup = this.form.formGroup.controls;
-    if (formGroup.competenceID.status == 'VALID') {
+    if (
+      formGroup.breakName.status == 'VALID' &&
+      formGroup.breakValue.status == 'VALID' &&
+      formGroup.color.status == 'VALID'
+    ) {
       this.dialog.dataService
         .save((option: any) => this.beforeSave(option), 0)
         .subscribe((res) => {
           if (this.isModeAdd) {
-            if (res && res.save[3][0]) this.dialog.close(res.save[3][0]);
+            if (res && res.save[2]) this.dialog.close(res.save[2]);
             else this.notification.notifyCode('SYS023');
           } else {
-            if (res && res.update[3][0]) this.dialog.close(res.update[3][0]);
-            else this.notification.notifyCode('SYS023');
+            if (res && res.update[2]) this.dialog.close(res.update[2]);
+            else this.notification.notifyCode('SYS007');
           }
         });
     } else this.fdSV.notifyInvalid(this.form.formGroup, this.formModel);
   }
 
   beforeSave(option) {
-    option.methodName = 'AddEditCompetencesAsync';
-    this.dataUpdate.category = '1';
-    this.dataUpdate.isGroup = false;
-    option.data = [this.dataUpdate, this.isModeAdd, this.formModel];
+    option.methodName = 'AddEditRangeLineAsync';
+    if (this.user.userName) this.dataUpdate.createdName = this.user.userName;
+    option.data = [this.dataUpdate, this.isModeAdd];
     return true;
   }
 }
