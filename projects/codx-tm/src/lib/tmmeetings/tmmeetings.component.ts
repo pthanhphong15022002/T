1e4c340e4bc981998008daca4145ee441395ab33
Vo Thao <vthao@lacviet.com.vn>
2022-10-12 10:27:22 +0700

    ....

--- projects/codx-tm/src/lib/tmmeetings/tmmeetings.component.ts
+++ projects/codx-tm/src/lib/tmmeetings/tmmeetings.component.ts
@@ -505,10 +505,23 @@ export class TMMeetingsComponent
       )
       .subscribe((res) => {
         if (res[0]) {
-          this.itemSelected = this.view.dataService.data[0];
+          this.view.dataService.onAction.next({ type: 'delete', data: data });
+          // this.itemSelected = this.view.dataService.data[0];
         }
       });
   }
+
+  // delete(data?) {
+  //   let deleteItem = this.view.dataService.dataSelected;
+  //   if (data) {
+  //     deleteItem = data;
+  //   }
+  //   this.view.dataService.delete([deleteItem]).subscribe((res) => {
+  //     console.log(res);
+  //   });
+  // }  
+
+
 
   beforeDel(opt: RequestOption) {
     var itemSelected = opt.data[0];
@@ -548,12 +561,12 @@ export class TMMeetingsComponent
 
       let dialogModel = new DialogModel();
       dialogModel.IsFull = true;
-      // dialogModel.zIndex = 900;
+      dialogModel.zIndex = 900;
       var dialog = this.callfc.openForm(
         PopupTabsViewsDetailsComponent,
         '',
-        0,
-        0,
+        100,
+        100,
         '',
         obj,
         '',
