1f6eb4f171fc9c82880f91910916c64dc874d298
TrongNguyen <76871869+LeTrongNguyen2000@users.noreply.github.com>
2022-10-18 18:01:38 +0700

    i package ngx-drag-drop

--- projects/codx-wp/src/lib/test-survey/test-survey.component.ts
+++ projects/codx-wp/src/lib/test-survey/test-survey.component.ts
@@ -2,18 +2,18 @@ import {
   Component,
   Injector,
   OnInit,
+  TemplateRef,
   ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
-import { ToolbarService } from '@syncfusion/ej2-angular-gantt';
+import {
+  MultiSelectService,
+  RteService,
+} from '@syncfusion/ej2-angular-inplace-editor';
 import { TextBoxModel } from '@syncfusion/ej2-angular-inputs';
 import {
-  LinkService,
-  ImageService,
-  HtmlEditorService,
-  QuickToolbarService,
   RichTextEditorModel,
-} from '@syncfusion/ej2-angular-richtexteditor';
+} from '@syncfusion/ej2-angular-richtexteditor'; 
 import { UIComponent } from 'codx-core';
 
 @Component({
@@ -21,13 +21,7 @@ import { UIComponent } from 'codx-core';
   templateUrl: './test-survey.component.html',
   styleUrls: ['./test-survey.component.scss'],
   encapsulation: ViewEncapsulation.None,
-  providers: [
-    ToolbarService,
-    LinkService,
-    ImageService,
-    HtmlEditorService,
-    QuickToolbarService,
-  ],
+  providers: [RteService, MultiSelectService],
 })
 export class TestSurveyComponent extends UIComponent implements OnInit {
   public titleEditorValue: String = 'Succinctly E-Book about TypeScript';
@@ -39,18 +33,7 @@ export class TestSurveyComponent extends UIComponent implements OnInit {
   public commentEditorModel: RichTextEditorModel = {
     toolbarSettings: {
       enableFloating: false,
-      items: [
-        'Bold',
-        'Italic',
-        'Underline',
-        'FontColor',
-        'BackgroundColor',
-        'LowerCase',
-        'UpperCase',
-        '|',
-        'OrderedList',
-        'UnorderedList',
-      ],
+      items: ['Bold', 'Italic', 'Underline', 'ClearFormat', 'CreateLink'],
     },
   };
   public titleRule: { [name: string]: { [rule: string]: Object } } = {
@@ -60,19 +43,46 @@ export class TestSurveyComponent extends UIComponent implements OnInit {
     rte: { required: [true, 'Enter valid comments'] },
   };
 
-  constructor(inject: Injector) {
+  constructor(inject: Injector, 
+    ) {
     super(inject);
   }
+
+  public data: any = [
+    {
+      text: 'JavaScript',
+      pic: 'javascript',
+      description:
+        'It is a lightweight interpreted or JIT-compiled programming language.',
+    },
+    {
+      text: 'TypeScript',
+      pic: 'typescript',
+      description:
+        'It is a typed superset of Javascript that compiles to plain JavaScript.',
+    },
+    {
+      text: 'Angular',
+      pic: 'angular',
+      description:
+        'It is a TypeScript-based open-source web application framework.',
+    },
+    {
+      text: 'React',
+      pic: 'react',
+      description:
+        'A JavaScript library for building user interfaces. It can also render on the server using Node.',
+    },
+    {
+      text: 'Vue',
+      pic: 'vue',
+      description:
+        'A progressive framework for building user interfaces. it is incrementally adoptable.',
+    },
+  ];
 
   onInit(): void {}
   valueChange(e) {
     console.log(e);
   }
-  public data: { [key: string]: Object }[] = [
-    { text: 'JavaScript', pic: 'javascript', description: 'It is a lightweight interpreted or JIT-compiled programming language.' },
-    { text: 'TypeScript', pic: 'typescript', description: 'It is a typed superset of Javascript that compiles to plain JavaScript.' },
-    { text: 'Angular', pic: 'angular', description: 'It is a TypeScript-based open-source web application framework.' },
-    { text: 'React', pic: 'react', description: 'A JavaScript library for building user interfaces. It can also render on the server using Node.' },
-    { text: 'Vue', pic: 'vue', description: 'A progressive framework for building user interfaces. it is incrementally adoptable.' }
-];
 }
