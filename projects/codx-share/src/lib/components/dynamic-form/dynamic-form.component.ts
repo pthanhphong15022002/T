ad80f9362cc8ea9e4d34cd86e8c9700d2d4a47d6
vvquang <vvquanglacviet.com.vn>
2022-10-18 10:01:08 +0700

    ...

--- projects/codx-share/src/lib/components/dynamic-form/dynamic-form.component.ts
+++ projects/codx-share/src/lib/components/dynamic-form/dynamic-form.component.ts
@@ -72,7 +72,7 @@ export class DynamicFormComponent extends UIComponent {
         model: {
           resources: this.columnsGrid,
           template2: this.morefunction,
-          // frozenColumns: 1,
+          frozenColumns: 1,
         },
       },
     ];
