afd01235f727a33d94deeec54b56647ea3986375
TrongNguyen <76871869+LeTrongNguyen2000@users.noreply.github.com>
2022-10-21 10:26:30 +0700

    add codx-sv

---
+++ projects/codx-sv/README.md
@@ -0,0 +1,24 @@
+# CodxSv
+
+This library was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.0.
+
+## Code scaffolding
+
+Run `ng generate component component-name --project codx-sv` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module --project codx-sv`.
+> Note: Don't forget to add `--project codx-sv` or else it will be added to the default project in your `angular.json` file. 
+
+## Build
+
+Run `ng build codx-sv` to build the project. The build artifacts will be stored in the `dist/` directory.
+
+## Publishing
+
+After building your library with `ng build codx-sv`, go to the dist folder `cd dist/codx-sv` and run `npm publish`.
+
+## Running unit tests
+
+Run `ng test codx-sv` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
---
+++ projects/codx-sv/src/lib/codx-sv.module.ts
@@ -0,0 +1,64 @@
+import { OverlayModule } from '@angular/cdk/overlay';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  ModuleWithProviders,
+  NgModule,
+  Type,
+} from '@angular/core';
+import { CoreModule } from '@core/core.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CodxCoreModule, EnvironmentConfig } from 'codx-core';
+import { InlineSVGModule } from 'ng-inline-svg';
+import { CodxShareModule } from 'projects/codx-share/src/public-api';
+import { LayoutComponent } from './_layout/layout.component';
+import { RouterModule, Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home/:funcID',
+        // component: '',
+      },
+    ],
+  },
+];
+
+const Component: Type<any>[] = [
+  LayoutComponent,
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    OverlayModule,
+    InlineSVGModule.forRoot(),
+    HttpClientModule,
+    CodxCoreModule,
+    CodxShareModule,
+    CodxSVModule,
+    CoreModule,
+    NgbModule,
+    RouterModule.forChild(routes),
+  ],
+  exports: [RouterModule],
+  declarations: Component,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class CodxSVModule {
+  public static forRoot(
+    config?: EnvironmentConfig
+  ): ModuleWithProviders<CodxCoreModule> {
+    return {
+      ngModule: CodxCoreModule,
+      providers: [ 
+        HttpClientModule,
+        { provide: EnvironmentConfig, useValue: config },
+      ],
+    };
+  }
+}   
