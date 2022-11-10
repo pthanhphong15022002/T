6775f8710dae4aeddc3519c86c125e773546b359
Nguyễn Ngọc Phú Sỹ <phusy9a11516@gmail.com>
2022-11-10 14:23:37 +0700

    ...

--- projects/codx-ep/src/lib/settings/epCards/popup-add-epCards/popup-add-epCards.component.ts
+++ projects/codx-ep/src/lib/settings/epCards/popup-add-epCards/popup-add-epCards.component.ts
@@ -75,21 +75,7 @@ export class PopupAddEpCardsComponent extends UIComponent {
     this.codxEpService.getAutoNumberDefault(this.formModel.funcID).subscribe(autoN=>{
       if(autoN){
         if(!autoN?.stop ){
-          if(this.isAdd){
-            //ktra tham so auto number stop =true == ko dùng auto number
-            // this.api.execSv("SYS", "ERM.Business.AD", "AutoNumbersBusiness", "CreateAutoNumberByFunction", [this.formModel.funcID]).subscribe(autoNumber=>{
-            //   if(autoNumber)
-            //   {
-            //     this.data.resourceID=autoNumber;
-            //     this.detectorRef.detectChanges();
-            //   }
-            // })
-            this.autoNumDisable=true;
-          }
-          else{
-            this.data.resourceID=null;
-          }
-          
+          this.autoNumDisable=true;
         }
       }
     })
