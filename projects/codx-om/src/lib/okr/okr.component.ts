f0ee63144fd125b3d41d80a00945f83a462ca9b9
chungthai7596 <bachung7596@gmail.com>
2022-11-11 09:19:06 +0700

    ..

--- projects/codx-om/src/lib/okr/okr.component.ts
+++ projects/codx-om/src/lib/okr/okr.component.ts
@@ -41,10 +41,10 @@ export class OKRComponent extends UIComponent implements AfterViewInit {
   onInit(): void {
     //Lấy thông tin công ty
     var user = this.auth.get();
-    this.cache.getCompany(user.userID).subscribe(item=>{
-      if(item)
-        this.titleRoom = item.organizationName
-    })
+    // this.cache.getCompany(user.userID).subscribe(item=>{
+    //   if(item)
+    //     this.titleRoom = item.organizationName
+    // })
   }
   //Thêm mới mục tiêu
   add(e:any)
