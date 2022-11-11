8e152059e18f202deefe5b804d8ae94cbcddac8b
chungthai7596 <bachung7596@gmail.com>
2022-11-11 09:12:03 +0700

    ..

--- projects/codx-om/src/lib/okr/okr.component.ts
+++ projects/codx-om/src/lib/okr/okr.component.ts
@@ -1,19 +1,55 @@
-import { Component, Injector } from '@angular/core';
-import { UIComponent } from 'codx-core';
+import { AfterViewInit, Component, Injector, TemplateRef, ViewChild } from '@angular/core';
+import { ButtonModel, UIComponent, ViewModel, ViewType ,AuthStore } from 'codx-core';
 
 @Component({
   selector: 'lib-okr',
   templateUrl: './okr.component.html',
   styleUrls: ['./okr.component.css']
 })
-export class OKRComponent extends UIComponent {
+export class OKRComponent extends UIComponent implements AfterViewInit {
 
+  button?: ButtonModel;
+  views: Array<ViewModel> | any = [];
+  @ViewChild('panelRight') panelRight: TemplateRef<any>;
+  
+  //title//
+  titleRoom = "Phòng kinh doanh";
+  /////////
+  auth: AuthStore;
   constructor(inject: Injector) {
     super(inject)
+    this.auth = inject.get(AuthStore);
    }
-
-  onInit(): void {
-    
+  ngAfterViewInit(): void {
+    this.views = [
+      {
+        id:"1",
+        type: ViewType.content,
+        active: true,
+        sameData: true,
+        model: {
+          panelRightRef: this.panelRight,
+          contextMenu: '',
+        },
+      },
+    ];
+    this.button = {
+      id: 'btnAdd',
+    };
   }
 
+  onInit(): void {
+    //Lấy thông tin công ty
+    var user = this.auth.get();
+    this.cache.getCompany(user.userID).subscribe(item=>{
+      if(item)
+        this.titleRoom = item.organizationName
+    })
+  }
+  //Thêm mới mục tiêu
+  add(e:any)
+  {
+
+  }
+ 
 }
