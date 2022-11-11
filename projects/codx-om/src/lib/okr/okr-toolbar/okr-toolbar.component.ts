73f7f3ebc4e765d1815378437f7d247ad0332b8f
chungthai7596 <bachung7596@gmail.com>
2022-11-11 18:36:10 +0700

    ..

--- projects/codx-om/src/lib/okr/okr-toolbar/okr-toolbar.component.ts
+++ projects/codx-om/src/lib/okr/okr-toolbar/okr-toolbar.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ButtonModel } from 'codx-core';
 
 @Component({
   selector: 'lib-okr-toolbar',
   templateUrl: './okr-toolbar.component.html',
-  styleUrls: ['./okr-toolbar.component.css']
+  styleUrls: ['./okr-toolbar.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class OkrToolbarComponent implements OnInit {
 
