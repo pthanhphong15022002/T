f63b3700ab6883629f827c1ff7f52d178338a0b1
Buu Nguyen <74578456+maiyozkyo@users.noreply.github.com>
2022-11-12 12:22:06 +0700

    cmt OKR.component.ts onInit

--- projects/codx-om/src/lib/okr/okr.component.ts
+++ projects/codx-om/src/lib/okr/okr.component.ts
@@ -1,29 +1,42 @@
-import { AfterViewInit, Component, Injector, TemplateRef, ViewChild } from '@angular/core';
-import { ButtonModel, UIComponent, ViewModel, ViewType ,AuthStore , CallFuncService, DialogModel } from 'codx-core';
+import {
+  AfterViewInit,
+  Component,
+  Injector,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
+import {
+  ButtonModel,
+  UIComponent,
+  ViewModel,
+  ViewType,
+  AuthStore,
+  CallFuncService,
+  DialogModel,
+} from 'codx-core';
 import { OkrAddComponent } from './okr-add/okr-add.component';
 
 @Component({
   selector: 'lib-okr',
   templateUrl: './okr.component.html',
-  styleUrls: ['./okr.component.css']
+  styleUrls: ['./okr.component.css'],
 })
 export class OKRComponent extends UIComponent implements AfterViewInit {
-
   views: Array<ViewModel> | any = [];
   @ViewChild('panelRight') panelRight: TemplateRef<any>;
-  
+
   //title//
-  titleRoom = "Phòng kinh doanh";
+  titleRoom = 'Phòng kinh doanh';
   /////////
   auth: AuthStore;
   constructor(inject: Injector) {
-    super(inject)
+    super(inject);
     this.auth = inject.get(AuthStore);
-   }
+  }
   ngAfterViewInit(): void {
     this.views = [
       {
-        id:"1",
+        id: '1',
         type: ViewType.content,
         active: true,
         sameData: true,
@@ -33,32 +46,29 @@ export class OKRComponent extends UIComponent implements AfterViewInit {
         },
       },
     ];
-  
   }
 
   onInit(): void {
     //Lấy thông tin công ty
     var user = this.auth.get();
-    this.cache.getCompany(user.userID).subscribe(item=>{
-      if(item)
-        this.titleRoom = item.organizationName
-    })
+    // this.cache.getCompany(user.userID).subscribe(item=>{
+    //   if(item)
+    //     this.titleRoom = item.organizationName
+    // })
   }
   //Thêm mới mục tiêu
-  add(e:any)
-  {
-    var dialogModel = new  DialogModel();
+  add(e: any) {
+    var dialogModel = new DialogModel();
     dialogModel.IsFull = true;
     this.callfc.openForm(
       OkrAddComponent,
-      "",
+      '',
       null,
       null,
       null,
       null,
-      "",
+      '',
       dialogModel
-    )
+    );
   }
- 
 }
