58eb29b0c262ca0da7c39c82c29ca35b14e18dae
vuhiepp07 <vuhiep2k107@gmail.com>
2022-11-18 17:54:49 +0700

    C&B EVisa UI continue, EPassport fixbug

--- projects/codx-hr/src/lib/employee-profile/employee-visa-form/employee-visa-form.component.ts
+++ projects/codx-hr/src/lib/employee-profile/employee-visa-form/employee-visa-form.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup } from '@angular/forms';
 import { CodxHrService } from './../../codx-hr.service';
 import { Injector } from '@angular/core';
 import { Component, OnInit, Optional, ViewChild } from '@angular/core';
@@ -17,10 +18,13 @@ import {
 })
 export class EmployeeVisaFormComponent extends UIComponent implements OnInit {
   formModel: FormModel;
+  formGroup: FormGroup;
   dialog: DialogRef;
   data;
-  isAfterRender = false;
   headerText: ''
+  funcID;
+  employId;
+  isAfterRender = false;
   @ViewChild('form') form: CodxFormComponent;
 
   constructor(
@@ -32,24 +36,54 @@ export class EmployeeVisaFormComponent extends UIComponent implements OnInit {
   ) {
     super(injector);
     this.dialog = dialog;
-    this.formModel = dialog?.formModel;
+    //this.formModel = dialog?.formModel;
     this.headerText = data?.data?.headerText;
-    if(this.formModel){
-      this.isAfterRender = true
+    // if(this.formModel){
+    //   this.isAfterRender = true
+    // }
+
+    if(!this.formModel){
+      this.formModel = new FormModel();
+      this.formModel.formName = 'EVisas';
+      this.formModel.entityName = 'HR_EVisas';
+      this.formModel.gridViewName = 'grvEVisas';
     }
-    this.data = dialog?.dataService?.dataSelected
-   }
+    this.funcID = this.dialog.formModel.funcID;
+    this.employId = data?.data?.employeeId;
+    console.log('employid', this.employId)
+    console.log('formmdel', this.formModel);
+  }
+
+    initForm(){
+      this.hrService
+      .getFormGroup(this.formModel.formName, this.formModel.gridViewName)
+      .then((item) => {
+        this.formGroup = item;  
+        console.log('formgr test:', this.formGroup)  
+        this.hrService.getEmployeeVisasInfo(this.employId).subscribe(p => {
+          console.log('thong tin ho chieu', p);
+          this.data = p;
+          this.formModel.currentData = this.data
+          // this.dialog.dataService.dataSelected = this.data
+          console.log('du lieu formmodel',this.formModel.currentData);
+          this.formGroup.patchValue(this.data)
+          
+          this.isAfterRender = true
+      })
+    });
+  }
 
   onInit(): void {
+    this.initForm();
   }
 
   onSaveForm(){
-    this.hrService.saveEmployeeAssurTaxBankAccountInfo(this.data).subscribe(p => {
+    this.hrService.updateEmployeeVisaInfo(this.data).subscribe(p => {
       if(p === "True"){
         this.notify.notifyCode('SYS007')
         this.dialog.close();
       }
-    })
+      else this.notify.notifyCode('DM034')
+    });
   }
-
 }
