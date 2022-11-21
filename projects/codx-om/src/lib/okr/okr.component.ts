8f14422620a0cd9ade98984de700c59f3bca2df1
Phúc <tdnphuck42@gmail.com>
2022-11-21 17:01:51 +0700

    phan quyen moi

--- projects/codx-om/src/lib/okr/okr.component.ts
+++ projects/codx-om/src/lib/okr/okr.component.ts
@@ -57,10 +57,10 @@ export class OKRComponent extends UIComponent implements AfterViewInit {
 
   onInit(): void {
     var user = this.auth.get();
-    this.cache.getCompany(user.userID).subscribe(item=>{
-      if(item)
-        this.titleRoom = item.organizationName
-    })
+    // this.cache.getCompany(user.userID).subscribe(item=>{
+    //   if(item)
+    //     this.titleRoom = item.organizationName
+    // })
     var dataRequest = new DataRequest();
     dataRequest.funcID = "OMT01"
     dataRequest.entityName = "OM_OKRs"
@@ -114,5 +114,5 @@ export class OKRComponent extends UIComponent implements AfterViewInit {
       });
   }
 
-  
+
 }
