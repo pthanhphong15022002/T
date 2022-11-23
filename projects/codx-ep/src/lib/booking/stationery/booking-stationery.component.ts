99e67e55d66fe6a4bc5387c730cbb3e53306a2af
Nguyễn Ngọc Phú Sỹ <phusy9a11516@gmail.com>
2022-11-23 08:57:27 +0700

    Update booking-stationery.component.ts

--- projects/codx-ep/src/lib/booking/stationery/booking-stationery.component.ts
+++ projects/codx-ep/src/lib/booking/stationery/booking-stationery.component.ts
@@ -149,7 +149,7 @@ export class BookingStationeryComponent
     if(event != null && data != null && data.issueStatus == 3){
       event.forEach((func) => {
         if (
-          func.functionID == 'EPT40303' /*MF sửa*/ 
+          func.functionID == 'EPT40303' /*MF cấp phát*/ 
         ) {
           func.disabled = true;
         }
