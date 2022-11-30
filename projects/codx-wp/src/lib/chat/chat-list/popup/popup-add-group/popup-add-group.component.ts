5d0eaf1a462f6745e00bea8731a84e14a15a06c8
LocTran <loctt.cntp@gamil.com>
2022-11-30 15:03:17 +0700

    ...

--- projects/codx-wp/src/lib/chat/chat-list/popup/popup-add-group/popup-add-group.component.ts
+++ projects/codx-wp/src/lib/chat/chat-list/popup/popup-add-group/popup-add-group.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectorRef, Component, OnInit, Optional } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, Optional, ViewChild } from '@angular/core';
 import { Dialog } from '@syncfusion/ej2-angular-popups';
-import { ApiHttpService, AuthStore, DialogData, DialogRef, NotificationsService } from 'codx-core';
+import { ApiHttpService, AuthStore, DialogData, DialogRef, ImageViewerComponent, NotificationsService } from 'codx-core';
 import { WP_Groups } from 'projects/codx-wp/src/lib/models/WP_Groups.model';
+import { WP_Members } from 'projects/codx-wp/src/lib/models/WP_Members.model';
 
 @Component({
   selector: 'wp-popup-add-group',
@@ -16,6 +17,7 @@ export class PopupAddGroupComponent implements OnInit {
   user:any = null;
   headerText:string = "";
   group:WP_Groups = new WP_Groups();
+  @ViewChild("codxImg") codxImg:ImageViewerComponent;
   constructor(
     private api:ApiHttpService,
     private notifiSV:NotificationsService,
@@ -50,5 +52,44 @@ export class PopupAddGroupComponent implements OnInit {
       this.group.groupName = event.data;
       this.dt.detectChanges();
     }
+  }
+
+  // insert group
+  insertGroup(){
+    
+    if(this.user)
+    {
+      let members:Array<WP_Members> = new Array<WP_Members>();
+      let member1:WP_Members = new WP_Members();
+      member1.userID = "ADMIN";
+      member1.userName = "ADMinistrator"
+      member1.menberType = "1";
+      member1.createdBy = "ADMIN";
+      let member2:WP_Members = new WP_Members();
+      member2.userID = "TTLOC";
+      member2.userName = "Trần Tấn Lộc"
+      member2.menberType = "2";
+      member2.createdBy = "ADMIN";
+      members.push(member1);
+      members.push(member2);
+      this.group.members = members;
+      this.group.createdBy = this.user.userID;
+      this.api.execSv("WP","ERM.Business.WP","GroupBusiness","InsertGroupAsync",[this.group])
+      .subscribe((res:any[]) =>{
+        if(Array.isArray(res) && res[0])
+        {
+          let data = res[1];
+          if(data.groupType === "2"){
+            this.codxImg.updateFileDirectReload(this.group.groupID).subscribe();
+          }
+          this.dialogRef.close(data);
+        }
+        else{
+          this.dialogRef.close();
+          this.notifiSV.notify("Thêm không thành công");
+        }
+      });
+    }
+    
   }
 }
