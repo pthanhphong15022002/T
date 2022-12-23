3b8bcbf21088c0659a452ee62a00292e9106bc8e
DTLONG <dtlong@lacviet.com.vn>
2022-12-23 10:04:49 +0700

    update Om service

---
+++ projects/codx-om/src/lib/model/distributeOKR.model.ts
@@ -0,0 +1,11 @@
+export class DistributeOKR {
+    orgUnitID: string;
+    orgUnitName: string;
+    weight:number;
+    distributeValue:number;
+    distributePct:number;
+    umid:string;
+    okrName:string;
+
+
+  }
