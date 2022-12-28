5fcd957a6c3cc2fb7644f2d27054e016cffd5d6d
nvthuan <nvthuan@lacviet.com.vn>
2022-12-28 16:12:09 +0700

    role edit processStep

--- projects/codx-bp/src/lib/processsteps/processsteps.component.ts
+++ projects/codx-bp/src/lib/processsteps/processsteps.component.ts
@@ -1310,12 +1310,16 @@ export class ProcessStepsComponent
   }
 
   closeMFTypeA() {
-    this.hideMoreFC = false;
-    this.hideMoreFCChild = true;
+    if(this.isEdit){
+      this.hideMoreFC = false;
+      this.hideMoreFCChild = true;
+    }
   }
   openMFTypeA() {
-    this.hideMoreFC = true;
-    this.hideMoreFCChild = false;
+    if(this.isEdit){
+      this.hideMoreFC = true;
+      this.hideMoreFCChild = false;
+    }
   }
 
   // showAllparent(text) {
