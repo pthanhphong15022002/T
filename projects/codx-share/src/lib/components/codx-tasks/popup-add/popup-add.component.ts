1cb5fde0dbce8064eee3ab4e6441fd4413d4cfcf
Vo Thao <vthao@lacviet.com.vn>
2023-01-04 17:17:08 +0700

    check send mail BE => cmt FE

--- projects/codx-share/src/lib/components/codx-tasks/popup-add/popup-add.component.ts
+++ projects/codx-share/src/lib/components/codx-tasks/popup-add/popup-add.component.ts
@@ -588,21 +588,22 @@ export class PopupAddComponent implements OnInit, AfterViewInit {
         this.attachment?.clearData();
         if (res && res.save) {
           var task = res.save[0];
-          if (task.category == '3') {
-            if (this.param?.ConfirmControl == '1')
-              this.tmSv
-                .sendAlertMail(task?.recID, 'TM_0008', this.functionID)
-                .subscribe();
-            else
-              this.tmSv
-                .sendAlertMail(task?.recID, 'TM_0001', this.functionID)
-                .subscribe();
-          }
+          //send mail FE
+          // if (task.category == '3') {
+          //   if (this.param?.ConfirmControl == '1')
+          //     this.tmSv
+          //       .sendAlertMail(task?.recID, 'TM_0008', this.functionID)
+          //       .subscribe();
+          //   else
+          //     this.tmSv
+          //       .sendAlertMail(task?.recID, 'TM_0001', this.functionID)
+          //       .subscribe();
+          // }
 
-          if (task?.category == '1' && task.verifyControl == '1')
-            this.tmSv
-              .sendAlertMail(task?.recID, 'TM_0018', this.functionID)
-              .subscribe();
+          // if (task?.category == '1' && task.verifyControl == '1')
+          //   this.tmSv
+          //     .sendAlertMail(task?.recID, 'TM_0018', this.functionID)
+          //     .subscribe();
         }
       });
   }
@@ -620,9 +621,10 @@ export class PopupAddComponent implements OnInit, AfterViewInit {
                   var task = res.update;
                   this.dialog.close(res.update);
                   this.attachment?.clearData();
-                  this.tmSv
-                    .sendAlertMail(task?.recID, 'TM_0002', this.functionID)
-                    .subscribe();
+                  //send mail FE
+                  // this.tmSv
+                  //   .sendAlertMail(task?.recID, 'TM_0002', this.functionID)
+                  //   .subscribe();
                 }
               } else {
                 this.dialog.close();
