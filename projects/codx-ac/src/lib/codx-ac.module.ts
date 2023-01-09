b1bf1279c56cf3f0d4a63b8dc8ba5428f1ab9b28
tantm <tmtan@lacviet.com.vn>
2023-01-09 18:04:14 +0700

    danh muc tien te(update,delete,add form mới)

--- projects/codx-ac/src/lib/codx-ac.module.ts
+++ projects/codx-ac/src/lib/codx-ac.module.ts
@@ -18,6 +18,7 @@ import { CodxReportModule } from 'projects/codx-report/src/public-api';
 import { AccumulationTooltipService } from '@syncfusion/ej2-angular-charts';
 import { PopAddCurrencyComponent } from './currency-form/pop-add-currency/pop-add-currency.component';
 import { LayoutNoAsideComponent } from 'projects/codx-share/src/lib/_layout/_noAside/_noAside.component';
+import { PopSettingExchangeComponent } from './currency-form/pop-setting-exchange/pop-setting-exchange.component';
 
 export const routes: Routes = [
   {
@@ -38,7 +39,8 @@ export const routes: Routes = [
     CodxAcComponent,
     LayoutComponent,
     CurrencyFormComponent,
-    PopAddCurrencyComponent
+    PopAddCurrencyComponent,
+    PopSettingExchangeComponent
   ],
   imports: [
     RouterModule.forChild(routes),
