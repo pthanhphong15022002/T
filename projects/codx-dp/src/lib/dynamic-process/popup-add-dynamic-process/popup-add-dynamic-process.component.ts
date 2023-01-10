485ef0dcb7a284ba8ff402dfda26c01ef4c9781a
nvthuan <nvthuan@lacviet.com.vn>
2023-01-10 09:07:14 +0700

    show and hiden

--- projects/codx-dp/src/lib/dynamic-process/popup-add-dynamic-process/popup-add-dynamic-process.component.ts
+++ projects/codx-dp/src/lib/dynamic-process/popup-add-dynamic-process/popup-add-dynamic-process.component.ts
@@ -96,14 +96,7 @@ export class PopupAddDynamicProcessComponent implements OnInit {
             name:"Đây là điều khác hẳn so với những lần gặp nhau trước đây. ",
             item:"Item3"
           }
-        },
-        {
-          item: "Item 116",
-          data:{
-            name:"Đây là điều khác hẳn so với những lần gặp nhau trước đây. ",
-            item:"Item3"
-          }
-        },
+        },      
       ]
     },
     {
@@ -116,21 +109,7 @@ export class PopupAddDynamicProcessComponent implements OnInit {
             name:"Đây là điều khác hẳn so với những lần gặp nhau trước đây. ",
             item:"Item3"
           }
-        },
-        {
-          item: "Item 1131",
-          data:{
-            name:"Đây là điều khác hẳn so với những lần gặp nhau trước đây. ",
-            item:"Item3"
-          }
-        },
-        {
-          item: "Item 11134",
-          data:{
-            name:"Đây là điều khác hẳn so với những lần gặp nhau trước đây. ",
-            item:"Item3"
-          }
-        },
+        },      
       ]
     },
 
@@ -160,9 +139,7 @@ export class PopupAddDynamicProcessComponent implements OnInit {
    show(){
     this.isShow = !this.isShow;
   }
-  showStage(){
-    this.isShowstage = !this.isShowstage;
-  }
+
   //#endregion
   //Setting class status Active
   updateNodeStatus(oldNode: number, newNode: number) {
@@ -282,12 +259,38 @@ export class PopupAddDynamicProcessComponent implements OnInit {
 
   }
 
-  show1(){
-    this.isShow = !this.isShow;
+  //#region Trường tùy chỉnh
+
+  //#region
+  clickRoles(e){
+
   }
-  addFile(e) {
-    this.attachment.uploadFile();
+  //end
+  //#endregion
+
+  //#region Trường tùy chỉnh 
+  clickShow(e,id){
+    let children = e.currentTarget.children[0];
+    let element = document.getElementById(id);
+    if (element) {
+     let isClose = element.classList.contains('hidden-main');
+     let isShow = element.classList.contains('show-main');
+      if (isClose){
+        children.classList.add('icon-expand_less');
+        children.classList.remove('icon-expand_more');
+        element.classList.remove('hidden-main');
+        element.classList.add('show-main');
+      } else if (isShow){
+        element.classList.remove('show-main');
+        element.classList.add('hidden-main');
+        children.classList.remove('icon-expand_less');
+        children.classList.add('icon-expand_more');
+      }
+   }
   }
+  //#region 
+
+  //#stage -- nvthuan
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
@@ -301,32 +304,11 @@ export class PopupAddDynamicProcessComponent implements OnInit {
     }
   }
 
-  drop1(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.data, event.previousIndex, event.currentIndex);
-  }
-  //#region Trường tùy chỉnh
+  //#stage
 
-  //#region
-  clickRoles(e){
-
+  //#job -- nvthuan
+  addFile(e) {
+    this.attachment.uploadFile();
   }
-  //end
-  //#endregion
-
-  //#region Trường tùy chỉnh 
-  clickShow(id){
-    var element = document.getElementById(id);
-    if (element) {
-     let isClose = element.classList.contains('hidden-main');
-     let isShow = element.classList.contains('show-main');
-      if (isClose) {
-        element.classList.remove('icon-add_box');
-        element.classList.add('hidden-main');
-      } else if (isShow) {
-        element.classList.remove('hidden-main');
-        element.classList.add('icon-add_box');
-      }
-   }
-  }
-  //#region 
+  //#job
 }
