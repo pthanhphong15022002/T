1fcb87d2569f123114c589c0f72eecfb7cd0e1a5
lvietbao <lvietbao@lacviet.com.vn>
2023-01-16 19:24:17 +0700

    push search in module DP

--- projects/codx-dp/src/lib/dynamic-process/dynamic-process.component.ts
+++ projects/codx-dp/src/lib/dynamic-process/dynamic-process.component.ts
@@ -142,7 +142,8 @@ export class DynamicProcessComponent
   }
 
   searchDynamicProcess($event) {
-    if ($event) this.changeDetectorRef.detectChanges();
+    this.view.dataService.search($event).subscribe();
+    this.changeDetectorRef.detectChanges();
   }
 
   async genAutoNumber() {
