90c032d1841c49aa6d7529ec68acd0e17fbca0c2
tantm <tmtan@lacviet.com.vn>
2023-01-18 17:03:43 +0700

    danh muc khach hang(popup)

--- projects/codx-ac/src/lib/customers/pop-add-customers/pop-add-customers.component.ts
+++ projects/codx-ac/src/lib/customers/pop-add-customers/pop-add-customers.component.ts
@@ -1,7 +1,11 @@
 import { ChangeDetectorRef, Component, Injector, OnInit, Optional, ViewChild } from '@angular/core';
 import { CacheService, ApiHttpService, CallFuncService, NotificationsService, UIComponent, DialogData, DialogRef, FormModel, CodxFormComponent, DialogModel } from 'codx-core';
 import { CodxAcService } from '../../codx-ac.service';
+import { Address } from '../../models/Address.model';
+import { BankAccount } from '../../models/BankAccount.model';
+import { Contact } from '../../models/Contact.model';
 import { Customers } from '../../models/Customers.model';
+import { PopAddAddressComponent } from '../pop-add-address/pop-add-address.component';
 import { PopAddBankComponent } from '../pop-add-bank/pop-add-bank.component';
 import { PopAddContactComponent } from '../pop-add-contact/pop-add-contact.component';
 
@@ -16,7 +20,10 @@ export class PopAddCustomersComponent extends UIComponent implements OnInit {
   headerText:string;
   formModel: FormModel;
   dialog!: DialogRef;
-  customers:Customers
+  customers:Customers;
+  objectBankaccount:Array<BankAccount> = [];
+  objectContact:Array<Contact> = [];
+  objectAddress:Array<Address> = [];
   tabInfo: any[] = [
     { icon: 'icon-info', text: 'Thông tin chung', name: 'Description' },
     { icon: 'icon-settings icon-20 me-3', text: 'Thiết lập', name: 'Establish' },
@@ -64,7 +71,7 @@ export class PopAddCustomersComponent extends UIComponent implements OnInit {
     opt.FormModel = dataModel;
     this.cache.gridViewSetup('BankAccounts','grvBankAccounts').subscribe(res=>{
       if(res){  
-        var dialogexchange = this.callfc.openForm(
+        var dialogbank = this.callfc.openForm(
           PopAddBankComponent,
           '',
           650,
@@ -73,8 +80,14 @@ export class PopAddCustomersComponent extends UIComponent implements OnInit {
           obj,
           '',
           opt
-        );
-      
+        ); 
+        dialogbank.closed.subscribe((x) => {
+          var databankaccount = JSON.parse(localStorage.getItem('databankaccount'));
+          if (databankaccount != null) {      
+            this.objectBankaccount.push(databankaccount);
+          }
+          window.localStorage.removeItem("databankaccount");
+        });
       }
     });
   }
@@ -90,7 +103,7 @@ export class PopAddCustomersComponent extends UIComponent implements OnInit {
     opt.FormModel = dataModel;
     this.cache.gridViewSetup('ContactBook','grvContactBook').subscribe(res=>{
       if(res){  
-        var dialogexchange = this.callfc.openForm(
+        var dialogcontact = this.callfc.openForm(
           PopAddContactComponent,
           '',
           650,
@@ -100,8 +113,164 @@ export class PopAddCustomersComponent extends UIComponent implements OnInit {
           '',
           opt
         );
-      
+        dialogcontact.closed.subscribe((x) => {
+          var datacontact = JSON.parse(localStorage.getItem('datacontact'));
+          if (datacontact != null) {      
+            this.objectContact.push(datacontact);
+          }
+          window.localStorage.removeItem("datacontact");
+        });
       }
     });
+  }
+  openPopupAddress(){
+    var obj = {
+      headerText: 'Thêm địa chỉ',
+    };
+    let opt = new DialogModel();
+    let dataModel = new FormModel();
+    dataModel.formName = 'AddressBook';
+    dataModel.gridViewName = 'grvAddressBook';
+    dataModel.entityName = 'BS_AddressBook';
+    opt.FormModel = dataModel;
+    this.cache.gridViewSetup('AddressBook','grvAddressBook').subscribe(res=>{
+      if(res){  
+        var dialogaddress = this.callfc.openForm(
+          PopAddAddressComponent,
+          '',
+          550,
+          650,
+          '',
+          obj,
+          '',
+          opt
+        );
+        dialogaddress.closed.subscribe((x) => {
+          var dataaddress = JSON.parse(localStorage.getItem('dataaddress'));
+          if (dataaddress != null) {      
+            this.objectAddress.push(dataaddress);
+          }
+          window.localStorage.removeItem("dataaddress");
+        });
+      }
+    });
+  }
+  deleteobject(data:any,type:any){
+    if (type == 'databank') {
+      let index = this.objectBankaccount.findIndex(x => x.bankAcctID == data.bankAcctID && x.bankID == data.bankID);
+      this.objectBankaccount.splice(index, 1);
+    }
+    if (type == 'datacontact') {
+      let index = this.objectContact.findIndex(x => x.contactName == data.contactName && x.phone == data.phone);
+      this.objectContact.splice(index, 1);
+    }
+    if (type == 'dataaddress') {
+      let index = this.objectAddress.findIndex(x => x.adressType == data.adressType && x.adressName == data.adressName);
+      this.objectAddress.splice(index, 1);
+    }
+  }
+  editobject(data:any,type:any){
+    if (type == 'databank') {
+      let index = this.objectBankaccount.findIndex(x => x.bankAcctID == data.bankAcctID);
+      var obj = {
+        headerText: 'Chỉnh sửa',
+        data:data
+      };
+      let opt = new DialogModel();
+      let dataModel = new FormModel();
+      dataModel.formName = 'BankAccounts';
+      dataModel.gridViewName = 'grvBankAccounts';
+      dataModel.entityName = 'BS_BankAccounts';
+      opt.FormModel = dataModel;
+      this.cache.gridViewSetup('BankAccounts','grvBankAccounts').subscribe(res=>{
+        if(res){  
+          var dialogbank = this.callfc.openForm(
+            PopAddBankComponent,
+            '',
+            650,
+            550,
+            '',
+            obj,
+            '',
+            opt
+          ); 
+          dialogbank.closed.subscribe((x) => {
+            var databankaccount = JSON.parse(localStorage.getItem('databankaccount'));
+            if (databankaccount != null) {      
+              this.objectBankaccount[index] = databankaccount;
+            }
+            window.localStorage.removeItem("databankaccount");
+          });
+        }
+      });
+    }
+    if (type == 'datacontact') {
+      let index = this.objectContact.findIndex(x => x.contactName == data.contactName && x.phone == data.phone);
+      var ob = {
+        headerText: 'Chỉnh sửa liên hệ',
+        data:data
+      };
+      let opt = new DialogModel();
+      let dataModel = new FormModel();
+      dataModel.formName = 'ContactBook';
+      dataModel.gridViewName = 'grvContactBook';
+      dataModel.entityName = 'BS_ContactBook';
+      opt.FormModel = dataModel;
+      this.cache.gridViewSetup('ContactBook','grvContactBook').subscribe(res=>{
+        if(res){  
+          var dialogcontact = this.callfc.openForm(
+            PopAddContactComponent,
+            '',
+            650,
+            550,
+            '',
+            ob,
+            '',
+            opt
+          );
+          dialogcontact.closed.subscribe((x) => {
+            var datacontact = JSON.parse(localStorage.getItem('datacontact'));
+            if (datacontact != null) {      
+              this.objectContact[index] = datacontact;
+            }
+            window.localStorage.removeItem("datacontact");
+          });
+        }
+      });
+    }
+    if (type == 'dataaddress') {
+      let index = this.objectAddress.findIndex(x => x.adressType == data.adressType && x.adressName == data.adressName);  
+      var obs = {
+        headerText: 'Chỉnh sửa địa chỉ',
+        data : data
+      };
+      let opt = new DialogModel();
+      let dataModel = new FormModel();
+      dataModel.formName = 'AddressBook';
+      dataModel.gridViewName = 'grvAddressBook';
+      dataModel.entityName = 'BS_AddressBook';
+      opt.FormModel = dataModel;
+      this.cache.gridViewSetup('AddressBook','grvAddressBook').subscribe(res=>{
+        if(res){  
+          var dialogaddress = this.callfc.openForm(
+            PopAddAddressComponent,
+            '',
+            550,
+            650,
+            '',
+            obs,
+            '',
+            opt
+          );
+          dialogaddress.closed.subscribe((x) => {
+            var dataaddress = JSON.parse(localStorage.getItem('dataaddress'));
+            if (dataaddress != null) {      
+              this.objectAddress[index] = dataaddress;
+            }
+            window.localStorage.removeItem("dataaddress");
+          });
+        }
+      });
+    }
   }
 }
