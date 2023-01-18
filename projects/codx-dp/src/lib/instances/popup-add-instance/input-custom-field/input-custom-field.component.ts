2fdde4cdb1b1fbcce7230084c572d07829f7c41b
Vo Thao <vthao@lacviet.com.vn>
2023-01-18 17:06:44 +0700

    check

--- projects/codx-dp/src/lib/instances/popup-add-instance/input-custom-field/input-custom-field.component.ts
+++ projects/codx-dp/src/lib/instances/popup-add-instance/input-custom-field/input-custom-field.component.ts
@@ -12,15 +12,28 @@ export class InputCustomFieldComponent implements OnInit {
   @Output() valueChangeCustom = new EventEmitter<any>();
   errorMessage = '';
   showErrMess = false;
+  //data tesst
+  typeControl = 'text';
+  currentRate = 1;
+  hovered = 0;
+  readonly = false;
+  min = 0;
+  max = 9999999;
   constructor(private cache: CacheService) {
     this.cache.message('SYS028').subscribe((res) => {
       this.errorMessage = res?.customName;
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.customField.note = 'Nhập số lượng';
+    this.customField.fieldName = 'số lượng';
+    this.customField.title = 'Số lượng nhân viên làm việc';
+    this.customField.rank = 10;
+    this.customField.rankIcon = 'fas fa-ambulance';
+  }
 
-  valueChange(e,data) {
-    this.valueChangeCustom.emit({e:e,data: data});
+  valueChange(e, data) {
+    this.valueChangeCustom.emit({ e: e, data: data });
   }
 }
