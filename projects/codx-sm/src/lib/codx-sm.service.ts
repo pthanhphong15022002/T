c541cab08e0c72707bd745d0f551875102661523
Hao Nguyen <nvhaohg95@gmail.com>
2023-02-07 17:43:36 +0700

    321

--- projects/codx-sm/src/lib/codx-sm.service.ts
+++ projects/codx-sm/src/lib/codx-sm.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { ApiHttpService, CacheService } from 'codx-core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CodxSmService {
+  constructor(private cache: CacheService, private api: ApiHttpService) {}
 
-  constructor() { }
+  loadData(assemblyName: any, className: any, methodName: any, data: any) {
+    return this.api.exec(assemblyName, className, methodName, data);
+  }
+  addData(assemblyName: any, className: any, methodName: any, data: any) {
+    return this.api.exec(assemblyName, className, methodName, data);
+  }
+  checkDataContactAddress(
+    assemblyName: any,
+    className: any,
+    methodName: any,
+    data: any
+  ) {
+    return this.api.exec(assemblyName, className, methodName, data);
+  }
 }
