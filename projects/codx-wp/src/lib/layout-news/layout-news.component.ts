c94273f3563edff1449877c8d30e8a0c40fe3a22
vvquang <vvquanglacviet.com.vn>
2023-02-07 15:45:44 +0700

    ...

--- projects/codx-wp/src/lib/layout-news/layout-news.component.ts
+++ projects/codx-wp/src/lib/layout-news/layout-news.component.ts
@@ -11,26 +11,25 @@ export class LayoutNewsComponent extends LayoutBaseComponent {
   override toolbar = false;
   override aside = false;
   override asideFixed = false;
+  override asideKeepActive = false;
   valueList: [];
-  category:string = "home";
-  funcID:string | null = "";
-  userPermission:any = null;
- 
+  category: string = 'home';
+  funcID: string | null = '';
+  userPermission: any = null;
 
   constructor(
     private route: ActivatedRoute,
     private cache: CacheService,
-    private dt:ChangeDetectorRef,
-    private api:ApiHttpService,
+    private dt: ChangeDetectorRef,
+    private api: ApiHttpService,
 
     inject: Injector
   ) {
     super(inject);
-    
   }
 
   onInit(): void {
-    this.getUserPermission("WPT022");
+    this.getUserPermission('WPT022');
     this.cache.valueList('WP002').subscribe((value) => {
       this.valueList = value.datas;
     });
@@ -38,28 +37,38 @@ export class LayoutNewsComponent extends LayoutBaseComponent {
 
   onAfterViewInit(): void {}
 
-  getUserPermission(funcID:string){
-    if(funcID){
-      this.api.execSv("SYS","ERM.Business.SYS","CommonBusiness","GetUserPermissionsAsync",[funcID])
-      .subscribe((res:any) => {
-        if(res){
-          this.userPermission = res;
-          this.dt.detectChanges();
-        }
-      });
+  getUserPermission(funcID: string) {
+    if (funcID) {
+      this.api
+        .execSv(
+          'SYS',
+          'ERM.Business.SYS',
+          'CommonBusiness',
+          'GetUserPermissionsAsync',
+          [funcID]
+        )
+        .subscribe((res: any) => {
+          if (res) {
+            this.userPermission = res;
+            this.dt.detectChanges();
+          }
+        });
     }
   }
   navigate(category, funcID) {
     this.category = category;
-    switch(category){
-      case "approvals":
+    switch (category) {
+      case 'approvals':
         this.codxService.navigate(funcID);
         break;
-      case "settings":
-        this.codxService.navigate('','wp/news/settings/'+funcID);
+      case 'settings':
+        this.codxService.navigate('', 'wp/news/settings/' + funcID);
         break;
       default:
-        this.codxService.navigate('','wp/news/'+funcID+'/'+this.category);
+        this.codxService.navigate(
+          '',
+          'wp/news/' + funcID + '/' + this.category
+        );
         break;
     }
     this.dt.detectChanges();
