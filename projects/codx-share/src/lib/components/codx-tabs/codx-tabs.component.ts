2befb64dc65d10544c036fff73f969fd217cdb66
Vo Thao <vthao@lacviet.com.vn>
2023-02-09 17:23:20 +0700

    check

--- projects/codx-share/src/lib/components/codx-tabs/codx-tabs.component.ts
+++ projects/codx-share/src/lib/components/codx-tabs/codx-tabs.component.ts
@@ -42,7 +42,7 @@ export class CodxTabsComponent implements OnInit {
   //ApprovalProcess
   @Input() transID: string;
   @Input() approveStatus: string;
-  @Input() referType: string;
+  @Input() referType: string =""; //de mac định the any moi luu dc file cho task dung-VTHAO sua ngay 9/2/2023
   private all: TabModel[] = [
     { name: 'History', textDefault: 'Lịch sử', isActive: true },
     { name: 'Attachment', textDefault: 'Đính kèm', isActive: false },
