b7221f1eedc4bad48fc16e75d3bb168e33b32f82
Vo Thao <vthao@lacviet.com.vn>
2023-02-14 11:07:28 +0700

    check

---
+++ projects/codx-share/src/lib/components/codx-history-temp/codx-history-temp.component.ts
@@ -0,0 +1,68 @@
+import { ChangeDetectorRef, Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { ApiHttpService, CallFuncService, DialogModel } from 'codx-core';
+
+@Component({
+  selector: 'lib-codx-history-temp',
+  templateUrl: './codx-history-temp.component.html',
+  styleUrls: ['./codx-history-temp.component.css']
+})
+export class CodxHistoryTempComponent implements OnInit {
+
+  @Input() objectID: string = null;
+  @Input() zIndex: number = 0; // Thảo truyền z index
+  @Input() openViewPopup = true ;// Thảo truyền ko cho click
+  lstData: any[] = [];
+  dVll: any = {};
+
+  countData: number = 0;
+  @ViewChild('tmpListItem') tmpListItem: TemplateRef<any>;
+  constructor(
+    private api: ApiHttpService,
+    private dt: ChangeDetectorRef,
+    private callFC: CallFuncService
+  ) {}
+
+  ngOnInit(): void {
+    this.getDataAsync(this.objectID);
+  }
+
+  getDataAsync(pObjectID: string) {
+    if (pObjectID) {
+      this.api
+        .execSv(
+          "BG",
+          "ERM.Business.BG",
+          "CommentLogsBusiness",
+          'GetTotalCommentsAsync',
+          [pObjectID]
+        )
+        .subscribe((res: number) => {
+          if (res) {
+            this.countData = res;
+          }
+        });
+    }
+  }
+
+  openPopup() {
+    if (this.tmpListItem) {
+      let option = new DialogModel();
+      if (this.zIndex > 0) option.zIndex = this.zIndex;
+      let popup = this.callFC.openForm(
+        this.tmpListItem,
+        '',
+        400,
+        500,
+        '',
+        null,
+        '',
+        option
+      );
+      popup.closed.subscribe((res: any) => {
+        if (res) {
+          this.getDataAsync(this.objectID);
+        }
+      });
+    }
+  }
+}
