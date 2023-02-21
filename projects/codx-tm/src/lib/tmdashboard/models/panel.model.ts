a5fd960195c83580d5891659257c504c3c28e8c4
Nguyễn Ngọc Phú Sỹ <phusy9a11516@gmail.com>
2023-02-21 16:11:08 +0700

    ...

---
+++ projects/codx-tm/src/lib/tmdashboard/models/panel.model.ts
@@ -0,0 +1,16 @@
+export class Panel {
+  id: string;
+  row: number;
+  col: number;
+  sizeX: number;
+  sizeY: number;
+  minSizeX: number;
+  minSizeY: number;
+  maxSizeX: number;
+  maxSizeY: number;
+}
+
+export class PanelData {
+  panelId: string;
+  data: any;
+}
