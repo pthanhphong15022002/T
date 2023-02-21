8aef0edca3c7d0539b940ba827dae4c87081eaf0
ntbvan <ntbvan@lacviet.com.vn>
2023-02-21 14:31:15 +0700

    css

--- projects/codx-dp/src/lib/_layout/layout.component.ts
+++ projects/codx-dp/src/lib/_layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, Injector, OnInit } from '@angular/core';
+import { Component, Injector, OnInit, ViewEncapsulation } from '@angular/core';
 import { CallFuncService, DialogRef, LayoutBaseComponent, SidebarModel } from 'codx-core';
 import { NoteDrawerComponent } from 'projects/codx-share/src/lib/layout/drawers/note-drawer/note-drawer.component';
 import { CodxShareService } from 'projects/codx-share/src/public-api';
@@ -6,13 +6,14 @@ import { CodxShareService } from 'projects/codx-share/src/public-api';
 @Component({
   selector: 'lib-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class LayoutComponent extends LayoutBaseComponent {
   module = "DP";
   dialog!: DialogRef;
- // override aside = false;
- // override toolbarFixed = false;
+  // override aside = false;
+  // override toolbarFixed = false;
   constructor(
     inject: Injector,
     private codxShareService: CodxShareService,
