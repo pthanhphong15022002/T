0c9ee0a5066fa47d0559657bbecf41c4ba02c16d
ndhoa <ndhoa@lacviet.com.vn>
2023-02-21 17:00:14 +0700

    jSalary + pageIndex pdf

--- projects/codx-hr/src/lib/employee-list/employee-detail/employee-detail.component.ts
+++ projects/codx-hr/src/lib/employee-list/employee-detail/employee-detail.component.ts
@@ -231,6 +231,7 @@ export class EmployeeDetailComponent extends UIComponent {
   benefitColumnGrid;
   dayoffColumnGrid;
   appointionColumnGrid;
+  jobSalaryColumnGrid;
   //#endregion
 
   filterByBenefitIDArr: any = [];
@@ -241,6 +242,7 @@ export class EmployeeDetailComponent extends UIComponent {
   ViewAllEBenefitFlag = false;
   ViewAllEskillFlag = false;
   ViewAllEBasicSalaryFlag = false;
+  ViewAllEJobSalaryFlag = false;
   ops = ['y'];
 
   //#region filter variables of form main eAssets
@@ -330,6 +332,16 @@ export class EmployeeDetailComponent extends UIComponent {
   templateDayOffGridCol2: TemplateRef<any>;
   @ViewChild('templateDayOffGridCol3', { static: true })
   templateDayOffGridCol3: TemplateRef<any>;
+
+  // Lương chức danh
+  @ViewChild('jobSalaryCol1', { static: true })
+  jobSalaryCol1: TemplateRef<any>;
+  @ViewChild('jobSalaryCol2', { static: true })
+  jobSalaryCol2: TemplateRef<any>;
+  @ViewChild('jobSalaryCol3', { static: true })
+  jobSalaryCol3: TemplateRef<any>;
+  @ViewChild('jobSalaryCol4', { static: true })
+  jobSalaryCol4: TemplateRef<any>;
   //#endregion
 
   //#region gridView viewChild
@@ -338,53 +350,9 @@ export class EmployeeDetailComponent extends UIComponent {
   @ViewChild('workPermitGridview') workPermitGridview: CodxGridviewComponent;
   @ViewChild('basicSalaryGridview') basicSalaryGridview: CodxGridviewComponent;
   @ViewChild('appointionGridView') appointionGridView: CodxGridviewComponent;
-  //#endregion
+  @ViewChild('jobSalaryGridview') jobSalaryGridview: CodxGridviewComponent;
 
   //#endregion
-  // objCollapes = {
-  //   '1': false,
-  //   '1.1': false,
-  //   '1.2': false,
-  //   '2': false,
-  //   '2.1': false,
-  //   '2.2': false,
-  //   '3': false,
-  //   '3.1': false,
-  //   '3.2': false,
-  //   '3.3': false,
-  //   '3.4': false,
-  //   '4': false,
-  //   '4.1': false,
-  //   '4.2': false,
-  //   '4.3': false,
-  //   '4.4': false,
-  //   '4.5': false,
-  //   '5': false,
-  //   '5.1': false,
-  //   '5.2': false,
-  //   '5.3': false,
-  //   '5.4': false,
-  //   '6': false,
-  //   '6.1': false,
-  //   '6.2': false,
-  //   '7': false,
-  //   '7.1': false,
-  //   '7.2': false,
-  //   '7.3': false,
-  //   '7.4': false,
-  // };
-
-  // vllTabs = [
-  //   { icon: 'icon-apartment', text: 'Thông tin cá nhân' },
-  //   { icon: 'icon-apartment', text: 'Thông tin công việc' },
-  //   { icon: 'icon-apartment', text: 'Phúc Lợi' },
-  //   { icon: 'icon-apartment', text: 'Quá trình nhân sự' },
-  //   { icon: 'icon-apartment', text: 'Kiến thức' },
-  //   { icon: 'icon-apartment', text: 'Khen thưởng' },
-  //   { icon: 'icon-apartment', text: 'Kỷ luật' },
-  //   { icon: 'icon-apartment', text: 'Sức khỏe' },
-  //   { icon: 'icon-apartment', text: 'Thôi việc' },
-  // ];
 
   listEmp: any;
   request: DataRequest;
@@ -422,6 +390,7 @@ export class EmployeeDetailComponent extends UIComponent {
   eHealthRowCount = 0;
   eVaccineRowCount = 0;
   appointionRowCount;
+  eJobSalaryRowCount;
   //#endregion
 
   //#region var functionID
@@ -469,6 +438,7 @@ export class EmployeeDetailComponent extends UIComponent {
   eVaccineFormModel: FormModel; //Tiêm vắc xin
   appointionFormModel: FormModel;
   dayofFormModel: FormModel;
+  eJobSalaryFormModel: FormModel; // Lương chức danh
   //#endregion
 
   //#region headerText
@@ -484,6 +454,7 @@ export class EmployeeDetailComponent extends UIComponent {
   eHealthHeaderText;
   eVaccineHeaderText;
   appointionHeaderTexts;
+  eJobSalaryHeaderText;
   //#endregion
 
   //#region headerTextString
@@ -541,10 +512,6 @@ export class EmployeeDetailComponent extends UIComponent {
 
     this.hrService.getFormModel(this.eWorkPermitFuncID).then((res) => {
       this.eWorkPermitFormModel = res;
-    });
-
-    this.hrService.getFormModel(this.degreeFuncID).then((res) => {
-      this.degreeFormModel = res;
     });
 
     this.hrService.getFormModel(this.eExperienceFuncID).then((res) => {
@@ -639,6 +606,10 @@ export class EmployeeDetailComponent extends UIComponent {
 
     this.hrService.getFormModel(this.appointionFuncID).then((res) => {
       this.appointionFormModel = res;
+    });
+
+    this.hrService.getFormModel(this.eJobSalFuntionID).then((res) => {
+      this.eJobSalaryFormModel = res;
     });
     //#endregion
 
@@ -1373,6 +1344,44 @@ export class EmployeeDetailComponent extends UIComponent {
           }
         });
         this.appointionRowCount = this.appointionGridView.dataService.rowCount;
+      }
+    }, 100);
+    //#endregion
+
+    //#region get columnGrid EJobSalary - Lương chức danh
+    this.hrService.getHeaderText(this.eJobSalFuntionID).then((res) => {
+      this.eJobSalaryHeaderText = res;
+      this.jobSalaryColumnGrid = [
+        {
+          headerText: this.eJobSalaryHeaderText['JSalary'] ?? '' + 'Mức lương',
+          template: this.jobSalaryCol1,
+          width: '150',
+        },
+        {
+          headerText: this.eJobSalaryHeaderText['EffectedDate'],
+          template: this.jobSalaryCol2,
+          width: '150',
+        },
+        {
+          headerText: this.eJobSalaryHeaderText['DecisionNo'] + ' | ' + this.eJobSalaryHeaderText['SignedDate'],
+          template: this.jobSalaryCol3,
+          width: '300',
+        },
+      ];
+    });
+
+    let insJSalary = setInterval(() => {
+      if (this.jobSalaryGridview) {
+        clearInterval(insJSalary);
+        let t = this;
+        this.jobSalaryGridview.dataService.onAction.subscribe((res) => {
+          if (res) {
+            if (res.type != null && res.type == 'loaded') {
+              t.eJobSalaryRowCount = res['data'].length;
+            }
+          }
+        });
+        this.eJobSalaryRowCount = this.jobSalaryGridview.dataService.rowCount;
       }
     }, 100);
     //#endregion
@@ -2809,19 +2818,17 @@ export class EmployeeDetailComponent extends UIComponent {
     actionType: string,
     data: any
   ) {
-    this.view.dataService.dataSelected = this.infoPersonal;
     let option = new SidebarModel();
-    // option.FormModel = this.view.formModel
-    option.Width = '850px';
+    option.FormModel = this.eJobSalaryFormModel
+    option.Width = '550px';
     let dialogAdd = this.callfunc.openSide(
       PopupEJobSalariesComponent,
       {
         actionType: actionType,
-        headerText: actionHeaderText + ' ' + this.getFormHeader('HRTEM0402'),
+        headerText: actionHeaderText + ' ' + this.getFormHeader(this.eJobSalFuntionID),
         employeeId: this.employeeID,
-        funcID: 'HRTEM0402',
-        lstJobSalaries: this.lstJobSalaries,
-        indexSelected: this.lstJobSalaries.indexOf(data),
+        funcID: this.eJobSalFuntionID,
+        jobSalaryObj: data,
       },
       option
     );
@@ -3994,6 +4001,22 @@ export class EmployeeDetailComponent extends UIComponent {
         });
         this.eBasicSalaryRowCount =
           this.basicSalaryGridview.dataService.rowCount;
+      }
+    }, 100);
+  }
+  valueChangeViewAllEJobSalary(evt) {
+    this.ViewAllEJobSalaryFlag = evt.data;
+    let ins = setInterval(() => {
+      if (this.jobSalaryGridview) {
+        clearInterval(ins);
+        let t = this;
+        this.jobSalaryGridview.dataService.onAction.subscribe((res) => {
+          if (res?.type == 'loaded') {
+            t.eJobSalaryRowCount = res['data'].length;
+          }
+        });
+        this.eJobSalaryRowCount =
+          this.jobSalaryGridview.dataService.rowCount;
       }
     }, 100);
   }
