789b1db7cc0d21804fc3db38c1d06bb84a5cba2d
ndhoa <ndhoa@lacviet.com.vn>
2023-02-22 10:42:03 +0700

    cancel for bookingRoom

--- projects/codx-ep/src/lib/car/booking/booking-car.component.ts
+++ projects/codx-ep/src/lib/car/booking/booking-car.component.ts
@@ -379,7 +379,7 @@ export class BookingCarComponent extends UIComponent implements AfterViewInit {
     }
   }
   cancel(data: any) {
-    this.codxEpService.cancel(data?.recID).subscribe((res: any) => {
+    this.codxEpService.cancel(data?.recID, '', this.formModel.entityName).subscribe((res: any) => {
       if (res != null) {
         this.notificationsService.notifyCode('SYS034'); //đã hủy gửi duyệt
         data.approveStatus = '1';
