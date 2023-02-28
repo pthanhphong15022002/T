fef11316d041fc2ada50481ec25fc368672d818c
Nguyễn Ngọc Phú Sỹ <phusy9a11516@gmail.com>
2023-02-28 16:38:03 +0700

    ...

--- projects/codx-tm/src/lib/tmdashboard/dashboard-content/dashboard-content.component.ts
+++ projects/codx-tm/src/lib/tmdashboard/dashboard-content/dashboard-content.component.ts
@@ -17,7 +17,7 @@ import {
 } from '@syncfusion/ej2-angular-treemap';
 import { UIComponent } from 'codx-core';
 import { BI_Charts, ChartSettings } from '../models/chart.model';
-import { Panel, PanelData } from '../models/panel.model';
+import { Panel, PanelOrder } from '../models/panel.model';
 import { RangeColorModel } from '@syncfusion/ej2-angular-progressbar';
 
 @Component({
@@ -37,7 +37,7 @@ export class DashboardContentComponent
   dashboard = [];
 
   panels: Panel[] = [];
-  datas: PanelData[] = [];
+  datas: PanelOrder[] = [];
 
   // chartSettings2: ChartSettings = {
   //   title: 'Tỷ lệ công việc được giao',
@@ -352,31 +352,74 @@ export class DashboardContentComponent
     ],
   };
 
-  constructor(private inject: Injector, private dbSerivce: TMDashboardService) {
+  constructor(private inject: Injector, private biSerivce: TMDashboardService) {
     super(inject);
     this.reportID = this.router.snapshot.params['reportID'];
-  }
-
-  onInit(): void {
     this.codxService.reloadComponent();
   }
 
+  onInit(): void {}
+
   ngAfterViewInit(): void {
     this.panels = JSON.parse(
-      '[{"id":"0.5293183694893755_layout","row":14,"col":26,"sizeX":22,"sizeY":6,"minSizeX":1,"minSizeY":1,"maxSizeX":null,"maxSizeY":null},{"id":"0.7462735198778399_layout","row":14,"col":0,"sizeX":26,"sizeY":6,"minSizeX":1,"minSizeY":1,"maxSizeX":null,"maxSizeY":null},{"id":"0.932033280876619_layout","row":4,"col":17,"sizeX":16,"sizeY":10,"minSizeX":1,"minSizeY":1,"maxSizeX":null,"maxSizeY":null},{"id":"0.6076202141678433_layout","row":4,"col":33,"sizeX":15,"sizeY":10,"minSizeX":1,"minSizeY":1,"maxSizeX":null,"maxSizeY":null},{"id":"0.5296463903570827_layout","row":4,"col":0,"sizeX":17,"sizeY":10,"minSizeX":1,"minSizeY":1,"maxSizeX":null,"maxSizeY":null},{"id":"0.18375208371519292_layout","row":0,"col":0,"sizeX":48,"sizeY":4,"minSizeX":1,"minSizeY":1,"maxSizeX":null,"maxSizeY":null}]'
+      '[{"id":"dfeb663a-185a-4fef-85b0-25613979133d","row":4,"col":0,"sizeX":17,"sizeY":10,"minSizeX":1,"minSizeY":1,"maxSizeX":null,"maxSizeY":null},{"id":"30a29412-9253-4033-a5e4-617246359357","row":4,"col":33,"sizeX":15,"sizeY":10,"minSizeX":1,"minSizeY":1,"maxSizeX":null,"maxSizeY":null},{"id":"fe57aaa2-4606-49a0-a8da-de9f0b190ef7","row":4,"col":17,"sizeX":16,"sizeY":10,"minSizeX":1,"minSizeY":1,"maxSizeX":null,"maxSizeY":null},{"id":"002dce4b-87c8-4c07-8c58-ed65a84e08ae","row":14,"col":0,"sizeX":26,"sizeY":6,"minSizeX":1,"minSizeY":1,"maxSizeX":null,"maxSizeY":null},{"id":"5abdc8fe-de06-4c77-81c0-f1d05128ec2f","row":14,"col":26,"sizeX":22,"sizeY":6,"minSizeX":1,"minSizeY":1,"maxSizeX":null,"maxSizeY":null},{"id":"0.9435262123249284_layout","row":0,"col":0,"sizeX":8,"sizeY":4,"minSizeX":8,"minSizeY":4,"maxSizeX":null,"maxSizeY":null},{"id":"0.31200119842251084_layout","row":0,"col":8,"sizeX":8,"sizeY":4,"minSizeX":8,"minSizeY":4,"maxSizeX":null,"maxSizeY":null},{"id":"0.5547337482348278_layout","row":0,"col":16,"sizeX":8,"sizeY":4,"minSizeX":8,"minSizeY":4,"maxSizeX":null,"maxSizeY":null},{"id":"0.4533692938771181_layout","row":0,"col":24,"sizeX":8,"sizeY":4,"minSizeX":8,"minSizeY":4,"maxSizeX":null,"maxSizeY":null},{"id":"0.23559424577365373_layout","row":0,"col":32,"sizeX":8,"sizeY":4,"minSizeX":8,"minSizeY":4,"maxSizeX":null,"maxSizeY":null},{"id":"0.5314304532149856_layout","row":0,"col":40,"sizeX":8,"sizeY":4,"minSizeX":8,"minSizeY":4,"maxSizeX":null,"maxSizeY":null}]'
     );
     this.datas = JSON.parse(
-      '[{"panelId":"0.5293183694893755_layout","data":"6"},{"panelId":"0.7462735198778399_layout","data":"5"},{"panelId":"0.932033280876619_layout","data":"4"},{"panelId":"0.6076202141678433_layout","data":"3"},{"panelId":"0.5296463903570827_layout","data":"2"},{"panelId":"0.18375208371519292_layout","data":"1"}]'
+      '[{"panelId":"dfeb663a-185a-4fef-85b0-25613979133d","data":"1"},{"panelId":"30a29412-9253-4033-a5e4-617246359357","data":"2"},{"panelId":"fe57aaa2-4606-49a0-a8da-de9f0b190ef7","data":"3"},{"panelId":"002dce4b-87c8-4c07-8c58-ed65a84e08ae","data":"4"},{"panelId":"5abdc8fe-de06-4c77-81c0-f1d05128ec2f","data":"5"},{"panelId":"0.9435262123249284_layout","data":"6"},{"panelId":"0.31200119842251084_layout","data":"7"},{"panelId":"0.5547337482348278_layout","data":"8"},{"panelId":"0.4533692938771181_layout","data":"9"},{"panelId":"0.23559424577365373_layout","data":"10"},{"panelId":"0.5314304532149856_layout","data":"11"}]'
     );
 
-    this.dbSerivce.loadCharts(this.reportID).subscribe((res: BI_Charts[]) => {
+    this.biSerivce.loadCharts(this.reportID).subscribe((res: BI_Charts[]) => {
       if (res) {
-        res.map((item: BI_Charts) => {
-          this.panels.push(JSON.parse(item.location));
+        res.map((item: BI_Charts, index: number) => {
+          let data: PanelOrder = {
+            panelId: item.recID,
+            data: (index + 1).toString(),
+          };
+
+          this.dataSource.push(data);
+
+          if (item.location) {
+            let location = JSON.parse(item.location);
+            let panel: Panel = {
+              id: item.recID,
+              row: location['row'],
+              col: location['col'],
+              sizeX: location['sizeX'],
+              sizeY: location['sizeY'],
+              minSizeX: location['minSizeX'],
+              minSizeY: location['minSizeY'],
+              maxSizeX: location['maxSizeX'],
+              maxSizeY: location['maxSizeY'],
+            };
+
+            this.panels.push(panel);
+          }
+
+          // item.location = JSON.stringify({
+          //   row: 14,
+          //   col: 26,
+          //   sizeX: 22,
+          //   sizeY: 6,
+          //   minSizeX: 1,
+          //   minSizeY: 1,
+          //   maxSizeX: null,
+          //   maxSizeY: null,
+          // });
+          // let location = JSON.parse(item.location);
+          // item.recID;
+          // let panel = {
+          //   id: item.recID,
+          //   row: location['row'],
+          //   col: location['col'],
+          //   sizeX: location['sizeX'],
+          //   sizeY: location['sizeY'],
+          //   minSizeX: location['minSizeX'],
+          //   minSizeY: location['minSizeY'],
+          //   maxSizeX: location['maxSizeX'],
+          //   maxSizeY: location['maxSizeY'],
+          // };
         });
       }
     });
-
-    this.detectorRef.detectChanges();
   }
 }
