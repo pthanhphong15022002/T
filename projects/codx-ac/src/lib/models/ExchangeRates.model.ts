771c168e829ff42598ad31254ed1b23a90d2caf6
tantm <tmtan@lacviet.com.vn>
2023-03-06 13:16:33 +0700

    fixbug

--- projects/codx-ac/src/lib/models/ExchangeRates.model.ts
+++ projects/codx-ac/src/lib/models/ExchangeRates.model.ts
@@ -7,8 +7,20 @@ export class ExchangeRates {
   modifiedBy: any;
   modifiedOn: any;
   note: any = '';
-  recID: any;
+  recID: any = Guid.newGuid();
   sourceType: any = '';
-  toDate: any = '';
+  toDate: any;
   toTime: any;
+}
+class Guid {
+  static newGuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+      /[xy]/g,
+      function (c) {
+        var r = (Math.random() * 16) | 0,
+          v = c == 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }
+    );
+  }
 }
