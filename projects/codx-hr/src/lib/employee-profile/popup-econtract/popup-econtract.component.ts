d74354f64c19e29c4908afaf7893c5117bf31b09
ndhoa <ndhoa@lacviet.com.vn>
2023-03-09 18:15:12 +0700

    .

--- projects/codx-hr/src/lib/employee-profile/popup-econtract/popup-econtract.component.ts
+++ projects/codx-hr/src/lib/employee-profile/popup-econtract/popup-econtract.component.ts
@@ -162,11 +162,30 @@ export class PopupEContractComponent extends UIComponent implements OnInit {
     if (this.actionType == 'add' || this.actionType == 'copy') {
       this.hrSevice.validateBeforeSaveContract(this.data, true).subscribe((res) => {
         console.log('result', res);
-        if (res && res[0]) {
-          //code test
-          this.notify.notifyCode('SYS006');
-          this.dialog && this.dialog.close(res);
-          this.data = res;
+        if (res) {
+          if(res[0]){
+            //code test
+            this.notify.notifyCode('SYS006');
+            this.dialog && this.dialog.close(res);
+            this.data = res;
+          }
+          else if(res[1]){
+            this.notify.alertCode(res[1]).subscribe(stt =>{
+              console.log('click', res);
+              if(stt?.event.status == 'Y'){
+                if(res[1] == 'HR010'){
+                  this.hrSevice.addEContract(this.data).subscribe(result =>{
+                    if(result && result[0]){
+                      this.notify.notifyCode('SYS006');
+                      this.dialog && this.dialog.close(result);
+                    }
+                  })
+                }
+              }
+              
+            })
+          }
+         
         }
       });
     } else if (this.actionType == 'edit') {
@@ -198,13 +217,13 @@ export class PopupEContractComponent extends UIComponent implements OnInit {
           this.data.limitMonths =
             event?.component?.itemsSelected[0]?.LimitMonths;
           this.formGroup.patchValue({ limitMonths: this.data.limitMonths });
-          this.setExpiredDate();
+          this.setExpiredDate(this.data.limitMonths);
           break;
         }
         case 'effectedDate': {
           this.data.effectedDate = event.data;
           this.formGroup.patchValue({ effectedDate: this.data.effectedDate });
-          this.setExpiredDate();
+          this.setExpiredDate(this.data.limitMonths);
           break;
         }
         case 'signerID': {
@@ -237,10 +256,10 @@ export class PopupEContractComponent extends UIComponent implements OnInit {
     }
   }
 
-  setExpiredDate() {
+  setExpiredDate(month) {
     if (this.data.effectedDate) {
       let date = new Date(this.data.effectedDate);
-      this.data.expiredDate = new Date(date.setMonth(date.getMonth() + 14));
+      this.data.expiredDate = new Date(date.setMonth(date.getMonth() + month));
       this.formGroup.patchValue({ expiredDate: this.data.expiredDate });
       this.cr.detectChanges();
     }
