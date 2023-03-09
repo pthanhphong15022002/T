761a196e540a0c1179b5602bb5b08ab97aa0518c
lvietbao <lvietbao@lacviet.com.vn>
2023-03-10 01:23:44 +0700

    push fix count instance

--- projects/codx-dp/src/lib/dynamic-process/dynamic-process.component.ts
+++ projects/codx-dp/src/lib/dynamic-process/dynamic-process.component.ts
@@ -227,7 +227,7 @@ export class DynamicProcessComponent
       this.dialog.closed.subscribe((e) => {
         if (!e?.event) this.view.dataService.clear();
         if (e && e.event != null) {
-          e.event.totalInstance =  this.totalInstance;
+          e.event.totalInstance = this.totalInstance;
           this.view.dataService.update(e.event).subscribe();
           this.changeDetectorRef.detectChanges();
         }
@@ -312,7 +312,7 @@ export class DynamicProcessComponent
         dialogProcessCopy.closed.subscribe((e) => {
           if (!e?.event) this.view.dataService.clear();
           if (e && e.event != null) {
-            e.event.totalInstance =  this.totalInstance;
+            e.event.totalInstance = this.totalInstance;
             this.view.dataService.update(e.event).subscribe();
             this.changeDetectorRef.detectChanges();
           }
@@ -341,7 +341,9 @@ export class DynamicProcessComponent
     if ($event && $event.currentTarget.checked) {
       this.listClickedCoppy.push(data);
       if (data.id === '3') {
-        this.listClickedCoppy= this.listClickedCoppy.concat(this.listSelectStepCoppy);
+        this.listClickedCoppy = this.listClickedCoppy.concat(
+          this.listSelectStepCoppy
+        );
       }
     } else {
       if (data.id === '3') {
@@ -641,8 +643,9 @@ export class DynamicProcessComponent
       : '';
   }
 
-  totalSteps(listStep:any){
-    return listStep.steps?.filter(x=> !x.isSuccessStep && !x.isFailStep).length;
+  totalSteps(listStep: any) {
+    return listStep.steps?.filter((x) => !x.isSuccessStep && !x.isFailStep)
+      .length;
   }
 
   //#endregion đang test
@@ -675,12 +678,29 @@ export class DynamicProcessComponent
 
     dialog.closed.subscribe((e) => {
       if (e?.event && e?.event != null) {
-        this.view.dataService.update(e?.event).subscribe();
-        this.detectorRef.detectChanges();
+        this.view.dataService.update(e?.event[0]).subscribe();
+        let totalInstanceById = e?.event[1];
+        if (totalInstanceById.size > 0 && totalInstanceById) {
+          let listProccess = this.view.dataService.data;
+          var index = 0;
+          for (let i = 0; i < listProccess.length; i++) {
+            let value = totalInstanceById.get(listProccess[i].recID);
+            if (value) {
+              listProccess[i].totalInstance =
+                listProccess[i].totalInstance + value;
+              this.view.dataService
+                .update(listProccess[i].totalInstance)
+                .subscribe();
+              index++;
+            }
+            if (index === totalInstanceById.size) {
+              break;
+            }
+          }
+        }
       }
     });
-
-
+    this.detectorRef.detectChanges();
   }
 
   // nvthuan
@@ -700,7 +720,7 @@ export class DynamicProcessComponent
   }
 
   editName() {
-    if(!this.processName?.trim()){
+    if (!this.processName?.trim()) {
       this.notificationsService.notifyCode('SYS009', 0, 'Tên quy trình');
       return;
     }
