3a89ca8ab27049aa6929fb4298eb386b1094c783
vuhiepp07 <vuhiep2k107@gmail.com>
2023-04-12 18:00:43 +0700

    Re-create and apply new view detail eContracts

--- projects/codx-hr/src/lib/codx-hr.service.ts
+++ projects/codx-hr/src/lib/codx-hr.service.ts
@@ -1555,13 +1555,13 @@ export class CodxHrService {
     );
   }
 
-  loadDataEContract(dataRequest: DataRequest) {
+  loadDataEContract(data) {
     return this.api.execSv<any>(
       'HR',
       'ERM.Business.HR',
       'EContractsBusiness',
       'LoadDataEContractAsync',
-      dataRequest
+      data
     );
   }
 
@@ -1948,6 +1948,7 @@ export class CodxHrService {
   // actionUpdateClosed = 'AU9'
 
   handleShowHideMF(evt, data, view){  
+    debugger
     // Kiem tra document co ap dung quy trinh xet duyet hay khong, neu khong thi hide di 1 so more func
     let category = '4';
     let formName = 'HRParameters';
