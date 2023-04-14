5a5e95abb1f9e6e2ae76597631c70d092f5e79b7
DTLONG <dtlong@lacviet.com.vn>
2023-04-14 17:49:27 +0700

    om

--- projects/codx-ep/src/lib/room/booking/popup-add-booking-room/popup-add-booking-room.component.ts
+++ projects/codx-ep/src/lib/room/booking/popup-add-booking-room/popup-add-booking-room.component.ts
@@ -607,9 +607,7 @@ export class PopupAddBookingRoomComponent extends UIComponent {
   }
 
   beforeSave(option: RequestOption) {
-    debugger;
-    let itemData = this.data;
-    
+    let itemData = this.data;    
     option.methodName = 'AddEditItemAsync';
     option.data = [
       itemData,
