ed02f297551bf151f602e07a31480d9f1e5ba3b0
LocTran <loctt.cntp@gamil.com>
2023-04-19 15:18:38 +0700

    ...

--- projects/codx-share/src/lib/components/treeview-comment/treeview-comment.component.ts
+++ projects/codx-share/src/lib/components/treeview-comment/treeview-comment.component.ts
@@ -67,6 +67,7 @@ export class TreeviewCommentComponent implements OnInit {
     .subscribe((res) => {
       if(Array.isArray(res.datas)) {
         this.vllL1480 = Array.from<any>(res.datas);
+        this.defaulVote = this.vllL1480[0];
         if(this.vllL1480.length > 0){
           this.dVll["0"] = null;
           this.vllL1480.forEach(element => {
@@ -264,5 +265,14 @@ export class TreeviewCommentComponent implements OnInit {
       this.data.totalComment = res;
       this.dt.detectChanges();
     });
+  }
+
+  // doubleclick votes
+  defaulVote:any = null;
+  dbLikePost(data){
+    debugger
+    if(data && this.defaulVote){
+      this.votePost(data,this.defaulVote.value);
+    }
   }
 }
