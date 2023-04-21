9d46f61965b2f1a5f058919a1e5e76327e6d8ddb
chungthai7596 <bachung7596@gmail.com>
2023-04-21 17:21:00 +0700

    .

--- projects/codx-dm/src/lib/codx-dm.service.ts
+++ projects/codx-dm/src/lib/codx-dm.service.ts
@@ -330,6 +330,40 @@ export class CodxDMService {
 
   ngOnInit(): void {}
 
+  loadValuelist(vll:any): Observable<any>
+  {
+    let paras = ["VLL",vll];
+    let keyRoot = "VLL" + vll;
+    let key = JSON.stringify(paras).toLowerCase();
+    if (this.caches.has(keyRoot)) {
+      var c = this.caches.get(keyRoot);
+      if (c && c.has(key)) {
+        return c.get(key);
+      }
+    }
+    else {
+      this.caches.set(keyRoot, new Map<any, any>());
+    }
+    
+    if (this.cachedObservables.has(key)) {
+      this.cachedObservables.get(key)
+    }
+    let observable = this.cache.valueList(vll)
+    .pipe(
+      map((res) => {
+        if (res) {
+          let c = this.caches.get(keyRoot);
+          c?.set(key, res);
+          return res;
+        }
+        return null
+      }),
+      share(),
+      finalize(() => this.cachedObservables.delete(key))
+    );
+    this.cachedObservables.set(key, observable);
+    return observable;
+  }
   //Load GridViewSetup
   loadGridView(formName:any, gridViewName:any): Observable<any>
   {
