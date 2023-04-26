06232079cb78a6fe597f9a0615829c157d42f064
Vo Thao <vthao@lacviet.com.vn>
2023-04-26 15:55:22 +0700

    check tam

--- projects/codx-dp/src/lib/instances/instance-detail/instance-detail.component.ts
+++ projects/codx-dp/src/lib/instances/instance-detail/instance-detail.component.ts
@@ -132,7 +132,7 @@ export class InstanceDetailComponent implements OnInit {
   HTMLProgress = `<div style="font-size:12px;font-weight:bold;color:#005DC7;fill:#005DC7;margin-top: 2px;"><span></span></div>`;
   //gan chart
   vllViewGannt = 'DP042';
-  crrViewGant = 'D';
+  crrViewGant = 'W';
   timelineSettings: any;
   tags = '';
   timelineSettingsHour: any = {
