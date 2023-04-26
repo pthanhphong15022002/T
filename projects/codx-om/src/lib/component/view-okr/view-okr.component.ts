830cc10e76f7e248268a2634da743bf8c8edd006
DTLONG <dtlong@lacviet.com.vn>
2023-04-26 14:22:31 +0700

    fix css

--- projects/codx-om/src/lib/component/view-okr/view-okr.component.ts
+++ projects/codx-om/src/lib/component/view-okr/view-okr.component.ts
@@ -161,4 +161,14 @@ export class ViewOKRComponent extends UIComponent implements AfterViewInit {
     evt.stopPropagation();
     evt.preventDefault();
   }
+  newGuid(): string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+      /[xy]/g,
+      function (c) {
+        var r = (Math.random() * 16) | 0,
+          v = c == 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }
+    );
+  }
 }
