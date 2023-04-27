628bd816489e5e147abbf160c9ccd5e831195fd0
Vo Thao <vthao@lacviet.com.vn>
2023-04-27 11:50:06 +0700

    check

--- projects/codx-dp/src/lib/codx-dp.module.ts
+++ projects/codx-dp/src/lib/codx-dp.module.ts
@@ -52,6 +52,7 @@ import { CodxTaskComponent } from './componnent-task/codx-task/codx-task.compone
 import { UpdateProgressComponent } from './componnent-task/update-progress/update-progress.component';
 import { TestComponent } from './componnent-task/test/test.component';
 import { InputNumberDurationComponent } from './dynamic-process/popup-add-dynamic-process/input-number-duration/input-number-duration.component';
+import { ApprovalsComponent } from './approvals/approvals.component';
 
 
 const routes: Routes = [
@@ -67,6 +68,12 @@ const routes: Routes = [
       {
         path: 'instances/:funcID/:processID',
         component: InstancesComponent,
+        data: { noReuse: true },
+      },
+      //dp/approvals/DPT0501
+      {
+        path: 'approvals/:funcID',
+        component: ApprovalsComponent,
         data: { noReuse: true },
       },
       {
@@ -136,7 +143,8 @@ const T_Component: Type<any>[] = [LayoutComponent];
     UpdateProgressComponent,
     CodxTaskComponent,
     TestComponent,
-    InputNumberDurationComponent
+    InputNumberDurationComponent,
+    ApprovalsComponent
   ],
   imports: [
     RouterModule.forChild(routes),
