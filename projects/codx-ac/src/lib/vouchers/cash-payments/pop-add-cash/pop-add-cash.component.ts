94af7c4010c65ab30657e9998269f70503654da5
nvhao <nvhaohg95@gmail.com>
2023-05-08 16:38:54 +0700

    SettledInvoice

--- projects/codx-ac/src/lib/vouchers/cash-payments/pop-add-cash/pop-add-cash.component.ts
+++ projects/codx-ac/src/lib/vouchers/cash-payments/pop-add-cash/pop-add-cash.component.ts
@@ -1083,7 +1083,6 @@ export class PopAddCashComponent extends UIComponent implements OnInit {
 
   loadformSettledInvoices() {
     var obj = {
-      cbxName: 'OpenInvoices',
       cashpayment: this.cashpayment,
     };
     let opt = new DialogModel();
@@ -1105,7 +1104,9 @@ export class PopAddCashComponent extends UIComponent implements OnInit {
     );
     voucherDialog.closed.subscribe((res) => {
       if (res && res.event && res.event.length) {
-        this.setVoucherRef(res.event);
+       // this.setVoucherRef(res.event);
+       this.settledInvoices = res.event;
+       this.gridSettledInvoices.gridRef.refresh();
       }
     });
   }
