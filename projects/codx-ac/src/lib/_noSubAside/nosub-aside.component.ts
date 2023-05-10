65995cb90ed7f2c72f22e07af7fd2ed51942037d
tantm <tmtan@lacviet.com.vn>
2023-05-10 17:57:48 +0700

    .

--- projects/codx-ac/src/lib/_noSubAside/nosub-aside.component.ts
+++ projects/codx-ac/src/lib/_noSubAside/nosub-aside.component.ts
@@ -1,6 +1,7 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { CallFuncService, LayoutBaseComponent, SidebarModel } from 'codx-core';
 import { NoteDrawerComponent } from 'projects/codx-share/src/lib/layout/drawers/note-drawer/note-drawer.component';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'lib-nosub-aside',
@@ -9,18 +10,24 @@ import { NoteDrawerComponent } from 'projects/codx-share/src/lib/layout/drawers/
 })
 export class NosubAsideComponent extends LayoutBaseComponent {
   // toolbar = false;
+  childMenu = new BehaviorSubject<any>(null);
   constructor(
     inject: Injector,
     private callfc: CallFuncService
   ) { 
     super(inject);
     this.module = 'AC';
+    this.layoutModel.toolbarFixed = true;
   }
 
   onInit(): void { }
 
   onAfterViewInit(): void { }
 
+  childMenuClick(e) {
+    this.childMenu.next(e);
+  }
+  menuClick(e) {}
   openFormNoteDrawer() {
     let option = new SidebarModel();
     option.Width = '550px';
