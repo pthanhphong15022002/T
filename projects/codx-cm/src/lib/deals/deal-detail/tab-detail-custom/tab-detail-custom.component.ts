3106b87f6f340f8c59592ac95212d8eb5eb5ed7b
Phúc <tdnphuck42@gmail.com>
2023-05-10 08:30:58 +0700

    contact in deal

--- projects/codx-cm/src/lib/deals/deal-detail/tab-detail-custom/tab-detail-custom.component.ts
+++ projects/codx-cm/src/lib/deals/deal-detail/tab-detail-custom/tab-detail-custom.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, Injector, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Injector,
+  Input,
+  OnInit,
+} from '@angular/core';
 import { EditSettingsModel } from '@syncfusion/ej2-gantt';
 import { UIComponent, FormModel, SidebarModel } from 'codx-core';
 import { PopupAddCmCustomerComponent } from '../../../cmcustomer/popup-add-cmcustomer/popup-add-cmcustomer.component';
@@ -7,21 +13,23 @@ import { CodxCmService } from '../../../codx-cm.service';
 @Component({
   selector: 'codx-tab-deal-detail',
   templateUrl: './tab-detail-custom.component.html',
-  styleUrls: ['./tab-detail-custom.component.scss']
+  styleUrls: ['./tab-detail-custom.component.scss'],
 })
-export class TabDetailCustomComponent extends UIComponent
-implements OnInit, AfterViewInit {
+export class TabDetailCustomComponent
+  extends UIComponent
+  implements OnInit, AfterViewInit
+{
   @Input() tabClicked: any;
   @Input() data: any;
-
+  @Input() tmpDataSelect: any;
   titleAction: string = '';
   listStep = [];
-  isUpdate = true //xư lý cho edit trung tuy chinh ko
-  listStepsProcess = [] ;
+  isUpdate = true; //xư lý cho edit trung tuy chinh ko
+  listStepsProcess = [];
   // titleDefault= "Trường tùy chỉnh"//truyen vay da
   readonly tabInformation: string = 'Information';
   readonly tabField: string = 'Field';
-  readonly tabContact: string ='Contact';
+  readonly tabContact: string = 'Contact';
   readonly tabOpponent: string = 'Opponent';
   readonly tabTask: string = 'Task';
   readonly tabProduct: string = 'Product';
@@ -37,31 +45,28 @@ implements OnInit, AfterViewInit {
     allowDeleting: true,
   };
 
-  constructor(
-    private inject: Injector,
-    private cmService: CodxCmService,
-  ) {
+  constructor(private inject: Injector, private cmService: CodxCmService) {
     super(inject);
   }
   ngAfterViewInit() {}
   onInit(): void {
-   this.getListInstanceStep();
+    this.getListInstanceStep();
   }
 
-  getListInstanceStep(){
+  getListInstanceStep() {
     let instanceID = this.data?.processID;
-    if(instanceID){
-      this.cmService.getStepInstance([instanceID]).subscribe(res =>{
-        this.listStep = res;    
-      })
+    if (instanceID) {
+      this.cmService.getStepInstance([instanceID]).subscribe((res) => {
+        this.listStep = res;
+      });
     }
   }
 
-  addContact(){
+  addContact() {
     var contact = 'CM0103'; // contact
     this.cache.functionList(contact).subscribe((fun) => {
       let option = new SidebarModel();
-     // option.DataService = this.view.dataService;
+      // option.DataService = this.view.dataService;
       var formMD = new FormModel();
       formMD.entityName = fun.entityName;
       formMD.formName = fun.formName;
@@ -70,14 +75,14 @@ implements OnInit, AfterViewInit {
       option.FormModel = JSON.parse(JSON.stringify(formMD));
       option.Width = '800px';
       option.DataService = null;
-      this.titleAction = ' Bao test'
+      this.titleAction = ' Bao test';
       var dialog = this.callfc.openSide(
         PopupAddCmCustomerComponent,
         ['add', this.titleAction],
         option
       );
       dialog.closed.subscribe((e) => {
-  //      if (!e?.event) this.view.dataService.clear();
+        //      if (!e?.event) this.view.dataService.clear();
         // if (e && e.event != null) {
         //   this.customerDetail.listTab(this.funcID);
         // }
@@ -85,7 +90,7 @@ implements OnInit, AfterViewInit {
     });
   }
 
-  //truong tuy chinh - đang cho bằng 1 
+  //truong tuy chinh - đang cho bằng 1
   showColumnControl(stepID) {
     if (this.listStepsProcess?.length > 0) {
       var idx = this.listStepsProcess.findIndex((x) => x.recID == stepID);
