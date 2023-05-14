b2e8223b52f1af5a9843d74f6e8d00bca8c58f99
chungthai7596 <bachung7596@gmail.com>
2023-05-14 10:14:49 +0700

    .

--- projects/codx-share/src/lib/components/attachment/attachment.component.ts
+++ projects/codx-share/src/lib/components/attachment/attachment.component.ts
@@ -730,7 +730,6 @@ export class AttachmentComponent implements OnInit, OnChanges {
       var tenants = from(check);
       return tenants.pipe(
         mergeMap((value : any, i) => { 
-          debugger
           if(typeof value == 'object' && value.AppId)
           {
             return from(this.fileService.getTotalHdd()).pipe(mergeMap(hdd=>{
@@ -3225,7 +3224,8 @@ export class AttachmentComponent implements OnInit, OnChanges {
         // var arrName = files[i].name.split(".");
         // arrName.splice((arrName.length - 1), 1);
         // var name = arrName.join('.');
-        
+        debugger
+        fileUpload.entityName = this.formModel.entityName
         fileUpload.mimeType = this.GetMimeType(files[i].type);
         fileUpload.type = files[i].type;
         fileUpload.objectType = this.objectType;
