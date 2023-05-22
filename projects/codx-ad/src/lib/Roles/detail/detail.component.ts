e4d0599f808569e0b627826ac9d96113365f3a27
vvquang <vvquanglacviet.com.vn>
2023-05-22 15:14:14 +0700

    ..

--- projects/codx-ad/src/lib/Roles/detail/detail.component.ts
+++ projects/codx-ad/src/lib/Roles/detail/detail.component.ts
@@ -329,8 +329,11 @@ export class RoleDetailComponent
         var dt = this.dataFuncRole[parent] as any[];
         var checkbox = element.querySelector('ejs-checkbox');
         var check = 'false';
-        if (checkbox) check = checkbox.getAttribute('aria-checked');
-        if (check === 'false') {
+        if (checkbox) {
+          var input = checkbox.querySelector('input');
+          check = checkbox?.ariaChecked || input?.checked + '';
+        }
+        if (!check || check === 'false') {
           if (dt && dt.length > 0) {
             dt = dt.filter((res) => res != id);
           } else {
