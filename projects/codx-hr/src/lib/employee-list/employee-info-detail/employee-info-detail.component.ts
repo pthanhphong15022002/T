795a2a201e020b11f585303fd85fc0cea5753876
vuhiepp07 <vuhiep2k107@gmail.com>
2023-05-23 16:50:44 +0700

    fix bug, update new UI EFamily, working on with EExperience

--- projects/codx-hr/src/lib/employee-list/employee-info-detail/employee-info-detail.component.ts
+++ projects/codx-hr/src/lib/employee-list/employee-info-detail/employee-info-detail.component.ts
@@ -674,6 +674,8 @@ export class EmployeeInfoDetailComponent extends UIComponent {
     this.routeActive.queryParams.subscribe((params) => {
       if (params.employeeID || this.user.userID) {
         this.employeeID = params.employeeID;
+        this.infoPersonal = null;
+        debugger
         if (history.state.empInfo) {
           this.infoPersonal = history.state.empInfo;
           this.getManagerEmployeeInfoById();
@@ -738,6 +740,7 @@ export class EmployeeInfoDetailComponent extends UIComponent {
           empRequest.predicates = 'EmployeeID=@0';
           empRequest.pageLoading = false;
           this.hrService.loadData('HR', empRequest).subscribe((emp) => {
+            debugger
             if (emp[1] > 0) {
               this.infoPersonal = emp[0][0];
               this.getManagerEmployeeInfoById();
--- projects/codx-hr/src/lib/employee-list/employee-list.component.ts
+++ projects/codx-hr/src/lib/employee-list/employee-list.component.ts
@@ -338,8 +338,9 @@ export class EmployeeListComponent extends UIComponent {
       let queryParams =  {
         employeeID: data.employeeID,
         page: this.view.dataService.page,
-        filter: JSON.stringify(this.view.dataService?.filter),
+        filter: JSON.stringify(this.view.dataService?.request.filter),
       };
+      debugger
       this.codxService.navigate("",func.url,queryParams);
     });
   }
