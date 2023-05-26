f4648d1064f65db363dccefd6d48367fa7e2626d
tmtan <you@example.com>
2023-05-26 16:27:41 +0700

    .

--- projects/codx-ac/src/lib/vouchers/cash-payments/cash-payments.component.ts
+++ projects/codx-ac/src/lib/vouchers/cash-payments/cash-payments.component.ts
@@ -29,7 +29,6 @@ import { CashPaymentLine } from '../../models/CashPaymentLine.model';
 import { CodxAcService } from '../../codx-ac.service';
 import { SettledInvoices } from '../../models/SettledInvoices.model';
 import { map } from 'rxjs';
-
 @Component({
   selector: 'lib-cash-payments',
   templateUrl: './cash-payments.component.html',
