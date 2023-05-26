95f256edf64df406aaf01de408d90406589e030d
nvthuan <nvthuan@lacviet.com.vn>
2023-05-26 18:01:38 +0700

    cm-contract

--- projects/codx-cm/src/lib/contracts/payment/popup-add-payment/popup-add-payment.component.ts
+++ projects/codx-cm/src/lib/contracts/payment/popup-add-payment/popup-add-payment.component.ts
@@ -32,11 +32,21 @@ export class PopupAddPaymentComponent {
     this.setDataInput();
   }
 
+  setPayment(){
+    let rowNo = this.listPayment?.length || 0;
+    this.payment = new CM_ContractsPayments();
+    this.payment.rowNo = rowNo + 1;
+    this.payment.refNo = this.contractID;
+    this.payment.status = '1';
+    this.payment.paidAmt = 0;
+  }
+
   setDataInput(){
     if(this.action == 'add'){
+      this.setPayment();
     }
     if(this.action == 'edit'){
-
+      this.payment = JSON.parse(JSON.stringify(this.payment));
     }
     if(this.action == 'copy'){
 
@@ -65,31 +75,54 @@ export class PopupAddPaymentComponent {
     this.payment[event?.field] = new Date(event?.data?.fromDate);
   }
 
-  save(){
+  saveAndClose(){
     if(this.action == 'add' || this.action == 'copy'){
-      this.addPayment();
+      this.addPayment(true);
     }
     if(this.action == 'edit'){
-      this.editPayment();
+      this.editPayment(true);
     }
   }
 
   saveAndContinue(){
-
+    if(this.action == 'add' || this.action == 'copy'){
+      this.addPayment(false);
+    }
+    if(this.action == 'edit'){
+      this.editPayment(false);
+    }
   }
 
-  addPayment() {
-    this.cmService.addPayments(this.payment).subscribe( res => {
-      if(res){
-          this.dialog.close({ payment: res, action: this.action });
-        }
-      })
+  addPayment(isClose) {
+    this.listPayment.push(this.payment);
+    if(isClose){
+      this.dialog.close(true);
+    }else{
+      this.action = "add";
+      this.setPayment();
+    }
+    // this.cmService.addPayments(this.payment).subscribe( res => {
+    //   if(res){
+    //       this.dialog.close({ payment: res, action: this.action });
+    //     }
+    //   })
   }
-  editPayment() {
-    this.cmService.editPayments(this.payment).subscribe( res => {
-      if(res){
-          this.dialog.close({ payment: res, action: this.action });
-        }
-      })
+  editPayment(isClose) {
+    let paymentIndex = this.listPayment.findIndex(payment => payment.recID == this.payment?.recID);
+    if(paymentIndex >= 0){
+      this.listPayment.splice(paymentIndex,1,this.payment);
+    }
+    if(isClose){
+      this.dialog.close(true);
+    }else{
+      this.action = "add";
+      this.setPayment();
+    }
+
+    // this.cmService.editPayments(this.payment).subscribe( res => {
+    //   if(res){
+    //       ({ payment: res, action: this.action });
+    //     }
+    //   })
   }
 }
