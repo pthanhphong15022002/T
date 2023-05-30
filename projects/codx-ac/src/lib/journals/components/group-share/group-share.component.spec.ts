00eb9453401d634ce7570700ff405e8b0b4297f4
khttung <khttung@lacviet.com.vn>
2023-05-30 13:41:00 +0700

    so nhat ky (role)

---
+++ projects/codx-ac/src/lib/journals/components/group-share/group-share.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GroupShareComponent } from './group-share.component';
+
+describe('GroupShareComponent', () => {
+  let component: GroupShareComponent;
+  let fixture: ComponentFixture<GroupShareComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GroupShareComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GroupShareComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
