13c062d163b27a60214a19b0174770b9f4eec463
Phúc <tdnphuck42@gmail.com>
2023-06-01 16:05:22 +0700

    lean code

--- projects/codx-cm/src/lib/leads/popup-merge-leads/popup-merge-leads.component.ts
+++ projects/codx-cm/src/lib/leads/popup-merge-leads/popup-merge-leads.component.ts
@@ -83,7 +83,6 @@ export class PopupMergeLeadsComponent implements OnInit {
     this.title = dt?.data?.title;
     this.leadOne = JSON.parse(JSON.stringify(dt?.data?.data));
     this.leadNew = JSON.parse(JSON.stringify(this.leadOne));
-
   }
   async ngOnInit() {
     this.leadNew.recID = Util.uid();
@@ -219,33 +218,39 @@ export class PopupMergeLeadsComponent implements OnInit {
   async cbxLeadChange(e, type) {
     if (e) {
       if (type == 'two') {
-        var index = this.lstLeadCbxTwo.findIndex((x) => x.recID == e);
-        if (index != -1) {
-          this.leadTwo = this.lstLeadCbxTwo[index];
-          this.lstLeadCbxThree = await this.getCbxLead(
-            this.leadOne?.recID,
-            this.leadTwo?.recID
-          );
-          this.lstContactTwo = await this.getContacts(this.leadTwo?.recID);
-          this.lstAddressTwo = await this.getListAddress(
-            this.dialog?.formModel?.entityName,
-            this.leadTwo?.recID
-          );
+        if (e != this.leadTwo?.recID) {
+          var index = this.lstLeadCbxTwo.findIndex((x) => x.recID == e);
+          if (index != -1) {
+            this.leadTwo = this.lstLeadCbxTwo[index];
+            this.lstLeadCbxThree = await this.getCbxLead(
+              this.leadOne?.recID,
+              this.leadTwo?.recID
+            );
+            this.lstContactTwo = await this.getContacts(this.leadTwo?.recID);
+            this.lstAddressTwo = await this.getListAddress(
+              this.dialog?.formModel?.entityName,
+              this.leadTwo?.recID
+            );
+          }
         }
       } else {
-        var index = this.lstLeadCbxThree.findIndex((x) => x.recID == e);
-        if (index != -1) {
-          this.leadThree = this.lstLeadCbxThree[index];
+        if (e != this.leadThree?.recID) {
+          var index = this.lstLeadCbxThree.findIndex((x) => x.recID == e);
+          if (index != -1) {
+            this.leadThree = this.lstLeadCbxThree[index];
 
-          this.lstLeadCbxTwo = await this.getCbxLead(
-            this.leadOne?.recID,
-            this.leadThree?.recID
-          );
-          this.lstContactThree = await this.getContacts(this.leadThree?.recID);
-          this.lstAddressThree = await this.getListAddress(
-            this.dialog?.formModel?.entityName,
-            this.leadThree?.recID
-          );
+            this.lstLeadCbxTwo = await this.getCbxLead(
+              this.leadOne?.recID,
+              this.leadThree?.recID
+            );
+            this.lstContactThree = await this.getContacts(
+              this.leadThree?.recID
+            );
+            this.lstAddressThree = await this.getListAddress(
+              this.dialog?.formModel?.entityName,
+              this.leadThree?.recID
+            );
+          }
         }
       }
     } else {
