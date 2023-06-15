744fab1bc3e7399da7b92bc49986eae040f457da
Phúc <tdnphuck42@gmail.com>
2023-06-15 09:57:43 +0700

    .

--- projects/codx-cm/src/lib/codx-cm.service.ts
+++ projects/codx-cm/src/lib/codx-cm.service.ts
@@ -434,6 +434,15 @@ export class CodxCmService {
     ]);
   }
 
+  getListFile(funcID, objectID, objectType, referType) {
+    return this.api.exec<any>('DM', 'FileBussiness', 'GetFilesForOutsideAsync', [
+      funcID,
+      objectID,
+      objectType,
+      referType,
+    ]);
+  }
+
   addInstance(data: any) {
     return this.api.exec<any>(
       'DP',
