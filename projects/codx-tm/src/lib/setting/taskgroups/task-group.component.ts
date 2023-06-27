d120555dc64c1242c87c15f982c9706ad6027864
Vo Thao <vthao@lacviet.com.vn>
2023-06-27 16:46:20 +0700

    ,,,

--- projects/codx-tm/src/lib/setting/taskgroups/task-group.component.ts
+++ projects/codx-tm/src/lib/setting/taskgroups/task-group.component.ts
@@ -136,7 +136,7 @@ export class TaskGroupComponent extends UIComponent implements OnInit {
     ];
   }
 
-  onLoading(e) {
+  onLoading() {
     let formModel = this.view.formModel;
     if (formModel) {
       this.cacheSv
