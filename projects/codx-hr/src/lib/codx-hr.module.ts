42e08bd4a7287cc5e847e5fdc949a36c373f3733
Tien Hoang Manh <55588287+sakifire@users.noreply.github.com>
2023-06-29 17:56:37 +0700

    Update codx-hr.module.ts

--- projects/codx-hr/src/lib/codx-hr.module.ts
+++ projects/codx-hr/src/lib/codx-hr.module.ts
@@ -127,7 +127,7 @@ import { EmployeePolicyalComponent } from './employee-policyal/employee-policyal
 import { PopupPolicyalComponent } from './employee-policyal/popup-policyal/popup-policyal.component';
 import { EmployeePolicybenefitsComponent } from './employee-policybenefits/employee-policybenefits.component';
 import { PopupPolicybenefitsComponent } from './employee-policybenefits/popup-policybenefits/popup-policybenefits.component';
-import { BasicPrimitivesModule } from 'ngx-basic-primitives';
+// import { BasicPrimitivesModule } from 'ngx-basic-primitives';
 import { OrgEmpContactDetailCardComponent } from './empcontacts/org-emp-contact-detail-card/org-emp-contact-detail-card.component';
 import { EmployeeListByOrgComponent } from './employee-list/employee-list-by-org/employee-list-by-org.component';
 export const routes: Routes = [
@@ -431,7 +431,7 @@ const T_Component: Type<any>[] = [
     ChartAllModule,
     DiagramAllModule,
     NgbModule,
-    BasicPrimitivesModule,
+    //BasicPrimitivesModule,
   ],
   exports: [RouterModule],
   declarations: T_Component,
