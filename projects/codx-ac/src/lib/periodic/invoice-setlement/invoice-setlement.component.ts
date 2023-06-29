886164d0159adc3a1b62e507431b23bf5a361036
Anh Tu <dcatu@lacviet.com.vn>
2023-06-29 08:17:19 +0700

    .

--- projects/codx-ac/src/lib/periodic/invoice-setlement/invoice-setlement.component.ts
+++ projects/codx-ac/src/lib/periodic/invoice-setlement/invoice-setlement.component.ts
@@ -65,7 +65,7 @@ export class InvoiceSetlementComponent extends UIComponent{
       },
       {
         type: ViewType.smallcard,
-        active: true,
+        active: false,
         sameData: true,
         model: {
           template: this.itemTemplate,
