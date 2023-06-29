eb446ae3c40e20ad476febe4b7bbb9eed4cfc29d
deedashing <phamducduy1506@gmail.com>
2023-06-29 17:25:28 +0700

    Add Org Chart new , fix bug

--- projects/codx-hr/src/lib/organization/organization-orgchart/organization-orgchart.component.ts
+++ projects/codx-hr/src/lib/organization/organization-orgchart/organization-orgchart.component.ts
@@ -8,7 +8,7 @@ import {
   EventEmitter,
   ViewChild,
 } from '@angular/core';
-import { Thickness } from '@syncfusion/ej2-angular-charts';
+// import { Thickness } from '@syncfusion/ej2-angular-charts';
 import {
   ConnectorModel,
   Diagram,
@@ -20,6 +20,20 @@ import {
   TextModel,
 } from '@syncfusion/ej2-angular-diagrams';
 import { DataManager } from '@syncfusion/ej2-data';
+import {
+  OrgItemConfig,
+  Enabled,
+  PageFitMode,
+  Colors,
+  AnnotationType,
+  Thickness,
+  LineType,
+  AdviserPlacementType,
+  ItemType,
+  ChildrenPlacementType,
+  GroupByType,
+  LevelAnnotationConfig,
+} from 'ngx-basic-primitives';
 import {
   ApiHttpService,
   CallFuncService,
@@ -37,6 +51,13 @@ import { PopupAddOrganizationComponent } from '../popup-add-organization/popup-a
   styleUrls: ['./organization-orgchart.component.css'],
 })
 export class OrganizationOrgchartComponent implements OnInit {
+  // PageFitMode = PageFitMode;
+  // Enabled = Enabled;
+  // ChildrenPlacementType = ChildrenPlacementType;
+  // GroupByType = GroupByType;
+  // items: Array<OrgItemConfig> = [];
+  // annotations: Array<LevelAnnotationConfig> = [];
+
   datasetting: any = null;
   dataSource: any = null;
   public layout: Object = {
@@ -69,25 +90,205 @@ export class OrganizationOrgchartComponent implements OnInit {
     private callFC: CallFuncService
   ) {}
 
-  ngOnInit(): void {}
-  //onChange dataSource
+  getDataPositionByID(orgUnitID: string) {
+    if (orgUnitID) {
+      this.api
+        .execSv(
+          'HR',
+          'ERM.Business.HR',
+          'OrganizationUnitsBusiness',
+          'GetDataOrgChartAsync',
+          [orgUnitID]
+        )
+        .subscribe((res: any) => {
+          if (res) {
+            console.log(res);
+            this.dataSource = this.newDataManager(res);
+          }
+        });
+    }
+  }
+
+  ngOnInit(): void {
+    // var items: Array<OrgItemConfig> = [];
+    // var rootItem = new OrgItemConfig({
+    //   id: 0,
+    //   parent: null,
+    //   title: 'Title A',
+    //   description: 'Description A',
+    //   // //image: './assets/photos/a.png',
+    //   // childrenPlacementType: ChildrenPlacementType.Vertical,
+    // });
+    // items.push(rootItem);
+    // items.push(
+    //   new OrgItemConfig({
+    //     id: 1,
+    //     parent: 0,
+    //     title: 'Assistant 1',
+    //     description: 'Assistant Description',
+    //     //image: './assets/photos/a.png',
+    //     itemType: ItemType.Assistant,
+    //     adviserPlacementType: AdviserPlacementType.Right,
+    //     groupTitle: 'Audit',
+    //     groupTitleColor: Colors.Olive,
+    //     levelOffset: 0,
+    //   })
+    // );
+    // items.push(
+    //   new OrgItemConfig({
+    //     id: 2,
+    //     parent: 0,
+    //     title: 'Assistant 2',
+    //     description: 'Assistant Description',
+    //     //image: './assets/photos/b.png',
+    //     itemType: ItemType.Assistant,
+    //     adviserPlacementType: AdviserPlacementType.Left,
+    //     groupTitle: 'Audit',
+    //     groupTitleColor: Colors.Olive,
+    //     levelOffset: 0,
+    //   })
+    // );
+    // items.push(
+    //   new OrgItemConfig({
+    //     id: 13,
+    //     parent: 0,
+    //     title: 'Assistant 3',
+    //     description: 'Assistant Description',
+    //     //image: './assets/photos/c.png',
+    //     itemType: ItemType.Assistant,
+    //     adviserPlacementType: AdviserPlacementType.Right,
+    //     groupTitle: 'Audit',
+    //     groupTitleColor: Colors.Olive,
+    //     levelOffset: 1,
+    //   })
+    // );
+    // for (var index = 3; index <= 10; index += 2) {
+    //   items.push(
+    //     new OrgItemConfig({
+    //       id: index,
+    //       parent: 1,
+    //       title: 'Sub Adviser 11',
+    //       description: 'Sub Adviser Description',
+    //       //image: './assets/photos/s.png',
+    //       itemType: ItemType.SubAdviser,
+    //       adviserPlacementType: AdviserPlacementType.Left,
+    //       groupTitle: 'Sub Adviser',
+    //       groupTitleColor: Colors.Red,
+    //     })
+    //   );
+    //   items.push(
+    //     new OrgItemConfig({
+    //       id: index + 1,
+    //       parent: 2,
+    //       title: 'Sub Adviser 22',
+    //       description: 'Sub Adviser Description',
+    //       //image: './assets/photos/s.png',
+    //       itemType: ItemType.SubAdviser,
+    //       adviserPlacementType: AdviserPlacementType.Right,
+    //       groupTitle: 'Sub Adviser',
+    //       groupTitleColor: Colors.Red,
+    //     })
+    //   );
+    //   items.push(
+    //     new OrgItemConfig({
+    //       id: index + 2,
+    //       parent: 3,
+    //       title: 'Sub Adviser 33',
+    //       description: 'Sub Adviser Description',
+    //       //image: './assets/photos/s.png',
+    //       itemType: ItemType.SubAdviser,
+    //       adviserPlacementType: AdviserPlacementType.Left,
+    //       groupTitle: 'Sub Adviser',
+    //       groupTitleColor: Colors.Red,
+    //     })
+    //   );
+    // }
+    // items.push(
+    //   new OrgItemConfig({
+    //     id: 14,
+    //     parent: 0,
+    //     title: 'Assistant 4',
+    //     description: 'Assistant Description',
+    //     //image: './assets/photos/d.png',
+    //     itemType: ItemType.Assistant,
+    //     adviserPlacementType: AdviserPlacementType.Left,
+    //     groupTitle: 'Audit',
+    //     groupTitleColor: Colors.Olive,
+    //     levelOffset: 1,
+    //   })
+    // );
+    // var annotations = [
+    //   new LevelAnnotationConfig({
+    //     annotationType: AnnotationType.Level,
+    //     levels: [0],
+    //     title: 'CEO',
+    //     titleColor: Colors.RoyalBlue,
+    //     offset: new Thickness(0, 0, 0, -1),
+    //     lineWidth: new Thickness(0, 0, 0, 0),
+    //     opacity: 0,
+    //     borderColor: Colors.Gray,
+    //     fillColor: Colors.Gray,
+    //     lineType: LineType.Dotted,
+    //   }),
+    //   new LevelAnnotationConfig({
+    //     annotationType: AnnotationType.Level,
+    //     levels: [1],
+    //     title: 'Children 1',
+    //     titleColor: Colors.RoyalBlue,
+    //     offset: new Thickness(0, 0, 0, -1),
+    //     lineWidth: new Thickness(0, 0, 0, 0),
+    //     opacity: 0.08,
+    //     borderColor: Colors.Gray,
+    //     fillColor: Colors.Gray,
+    //     lineType: LineType.Dotted,
+    //   }),
+    //   new LevelAnnotationConfig({
+    //     annotationType: AnnotationType.Level,
+    //     levels: [2],
+    //     title: 'Children 2',
+    //     titleColor: Colors.RoyalBlue,
+    //     offset: new Thickness(0, 0, 0, -1),
+    //     lineWidth: new Thickness(0, 0, 0, 0),
+    //     opacity: 0,
+    //     borderColor: Colors.Gray,
+    //     fillColor: Colors.Gray,
+    //     lineType: LineType.Dotted,
+    //   }),
+    //   new LevelAnnotationConfig({
+    //     annotationType: AnnotationType.Level,
+    //     levels: [3],
+    //     title: 'Members',
+    //     titleColor: Colors.RoyalBlue,
+    //     offset: new Thickness(0, 0, 0, -1),
+    //     lineWidth: new Thickness(0, 0, 0, 0),
+    //     opacity: 0.08,
+    //     borderColor: Colors.Gray,
+    //     fillColor: Colors.Gray,
+    //     lineType: LineType.Dotted,
+    //   }),
+    // ];
+    // this.items = items;
+    // this.annotations = annotations;
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.orgUnitID.currentValue != changes.orgUnitID.previousValue) {
-      // console.log(this.orgUnitID);
       if (this.orgUnitID) {
-        this.dataService
-          .setPredicates([], [this.orgUnitID])
-          .subscribe((res) => {
-            if (res) {
-              res.forEach((x) => {
-                if (x.orgUnitID === this.orgUnitID) {
-                  x.parentID = '';
-                  return;
-                }
-              });
-            }
-            this.dataSource = this.newDataManager(res);
-          });
+        //Function get new orgchart
+        // this.getDataPositionByID(this.orgUnitID);
+
+        //Function get olg orgchart
+        this.dataService.setPredicates([], [this.orgUnitID], (res) => {
+          if (res) {
+            res.forEach((x) => {
+              if (x.orgUnitID === this.orgUnitID) {
+                x.parentID = '';
+                return;
+              }
+            });
+          }
+          this.dataSource = this.newDataManager(res);
+        });
       }
     }
   }
