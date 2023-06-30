1ce550d2de765908021f0a0bf71b1c0dacdaf228
LocTran <loctt.cntp@gamil.com>
2023-06-30 10:00:48 +0700

    ...

--- projects/codx-hr/src/lib/organization/organization.component.ts
+++ projects/codx-hr/src/lib/organization/organization.component.ts
@@ -348,4 +348,9 @@ export class OrgorganizationComponent extends UIComponent {
   //       }
   //     });
   // }
+
+  searchText:string = "";
+  searchUser(event:any){
+    this.searchText = event;
+  }
 }
