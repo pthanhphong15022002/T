158553e495b43927b7e73b38c944c08da1b501b7
lvietbao <lvietbao@lacviet.com.vn>
2023-06-30 08:23:13 +0700

    fix status deal

--- projects/codx-cm/src/lib/deals/deal-detail/tab-detail-custom/tab-detail-custom.component.ts
+++ projects/codx-cm/src/lib/deals/deal-detail/tab-detail-custom/tab-detail-custom.component.ts
@@ -19,7 +19,7 @@ import { DP_Instances_Steps } from 'projects/codx-dp/src/lib/models/models';
 import { DealsComponent } from '../../deals.component';
 import { DealDetailComponent } from '../deal-detail.component';
 import { CodxListContactsComponent } from '../../../cmcustomer/cmcustomer-detail/codx-list-contacts/codx-list-contacts.component';
-import { CM_Contacts } from '../../../models/cm_model';
+import { CM_Contacts, CM_Contracts } from '../../../models/cm_model';
 
 @Component({
   selector: 'codx-tab-deal-detail',
@@ -43,7 +43,12 @@ export class TabDetailCustomComponent
   // isUpdate = true; //xư lý cho edit trung tuy chinh ko
   listStepsProcess = [];
   listCategory = [];
-  listContract: CM_Contacts[];
+  listContract: CM_Contracts[];
+
+  listContractRef: CM_Contracts[];
+  // listQ
+
+
   // titleDefault= "Trường tùy chỉnh"//truyen vay da
   readonly tabInformation: string = 'Information';
   readonly tabField: string = 'Field';
@@ -54,6 +59,8 @@ export class TabDetailCustomComponent
   readonly tabProduct: string = 'Product';
   readonly tabQuotation: string = 'Quotation';
   readonly tabContract: string = 'Contract';
+  readonly tabHistory: string = 'History';
+
 
   fmProcductsLines: FormModel = {
     formName: 'CMProducts',
@@ -92,6 +99,9 @@ export class TabDetailCustomComponent
       {
         this.loadContactDeal.getListContactsByObjectId(this.dataSelected.recID);
       }
+      if(this.tabClicked === this.tabHistory) {
+        this.getHistoryByDeaID();
+      }
       this.getListInstanceStep();
       this.getContractByDeaID();
     }
@@ -105,6 +115,16 @@ export class TabDetailCustomComponent
           this.listContract = res;
         } else {
           this.listContract = [];
+        }
+      });
+    }
+  }
+  async getHistoryByDeaID() {
+    if (this.dataSelected?.recID) {
+      var data = [this.dataSelected?.recID];
+      this.codxCmService.getDataTabHistoryDealAsync(data).subscribe((res) => {
+        if (res) {
+          console.log('có data nha');
         }
       });
     }
