792e66463b0472128a0a71172c2de5a2db480ccc
lvietbao <lvietbao@lacviet.com.vn>
2023-06-30 16:05:16 +0700

    fix bug in lead

--- projects/codx-share/src/lib/components/codx-step/codx-taskbar/codx-taskbar.component.ts
+++ projects/codx-share/src/lib/components/codx-step/codx-taskbar/codx-taskbar.component.ts
@@ -9,9 +9,9 @@ export class CodxTaskbarComponent implements OnInit,OnChanges {
   @Input() listTab = []; // danh sách tab {icon, name, textDefault}
   @Input() size = 20; // kích thước icon và chữ
   @Input() color = '';
-  @Input() class = ''; 
-  @Input() change: any; 
-  @Input() isStart: any; 
+  @Input() class = '';
+  @Input() change: any;
+  @Input() isStart: any;
   @Output() tab = new EventEmitter(); // giá trị trả về khi chọn => name
 
   sizeIcon = '';
@@ -19,12 +19,12 @@ export class CodxTaskbarComponent implements OnInit,OnChanges {
   listTaskConvert = [];
   tabOld;
   constructor() {
-    
+
   }
   ngOnInit(): void {
     this.size = this.size%2 == 0 ? this.size : this.size + 1;
     this.sizeIcon = this.size.toString();
-    this.sizeText = (this.size - 8).toString() + 'px'; 
+    this.sizeText = (this.size - 8).toString() + 'px';
   }
 
   ngOnChanges(changes: SimpleChanges){
@@ -35,17 +35,17 @@ export class CodxTaskbarComponent implements OnInit,OnChanges {
         })
         this.listTaskConvert[0].isActive = true;
         this.tabOld = this.listTaskConvert[0];
-        this.tab.emit(this.listTaskConvert[0].name); 
+        this.tab.emit(this.listTaskConvert[0].name);
       }else{
         if(this.tabOld){
-          this.tab.emit(this.tabOld.name); 
+          this.tab.emit(this.tabOld.name);
         }else{
           this.listTaskConvert = this.listTab.map((item) => {
             return {...item, isActive: false}
           })
           this.listTaskConvert[0].isActive = true;
           this.tabOld = this.listTaskConvert[0];
-          this.tab.emit(this.listTaskConvert[0].name); 
+          this.tab.emit(this.listTaskConvert[0].name);
         }
       }
     }
