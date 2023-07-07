9591928dc569557d5d219eddaacc505258480a74
Vo Thao <vthao@lacviet.com.vn>
2023-07-07 09:59:38 +0700

    ..

--- projects/codx-cm/src/lib/cm-dashboard/cm-dashboard.component.ts
+++ projects/codx-cm/src/lib/cm-dashboard/cm-dashboard.component.ts
@@ -56,6 +56,14 @@ export class CmDashboardComponent extends UIComponent implements AfterViewInit {
   maxOwners = [];
   minOwners = [];
 
+  primaryXAxis = {
+    interval: 1,
+  };
+  primaryYAxis = {
+    title: 'Tỷ lệ thành công',
+  };
+  chartData = [];
+
   constructor(inject: Injector, private layout: LayoutComponent) {
     super(inject);
     this.funcID = this.router.snapshot.params['funcID'];
