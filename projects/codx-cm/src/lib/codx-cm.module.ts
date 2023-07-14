145b5c7a0a5d977364a94dd3915d13725edc52ed
nvthuan <nvthuan@lacviet.com.vn>
2023-07-14 11:19:41 +0700

    fix task and contract in custommer

--- projects/codx-cm/src/lib/codx-cm.module.ts
+++ projects/codx-cm/src/lib/codx-cm.module.ts
@@ -44,8 +44,6 @@ import { CodxTabDealcompetitorsComponent } from './deals/deal-detail/codx-tab-de
 import { PopupAddDealcompetitorComponent } from './deals/deal-detail/codx-tab-dealcompetitors/popup-add-dealcompetitor/popup-add-dealcompetitor.component';
 import { ViewDealcompetitorsComponent } from './cmcustomer/cmcustomer-detail/view-dealcompetitors/view-dealcompetitors.component';
 import { QuotationsViewDetailComponent } from './quotations/quotations-view-detail/quotations-view-detail.component';
-import { CodxComboboxComponent } from './contracts/component/codx-combobox/codx-combobox.component';
-import { ContractsDetailComponent } from './contracts/contracts-detail/contracts-detail.component';
 import { CasesComponent } from './cases/cases.component';
 import { CodxAddressCmComponent } from './cmcustomer/cmcustomer-detail/codx-address-cm/codx-address-cm.component';
 import { LeadsComponent } from './leads/leads.component';
@@ -62,7 +60,6 @@ import { PopupAddPaymentComponent } from './contracts/payment/popup-add-payment/
 import { PopupAddPaymentHistoryComponent } from './contracts/payment/popup-add-payment-history/popup-add-payment-history.component';
 import { PopupViewPaymentHistoryComponent } from './contracts/payment/popup-view-payment-history/popup-view-payment-history.component';
 import { PopupMergeLeadsComponent } from './leads/popup-merge-leads/popup-merge-leads.component';
-import { ViewQuotationsComponent } from './contracts/component/view-quotations/view-quotations.component';
 import { ViewImgContactComponent } from './leads/popup-merge-leads/view-img-contact/view-img-contact.component';
 import { PopupRemoveAddContactComponent } from './leads/popup-merge-leads/popup-remove-add-contact/popup-remove-add-contact.component';
 import { ViewPaymentComponent } from './contracts/payment/view-payment/view-payment.component';
@@ -198,7 +195,6 @@ const T_Component: Type<any>[] = [LayoutComponent];
     CodxTabDealcompetitorsComponent,
     PopupAddDealcompetitorComponent,
     ViewDealcompetitorsComponent,
-    CodxComboboxComponent,
     QuotationsViewDetailComponent,
     CasesComponent,
     CasesDetailComponent,
@@ -214,7 +210,6 @@ const T_Component: Type<any>[] = [LayoutComponent];
     PopupStatusCompetitorComponent,
     CampaignsDetailComponent,
     PopupConvertLeadComponent,
-    ViewQuotationsComponent,
     ContractsComponent,
     CustomergroupsComponent,
     PopupAddCustgroupComponent,
@@ -232,7 +227,6 @@ const T_Component: Type<any>[] = [LayoutComponent];
     SettingProcessCmComponent,
     QuotationsTabViewComponent,
     ContractsViewDetailComponent,
-    ContractsDetailComponent,
     TaskComponent,
     PopupOwnerDealComponent,
     ViewIconGroupComponent,
