38fdc4e5367cf05d54a8a1aeaa35c57b65b1a23f
Anh Tu <dcatu@lacviet.com.vn>
2023-07-17 17:32:08 +0700

    .

---
+++ projects/codx-ac/src/lib/models/FiscalPeriods.model.ts
@@ -0,0 +1,15 @@
+export class FiscalPeriods{
+    periodID: any;
+    periodName: any;
+    periodName2: any;
+    periodType: any;
+    fiscalYear: any;
+    startDate: any;
+    endDate: any;
+    periodControl: any;
+    moduleControl: any;
+    createdOn: any;
+    createdBy: any;
+    modifiedOn: any;
+    modifiedBy: any;
+}
