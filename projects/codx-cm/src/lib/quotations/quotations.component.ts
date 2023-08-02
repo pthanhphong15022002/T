a9099ef487c3ef45fad9760fc52a22f028f26693
nvthuan <nvthuan@lacviet.com.vn>
2023-08-02 13:53:08 +0700

    contract quotation

--- projects/codx-cm/src/lib/quotations/quotations.component.ts
+++ projects/codx-cm/src/lib/quotations/quotations.component.ts
@@ -27,7 +27,7 @@ import { PopupAddQuotationsComponent } from './popup-add-quotations/popup-add-qu
 import { Observable, finalize, firstValueFrom, map } from 'rxjs';
 import { CodxCmService } from '../codx-cm.service';
 import { CodxShareService } from 'projects/codx-share/src/lib/codx-share.service';
-import { CM_Contracts } from '../models/cm_model';
+import { CM_Contracts, CM_Quotations } from '../models/cm_model';
 import { AddContractsComponent } from '../contracts/add-contracts/add-contracts.component';
 import { debug } from 'util';
 
@@ -590,8 +590,10 @@ export class QuotationsComponent extends UIComponent implements OnInit {
   }
 
   // tạo hợp đồng
-  createContract(dt) {
+  createContract(quotation: CM_Quotations) {
     let contract = new CM_Contracts();
+    contract.customerID = quotation?.customerID;
+    contract.quotationID = quotation?.recID;
     let data = {
       projectID: null,
       action: "add",
@@ -604,7 +606,7 @@ export class QuotationsComponent extends UIComponent implements OnInit {
     option.IsFull = true;
     option.zIndex = 1010;
     option.FormModel = this.formModel;
-    let popupContract = this.callfunc.openForm(
+    this.callfunc.openForm(
       AddContractsComponent,
       '',
       null,
