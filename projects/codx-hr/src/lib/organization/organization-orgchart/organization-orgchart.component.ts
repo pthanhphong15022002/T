d3942e1795ba511e4fea285787be5f6fb0273951
deedashing <phamducduy1506@gmail.com>
2023-08-03 09:48:58 +0700

    Fix warning console chart

--- projects/codx-hr/src/lib/organization/organization-orgchart/organization-orgchart.component.ts
+++ projects/codx-hr/src/lib/organization/organization-orgchart/organization-orgchart.component.ts
@@ -1300,7 +1300,6 @@ export class OrganizationOrgchartComponent {
       .subscribe((res: any) => {
         if (res) {
           this.dialogEditStatus && this.dialogEditStatus.close();
-          // console.log(res.dataValue);
         }
       });
   }
