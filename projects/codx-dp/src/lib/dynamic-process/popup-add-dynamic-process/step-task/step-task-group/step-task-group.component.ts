c22546893f319b092ee1c3c5cf9e633c12fe7542
nvthuan <nvthuan@lacviet.com.vn>
2023-08-09 18:26:22 +0700

    role task

--- projects/codx-dp/src/lib/dynamic-process/popup-add-dynamic-process/step-task/step-task-group/step-task-group.component.ts
+++ projects/codx-dp/src/lib/dynamic-process/popup-add-dynamic-process/step-task/step-task-group/step-task-group.component.ts
@@ -83,6 +83,9 @@ export class StepTaskGroupComponent implements OnInit {
 
   changeUser(e) {
     this.taskGroup['roles'] = e || [];
+    if(this.taskGroup?.roles?.length > 0){
+      this.taskGroup.owner = this.taskGroup?.roles[0]?.objectID;
+    }
   }
 
   changeValueNumber(event) {
