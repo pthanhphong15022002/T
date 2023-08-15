c2f354c34ffbc1a257431879e8254d5c43ba7307
deedashing <phamducduy1506@gmail.com>
2023-08-15 08:18:37 +0700

    Fix error patch formgroup EContract

--- projects/codx-hr/src/lib/employee-contract/popup-eprocess-contract/popup-eprocess-contract.component.ts
+++ projects/codx-hr/src/lib/employee-contract/popup-eprocess-contract/popup-eprocess-contract.component.ts
@@ -271,7 +271,7 @@ export class PopupEProcessContractComponent
         )
         .subscribe((res) => {
           if (res) {
-            debugger
+            debugger;
             this.autoNumField = res.key ? res.key : null;
             this.loadedAutoField = true;
             this.df.detectChanges();
@@ -310,8 +310,9 @@ export class PopupEProcessContractComponent
         )
         .subscribe((res) => {
           if (res) {
-            this.autoNumField = res.key ? res.key : null}
-        })
+            this.autoNumField = res.key ? res.key : null;
+          }
+        });
       this.loadedAutoField = true;
       if (this.actionType == 'copy') {
         if (this.data.signedDate == '0001-01-01T00:00:00') {
