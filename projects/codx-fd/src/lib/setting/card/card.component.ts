5ecec7c002964681509c1f37abf3e52bd1a73d21
Buu Nguyen <74578456+maiyozkyo@users.noreply.github.com>
2023-08-21 14:02:49 +0700

    fd: settings, co: ntoe

--- projects/codx-fd/src/lib/setting/card/card.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss'],
-})
-export class CardComponent {}
