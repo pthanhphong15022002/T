71fde1febc3743dd8781af2365e70a0ac3ddd0df
nvthuan <nvthuan@lacviet.com.vn>
2023-09-05 17:51:41 +0700

    add task

--- projects/codx-cm/src/lib/calendars/view-calendar/view-calendar.component.ts
+++ projects/codx-cm/src/lib/calendars/view-calendar/view-calendar.component.ts
@@ -17,12 +17,15 @@ import {
   FormModel,
   ResourceModel,
   UIComponent,
+  Util,
   ViewModel,
   ViewType,
 } from 'codx-core';
 import { CodxCmService } from '../../codx-cm.service';
-import { Observable, finalize, map } from 'rxjs';
+import { Observable, finalize, firstValueFrom, map } from 'rxjs';
 import { CM_Customers } from '../../models/cm_model';
+import { CodxTypeTaskComponent } from 'projects/codx-share/src/lib/components/codx-step/codx-type-task/codx-type-task.component';
+import { StepService } from 'projects/codx-share/src/lib/components/codx-step/step.service';
 
 @Component({
   selector: 'lib-view-calendar',
@@ -47,14 +50,15 @@ export class ViewCalendarComponent
   requestSchedule: ResourceModel;
   modelResource: ResourceModel;
 
+  taskType;
   popupTypeCM: DialogRef
   fieldsGroup = { text: 'text', value: 'entityName' };
 
-  fieldsCustomer = { text: 'customerName', value: 'entityName' };
-  fieldsLead = { text: 'leadName', value: 'entityName' };
+  fieldsCustomer = { text: 'customerName', value: 'recID' };
+  fieldsLead = { text: 'leadName', value: 'recID' };
   fieldsDeal = { text: 'dealName', value: 'recID' };
-  fieldsCase = { text: 'caseName', value: 'entityName' };
-  fieldsContract = { text: 'contractName', value: 'entityName' };
+  fieldsCase = { text: 'caseName', value: 'recID' };
+  fieldsContract = { text: 'contractName', value: 'recID' };
 
   typeCMs = [
     { text: 'Khách hàng', entityName: 'CM_Customers',funcID:'CM0101' },
@@ -71,6 +75,9 @@ export class ViewCalendarComponent
   listCustomer: CM_Customers[];
   listContract: CM_Customers[];
 
+  objectID = '';
+  isActivitie = false;
+  isStepTask = false;
   fieldTypeCm = '';
   service = 'CM';
   entityName = 'CM_Contracts';
@@ -117,7 +124,8 @@ export class ViewCalendarComponent
   constructor(
     private inject: Injector,
     private authstore: AuthStore,
-    private cmService: CodxCmService
+    private cmService: CodxCmService,
+    private stepService: StepService,
   ) {
     super(inject);
     this.router.params.subscribe((param: any) => {
@@ -330,6 +338,7 @@ export class ViewCalendarComponent
         this.getDatas(typeCM?.entityName,typeCM?.funcID,null,null);
         break;
       case 'CM_Customers':
+        this.objectID = event?.value;
         break;
       case 'CM_Leads':
         break;
@@ -365,6 +374,8 @@ export class ViewCalendarComponent
           break;
         case 'CM_Customers':
           this.listCustomer = res
+          this.isStepTask = false;
+          this.isActivitie = true;
           break;
       }
       console.log(res)
@@ -389,4 +400,51 @@ export class ViewCalendarComponent
       );
     }
 
+    continue(){
+      this.popupTypeCM.close();
+      this.chooseTypeTask();
+    }
+
+    async chooseTypeTask(){
+     this.taskType = await this.stepService.chooseTypeTask(true);
+      if(this.taskType){
+        if (this.taskType?.value == 'G') {          
+          await this.addGroup();
+        } else {
+          await this.addTask(this.taskType);
+        }
+      }
+    }
+  
+    async addGroup(){
+      let groupOutput = await this.stepService.addGroupTask(null);
+      // if(groupOutput?.groupTask){
+      //   this.groupTaskAdd = groupOutput?.groupTask;
+      //   this.instanceStep?.taskGroups?.push(groupOutput?.groupTask);
+      //   this.changeDetectorRef.detectChanges();
+      // }
+    }
+  
+    async addTask(dataType){
+      let taskOutput = await this.stepService.addTask(dataType,null,null);
+      let task = taskOutput;
+      if(task){
+        task['progress'] = 0;
+        task['refID'] = Util.uid();
+        task['isTaskDefault'] = false;
+        task['taskType'] = this.taskType?.value;
+        task['objectID'] = this.objectID;
+        task['objectType'] = this.entityName;
+      this.api
+        .exec<any>('DP', 'InstanceStepsBusiness', 'AddActivitiesAsync', [
+          task,this.entityName
+        ])
+        .subscribe((res) => {
+          if (res) {
+
+            this.detectorRef.detectChanges();
+          }
+        });
+      }
+    }
 }
