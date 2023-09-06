3b4404cf75d4c7cb73b17a08a3c4e65c13b98275
chungthai7596 <bachung7596@gmail.com>
2023-09-06 10:09:42 +0700

    .

--- projects/codx-ws/src/lib/_layout/codx-ws-header/codx-ws-header.component.ts
+++ projects/codx-ws/src/lib/_layout/codx-ws-header/codx-ws-header.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Injector, OnInit, Output } from '@angular/core';
-import { CacheService, CodxService, LayoutBaseComponent, LayoutService, PageTitleService } from 'codx-core';
+import { AuthStore, CacheService, CodxService, LayoutBaseComponent, LayoutService, PageTitleService } from 'codx-core';
 import { CodxWsService } from '../../codx-ws.service';
 import { isObservable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
@@ -20,17 +20,19 @@ export class CodxWsHeaderComponent extends LayoutBaseComponent{
   funcList:any;
   selectedIndex = 0;
   funcID:any;
-  
+  userInfo:any;
   constructor(
     inject: Injector,
     private pageTitle: PageTitleService,
     override codxService: CodxService,
     private codxWsService: CodxWsService,
+    private authStore: AuthStore,
   ) {
     super(inject);
     this.module = 'WS';
     this.layoutModel.asideDisplay = false;
     this.layoutModel.toolbarFixed = false;
+    this.userInfo = this.authStore.get();
   }
 
   override onInit(): void {
@@ -39,6 +41,7 @@ export class CodxWsHeaderComponent extends LayoutBaseComponent{
     this.logo$ = this.layout.logo.asObservable();
 
     this.getFuncChange();
+    this.getModuleByUserID();
   }
  
   
@@ -67,6 +70,27 @@ export class CodxWsHeaderComponent extends LayoutBaseComponent{
       this.funcList = fucList.filter(x=>x.parentID == this.module && (x.functionType == "T" || x.functionType == "D" || x.functionType == "R" ));
       this.selectedIndex = this.funcList.findIndex(x=>x.functionID == funcID);
     }
+  }
+
+  getModuleByUserID()
+  {
+    var module = this.codxWsService.loadModuleByUserID(this.userInfo?.userID) as any;
+    if(isObservable(module))
+    {
+      module.subscribe((item:any)=>{
+        debugger
+        if(item) var listModule = item.map(function(x){return x.module;});
+      })
+    }
+    else
+    {
+      debugger
+      var listModule = module.map(function(x){return x.module;});
+    }
+  }
+  getDashboardOrReport()
+  {
+
   }
 
   selectedChange(i:any , item:any)
