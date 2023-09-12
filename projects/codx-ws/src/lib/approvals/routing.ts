ceb7c70447068b88c0299d8153047d57973d3b1c
chungthai7596 <bachung7596@gmail.com>
2023-09-12 13:06:15 +0700

    .

--- projects/codx-ws/src/lib/approvals/routing.ts
+++ projects/codx-ws/src/lib/approvals/routing.ts
@@ -1,10 +1,13 @@
 import { IncommingComponent } from "projects/codx-od/src/lib/incomming/incomming.component";
 import { RequestReviewComponent } from "./request-review/request-review.component";
+import { EmployeeContractComponent } from "projects/codx-hr/src/lib/employee-contract/employee-contract.component";
 
 //Routing component xét duyệt
 export var components = {
     cpnApproval: RequestReviewComponent,
     //Công văn
-    cpnDispatches: IncommingComponent
+    cpnDispatches: IncommingComponent,
+    //Nhân sự
+    cpnEmployeeContract : EmployeeContractComponent
   };
   
