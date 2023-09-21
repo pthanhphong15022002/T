4ca5dfa62b253cb555691acabcab7eb9ca42e93b
khttung <khttung@lacviet.com.vn>
2023-09-21 11:24:26 +0700

    .

--- projects/codx-ac/src/lib/vouchers/purchaseinvoices/purchaseinvoices-add/purchaseinvoices-add.component.ts
+++ projects/codx-ac/src/lib/vouchers/purchaseinvoices/purchaseinvoices-add/purchaseinvoices-add.component.ts
@@ -257,6 +257,7 @@ export class PurchaseinvoicesAddComponent
           }
 
           if (closeAfterSave) {
+            this.masterService.update(master).subscribe();
             this.dialog.close();
           } else {
             this.resetForm();
