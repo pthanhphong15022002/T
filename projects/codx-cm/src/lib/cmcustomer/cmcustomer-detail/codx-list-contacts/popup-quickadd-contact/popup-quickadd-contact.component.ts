6ba26995c1a0a1512ce623036f018be2e0efc323
Vo Thao <vthao@lacviet.com.vn>
2023-09-22 12:42:47 +0700

    check

--- projects/codx-cm/src/lib/cmcustomer/cmcustomer-detail/codx-list-contacts/popup-quickadd-contact/popup-quickadd-contact.component.ts
+++ projects/codx-cm/src/lib/cmcustomer/cmcustomer-detail/codx-list-contacts/popup-quickadd-contact/popup-quickadd-contact.component.ts
@@ -222,6 +222,7 @@ export class PopupQuickaddContactComponent implements OnInit {
       this.data.share = true;
       if (type == 'save') {
         this.dialog.close(this.data);
+        this.data = new CM_Contacts(); //gán lại gia tri de ko bị binding Data
       } else {
         this.deleteContact(this.data);
       }
