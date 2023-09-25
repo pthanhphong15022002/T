3dd724328791b93c0207d1b5b853da69296a2f68
Anh Tu <dcatu@lacviet.com.vn>
2023-09-25 11:44:07 +0700

    .

--- projects/codx-ac/src/lib/advance-payment/advance-payment-add/advance-payment-add.component.ts
+++ projects/codx-ac/src/lib/advance-payment/advance-payment-add/advance-payment-add.component.ts
@@ -49,7 +49,7 @@ export class AdvancePaymentAddComponent extends UIComponent
   ) {
     super(inject);
     this.dialogRef = dialog;
-    this.advancedPayment = dialogData.data?.advancedPayment;
+    this.advancedPayment = {...dialogData.data?.advancedPayment};
     this.company = dialogData.data?.company;
     this.advancedPayment.currencyID = this.company.baseCurr;
     this.formType = dialogData.data?.formType;
