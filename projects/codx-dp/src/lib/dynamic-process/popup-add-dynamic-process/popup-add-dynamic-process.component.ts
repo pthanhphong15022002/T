73415dace2f7b65fc8934a60ffd9bb525b4a7069
Phúc <tdnphuck42@gmail.com>
2023-09-29 10:42:52 +0700

    ...

--- projects/codx-dp/src/lib/dynamic-process/popup-add-dynamic-process/popup-add-dynamic-process.component.ts
+++ projects/codx-dp/src/lib/dynamic-process/popup-add-dynamic-process/popup-add-dynamic-process.component.ts
@@ -1560,6 +1560,7 @@ export class PopupAddDynamicProcessComponent implements OnInit {
           }
           this.updateStepChange(this.step?.recID);
         }
+        this.changeDetectorRef.detectChanges();
       }
     });
   }
