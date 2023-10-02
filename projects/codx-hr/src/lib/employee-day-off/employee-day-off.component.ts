d21fbd6faa6807801314d4cd945061ba45947019
deedashing <phamducduy1506@gmail.com>
2023-10-02 08:24:17 +0700

    Fix view current data QTNS , Update OT

--- projects/codx-hr/src/lib/employee-day-off/employee-day-off.component.ts
+++ projects/codx-hr/src/lib/employee-day-off/employee-day-off.component.ts
@@ -348,6 +348,10 @@ export class EmployeeDayOffComponent extends UIComponent {
     this.editStatusObj = data;
     this.currentEmpObj = data.emp;
     this.formGroup.patchValue(this.editStatusObj);
+    if (!this.view.formModel.currentData) {
+      this.view.formModel.currentData = this.editStatusObj;
+    }
+
     this.dialogEditStatus = this.callfc.openForm(
       this.templateUpdateStatus,
       null,
