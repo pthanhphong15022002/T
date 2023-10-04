da8ec1cd0617183ee60f5715bb41061a8b6b909b
nvhao <nvhaohg95@gmail.com>
2023-10-04 15:52:21 +0700

    tach common service

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -3,32 +3,14 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { CommonModule } from '@angular/common';
 
-import { GroupFilterPipe } from './pipes';
 import { FormsModule } from '@angular/forms';
-import { FilterPipe } from './pipes/filterObject.pipe';
-import { DatetimePipe } from './pipes/datetime.pipe';
-import { FormatDatetimePipe } from './pipes/format-datetime.pipe';
 // import { SharedControlPipe } from './pipes/sharedcontrol.pipe';
-import { FunctionPipe } from './pipes/function.pipe';
-import { BackgroundImagePipe } from './pipes/background-image.pipe';
 import { ErrorInterceptor, JwtInterceptor } from 'codx-core';
-import { AvatarCardPipe } from './pipes/AvatarCard.pipe';
 // import { DisplayNameCardPipe } from './pipes/display-name-card.pipe';
 // import { MessagePipe } from './pipes/message.pipe';
 
 @NgModule({
-  declarations: [
-    GroupFilterPipe,
-    DatetimePipe,
-    FilterPipe,
-    DatetimePipe,
-    FormatDatetimePipe,
-    // SharedControlPipe,
-    FunctionPipe,
-    BackgroundImagePipe,
-    AvatarCardPipe,
-    // DisplayNameCardPipe,
-  ],
+  declarations: [],
   imports: [CommonModule, FormsModule],
   providers: [
     // { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
@@ -36,15 +18,6 @@ import { AvatarCardPipe } from './pipes/AvatarCard.pipe';
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     { provide: LOCALE_ID, useValue: 'vi-VN' },
   ],
-  exports: [
-    GroupFilterPipe,
-    FilterPipe,
-    FormatDatetimePipe,
-    // SharedControlPipe,
-    FunctionPipe,
-    BackgroundImagePipe,
-    AvatarCardPipe,
-    // DisplayNameCardPipe,
-  ],
+  exports: [],
 })
 export class CoreModule {}
