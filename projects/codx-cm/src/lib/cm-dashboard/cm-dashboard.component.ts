2e30053c1b76d5d99241588fdfcefb0ccafcbc58
Vo Thao <vthao@lacviet.com.vn>
2023-10-19 17:22:21 +0700

    ...

--- projects/codx-cm/src/lib/cm-dashboard/cm-dashboard.component.ts
+++ projects/codx-cm/src/lib/cm-dashboard/cm-dashboard.component.ts
@@ -252,8 +252,8 @@ export class CmDashboardComponent extends UIComponent implements AfterViewInit {
   //end
 
   //business line or linh vuc hoac dong
-  isBussinessLine = false;
-
+  isBussinessLine = true;
+  tabActiveBusIns: string = 'btBussinessLine';
   //status or
   isStatus = false;
 
@@ -462,6 +462,7 @@ export class CmDashboardComponent extends UIComponent implements AfterViewInit {
   countProcessing = 0;
   countSuccess = 0;
   countFail = 0;
+  dataSet: any[] = [];
 
   //end
   constructor(
@@ -581,11 +582,11 @@ export class CmDashboardComponent extends UIComponent implements AfterViewInit {
         break;
       //ca nha chua co ne de vay
       case 'CMD003':
-        // let lenght = dataValues.split(';')?.length ?? 0;
+        // let length = dataValues.split(';')?.length ?? 0;
 
         // let predicate =
-        //   lenght == 0 ? 'Owner =@' + lenght : ' and ' + 'Owner =@' + lenght;
-        // let dataValue = lenght == 0 ? this.user.userID : ';' + this.user.userID;
+        //   length == 0 ? 'Owner =@' + length : ' and ' + 'Owner =@' + length;
+        // let dataValue = length == 0 ? this.user.userID : ';' + this.user.userID;
 
         // predicates += predicate;
         // dataValues += dataValue;
@@ -1051,7 +1052,7 @@ export class CmDashboardComponent extends UIComponent implements AfterViewInit {
     this.resetData();
     if (method) {
       this.subscription = this.api
-        .execSv(
+        .execSv<any[]>(
           'rptcm',
           'Codx.RptBusiness.CM',
           'SalesDataSetBusiness',
@@ -1061,6 +1062,7 @@ export class CmDashboardComponent extends UIComponent implements AfterViewInit {
         .subscribe((res) => {
           if (res) {
             //xu ly nv
+
             switch (this.funcID) {
               case 'CMD001':
                 break;
@@ -1073,7 +1075,9 @@ export class CmDashboardComponent extends UIComponent implements AfterViewInit {
                 break;
             }
           }
-          this.isLoaded = true;
+          setTimeout(() => {
+            this.isLoaded = true;
+          }, 500);
         });
 
       this.detectorRef.detectChanges();
@@ -1095,8 +1099,9 @@ export class CmDashboardComponent extends UIComponent implements AfterViewInit {
     this.countSuccess = 0;
     this.countFail = 0;
     this.countProcessing = 0;
+    this.dataSet = [];
   }
-
+  // ---------------------------FUNC ----------------------------//
   //sort lấy top
   sortByProp(arr: any[], property: string, dir: string = 'asc') {
     if (arr.length && property) {
@@ -1113,16 +1118,126 @@ export class CmDashboardComponent extends UIComponent implements AfterViewInit {
     return [];
   }
 
+  random_bg_color() {
+    let x = Math.floor(Math.random() * 230);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    return 'rgb(' + x + ',' + y + ',' + z + ')';
+  }
+
+  groupBy(arr: any, key: any) {
+    return arr.reduce(function (r: any, a: any) {
+      r[a[key]] = r[a[key]] || [];
+      r[a[key]].push(a);
+      return r;
+    }, Object.create(null));
+  }
+  // ---------------------------FUNC ----------------------------//
+
   // --------------------------------------------//
   //Get DATA Ca nhan
   // --------------------------------------------//
-  changeMySales(deals) {
-    this.countNew = deals.filter(
+  changeMySales(dataSet) {
+    if (dataSet?.lenght == 0) return;
+    this.countNew = dataSet.filter(
       (x) => x.status == '1' || x.status == '0'
     )?.length;
-    this.countProcessing = deals.filter((x) => x.status == '2')?.length;
-    this.countSuccess = deals.filter((x) => x.status == '3')?.length;
-    this.countFail = deals.filter((x) => x.status == '5')?.length;
+    this.countProcessing = dataSet.filter((x) => x.status == '2')?.length;
+    let dataSuccess = dataSet.filter((x) => x.status == '3');
+    this.countSuccess = dataSuccess?.length;
+    let dataFails = dataSet.filter((x) => x.status == '5');
+    this.countFail = dataFails?.length;
+
+    this.getBusinessLine(dataSet);
+    this.getIndustries(dataSet);
+    this.getOwnerTop(dataSuccess);
+  }
+
+  getBusinessLine(dataSet) {
+    let businesLine = this.groupBy(dataSet, 'businessLineID');
+    if (businesLine) {
+      for (let key in businesLine) {
+        let color = this.random_bg_color();
+        let quantity = businesLine[key].length;
+        let obj = {
+          businessLineID: key,
+          businessLineName: businesLine[key][0].businessLineName ?? key,
+          quantity: quantity,
+          percentage: ((quantity * 100) / dataSet?.length).toFixed(2), //chua tinh
+          color: color,
+        };
+        this.dataSourceBussnessLine.push(obj);
+      }
+    }
+  }
+
+  getIndustries(dataSet) {
+    let listIndustries = this.groupBy(dataSet, 'industries');
+    if (listIndustries) {
+      for (let key in listIndustries) {
+        let color = this.random_bg_color();
+        let quantity = listIndustries[key].length;
+        let obj = {
+          industryID: key,
+          industryName:
+            listIndustries[key][0].industriesName ??
+            key ??
+            this.user.language.toUpperCase() == 'VN'
+              ? 'Chưa có'
+              : 'Not yet',
+          quantity: quantity,
+          percentage: ((quantity * 100) / dataSet?.length).toFixed(2), //chua tinh
+          color: color,
+        };
+        this.dataSourceIndustry.push(obj);
+      }
+    }
+  }
+
+  getOwnerTop(dataSet) {
+    let listOwner = this.groupBy(dataSet, 'owner');
+    if (listOwner) {
+      let owner = [];
+      for (let key in listOwner) {
+        let color = this.random_bg_color();
+        let quantity = listOwner[key].length;
+        let obj = {
+          objectID: key,
+          objectName: listOwner[key][0].ownerName ?? key,
+          quantity: quantity,
+          percentage: ((quantity * 100) / dataSet?.length).toFixed(2), //chua tinh
+          color: color,
+        };
+        owner.push(obj);
+      }
+      this.maxOwners = owner.sort((a, b) => {
+        return a.quantity - b.quantity;
+      });
+      this.minOwners = owner.sort((a, b) => {
+        return b.quantity - a.quantity;
+      });
+    }
+  }
+
+  changeBusIns(ele: any, obj: any) {
+    if (ele.id == this.tabActiveBusIns) return;
+    this.tabActiveBusIns = ele.id;
+    if (ele.id == 'btBussinessLine' && Object.keys(obj).length) {
+      !obj.chart2.pie2.element.classList.contains('d-none') &&
+        obj.chart2.pie2.element.classList.add('d-none');
+      obj.chart1.pie1.element.classList.contains('d-none') &&
+        obj.chart1.pie1.element.classList.remove('d-none');
+      obj.chart1.pie1.refresh();
+    }
+    if (ele.id == 'btIndustries' && Object.keys(obj).length) {
+      !obj.chart1.pie1.element.classList.contains('d-none') &&
+        obj.chart1.pie1.element.classList.add('d-none');
+
+      obj.chart2.pie2.element.classList.contains('d-none') &&
+        obj.chart2.pie2.element.classList.remove('d-none');
+      obj.chart2.pie2.refresh();
+    }
+    this.detectorRef.detectChanges();
   }
   // --------------------------------------------//
   //End Ca nhan
