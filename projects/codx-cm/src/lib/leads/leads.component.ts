bb4be6a5bb745c7ca7a6726f0a13482969174e24
lvietbao <lvietbao@lacviet.com.vn>
2023-11-02 09:49:25 +0700

    get list in deal

--- projects/codx-cm/src/lib/leads/leads.component.ts
+++ projects/codx-cm/src/lib/leads/leads.component.ts
@@ -1247,7 +1247,7 @@ export class LeadsComponent
                 data.processID = res[0].processID;
                 data.datas = res[0].datas;
                 data.status = res[0].status;
-                data.owner = res[0]?.owner; /// check dụm bao
+                data.owner = res[0]?.owner;
                 this.addPermission(res[0].permissions, data);
                 let datas = [data, isCheck];
                 this.getApiUpdateProcess(datas, res[1]);
