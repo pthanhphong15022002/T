75d664e7e17ff2ff9812498b2f236225aaed96d9
Vo Thao <vthao@lacviet.com.vn>
2023-11-04 13:56:25 +0700

    check other modele call

--- projects/codx-share/src/lib/components/codx-tasks/codx-tasks.service.ts
+++ projects/codx-share/src/lib/components/codx-tasks/codx-tasks.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ApiHttpService } from 'codx-core';
+import {
+  ApiHttpService,
+  CacheService,
+  CallFuncService,
+  FormModel,
+  NotificationsService,
+  SidebarModel,
+} from 'codx-core';
+import { PopupAddComponent } from './popup-add/popup-add.component';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CodxTasksService {
   urlback = '';
-  constructor(private api: ApiHttpService) {}
+
+  constructor(
+    private api: ApiHttpService,
+    private notiService: NotificationsService,
+    private callfc: CallFuncService,
+    private cache: CacheService
+  ) {}
 
   getTask(id) {
     return this.api.execSv<any>(
@@ -47,33 +61,33 @@ export class CodxTasksService {
       listDepID
     );
   }
-  getListUserIDByListPositionsID(listPositionID){
+  getListUserIDByListPositionsID(listPositionID) {
     return this.api.execSv<any>(
       'HR',
       'HR',
       'EmployeesBusiness',
       'GetListUserIDByListPositionsIDAsync',
       listPositionID
-    );  
+    );
   }
-  getListUserIDByListEmployeeID(listEmployeeID){
+  getListUserIDByListEmployeeID(listEmployeeID) {
     return this.api.execSv<any>(
       'HR',
       'HR',
       'EmployeesBusiness',
       'GetListUserIDbyListEmployeeIDAsync',
       listEmployeeID
-    );  
+    );
   }
 
-  getListUserIDByListGroupID(listGroupID){
+  getListUserIDByListGroupID(listGroupID) {
     return this.api.execSv<any>(
       'SYS',
       'AD',
       'GroupMembersBusiness',
       'GetListUserIDByListGroupIDAsync',
       listGroupID
-    );  
+    );
   }
 
   //update status
@@ -111,17 +125,22 @@ export class CodxTasksService {
   }
 
   //sendMail
-  sendAlertMail(recID: string, valueRuleNo: string, funcID: string, recIDTaskExtend : string =null) {
+  sendAlertMail(
+    recID: string,
+    valueRuleNo: string,
+    funcID: string,
+    recIDTaskExtend: string = null
+  ) {
     return this.api.execSv<any>(
       'TM',
       'TM',
       'TaskBusiness',
       'SendAlertMailAsync',
-      [recID,valueRuleNo,funcID,recIDTaskExtend]
+      [recID, valueRuleNo, funcID, recIDTaskExtend]
     );
   }
 
-  //getRecID 
+  //getRecID
   getRecIDTaskByIdAsync(id) {
     return this.api.execSv<any>(
       'TM',
@@ -150,9 +169,9 @@ export class CodxTasksService {
       taskID
     );
   }
- 
+
   //get listTaskGoad
-  getListTaskGoad(taskID){
+  getListTaskGoad(taskID) {
     return this.api.execSv<any>(
       'TM',
       'TM',
@@ -161,4 +180,148 @@ export class CodxTasksService {
       taskID
     );
   }
+
+  // edit Task từ service để modeule khác có thể gọi vào được.....................
+  editTask(
+    recID, // recID data Tasks
+    funcID, // func nghiep vu
+    isAssignTask, // la giao viec
+    titleAction, // tiêu đề form
+    afterSave?: Function, // hàm trả kết quả về
+    data = null // data Tasks - nếu có thể truyền thằng như vậy
+  ) {
+    if (data) {
+      if (data && !'00,07,09,10,20'.includes(data.status)) {
+        this.notiService.notifyCode('TM013');
+        return;
+      } else if (
+        data.category == '1' &&
+        data.verifyControl == '1' &&
+        data.status != '00'
+      ) {
+        this.notiService.notifyCode('TM014');
+        return;
+      }
+      if (data.category == '1' || data.category == '2') {
+        this.editConfirm(data, funcID, isAssignTask, titleAction);
+      } else {
+        var isCanEdit = true;
+        this.api
+          .execSv<any>(
+            'TM',
+            'ERM.Business.TM',
+            'TaskBusiness',
+            'GetListTaskChildDetailAsync',
+            data.taskID
+          )
+          .subscribe((res: any) => {
+            if (res && res?.length > 0) {
+              for (let i = 0; i < res.length; i++) {
+                let element = res[i];
+                if (element.status != '00' && element.status != '10') {
+                  isCanEdit = false;
+                  break;
+                }
+              }
+              if (!isCanEdit) {
+                this.notiService.notifyCode('TM016');
+              } else {
+                this.editConfirm(
+                  data,
+                  funcID,
+                  isAssignTask,
+                  titleAction,
+                  afterSave
+                );
+              }
+            }
+          });
+      }
+    } else {
+      this.api
+        .execSv<any>(
+          'TM',
+          'ERM.Business.TM',
+          'TaskBusiness',
+          'GetTaskUpdateByRecIDAsync',
+          recID
+        )
+        .subscribe((res: any) => {
+          if (res && res?.lenght > 0) {
+            let task = res[0];
+            let listUserDetail = res[1] || [];
+            let listTodo = res[2];
+            let listTaskResources = res[3];
+            this.editConfirm(
+              task,
+              funcID,
+              isAssignTask,
+              titleAction,
+              afterSave,
+              true,
+              listUserDetail,
+              listTodo,
+              listTaskResources
+            );
+          }
+        });
+    }
+  }
+  editConfirm(
+    data,
+    funcID,
+    isAssignTask,
+    titleAction,
+    afterSave?: Function,
+    isLoadedData = false,
+    listUserDetail = [],
+    listTodo = [],
+    listTaskResources = []
+  ) {
+    this.cache.functionList(funcID).subscribe((f) => {
+      if (f) {
+        let formModel = new FormModel();
+        let option = new SidebarModel();
+        this.cache.gridView(f.gridViewName).subscribe((res) => {
+          this.cache
+            .gridViewSetup(f.formName, f.gridViewName)
+            .subscribe((grvSetup) => {
+              if (grvSetup) {
+                formModel.funcID = funcID;
+                formModel.entityName = f.entityName;
+                formModel.formName = f.formName;
+                formModel.gridViewName = f.gridViewName;
+                option.FormModel = formModel;
+                option.Width = '800px';
+                option.zIndex = 1001;
+
+                let obj = {
+                  data: data,
+                  action: 'edit',
+                  isAssignTask: isAssignTask,
+                  titleAction: titleAction,
+                  functionID: funcID,
+                  isOtherModule: true,
+                  isLoadedData: isLoadedData,
+                  listUserDetail: listUserDetail,
+                  listTodo: listTodo,
+                  listTaskResources: listTaskResources,
+                };
+                let dialog = this.callfc.openSide(
+                  PopupAddComponent,
+                  obj,
+                  option
+                );
+                dialog.closed.subscribe((e) => {
+                  if (e?.event && e?.event != null) {
+                    afterSave(e.event);
+                  }
+                });
+              }
+            });
+        });
+      }
+    });
+  }
+  //end
 }
