f2133a6b280b5b0871fedcf6888610af105240d9
LocTran <loctt.cntp@gamil.com>
2023-11-08 14:09:32 +0700

    ...

--- projects/codx-co/src/lib/calendar/calendar-center/calendar-center.component.ts
+++ projects/codx-co/src/lib/calendar/calendar-center/calendar-center.component.ts
@@ -89,11 +89,9 @@ export class CalendarCenterComponent
         this.codxSchedule.dataSource = this.events;
         this.codxSchedule.selectedDate = this.selectedDate ?? new Date();
         this.codxSchedule.statusColor = this.statusColor;
-        setTimeout(() => {
-          this.codxSchedule.currentView = "Month";
-          this.codxSchedule.onTimelineViewChange(false);
-          this.codxSchedule.setEventSettings();
-        }, 3000);
+        this.codxSchedule.isCalendarView = true;
+        this.codxSchedule.onTimelineViewChange(false);
+        this.codxSchedule.setEventSettings();
       }
     },500)
   }
@@ -122,7 +120,6 @@ export class CalendarCenterComponent
     if(this.codxSchedule)
     {
       this.codxSchedule.resourceDataSource = resources;
-      // this.codxSchedule.isCalendarView = true;
       this.codxSchedule.onGroupingChange(resources);
       this.codxSchedule.onGridlinesChange(true);
       this.codxSchedule.onTimelineViewChange(true);
@@ -130,11 +127,12 @@ export class CalendarCenterComponent
     }
   }
 
-  //
+  // change mode calendar <-> schedule
   changeModeView(isCalendarView:boolean){
     if(this.codxSchedule)
     {
       this.codxSchedule.isCalendarView = isCalendarView;
+      this.codxSchedule.onTimelineViewChange(isCalendarView);
       this.codxSchedule.setEventSettings();
     }
   }
