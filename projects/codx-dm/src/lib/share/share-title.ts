cabf45db52d58ec0d7bf9d0452344232c89ce7b4
chungthai7596 <bachung7596@gmail.com>
2023-12-14 13:57:26 +0700

    ..

---
+++ projects/codx-dm/src/lib/share/share-title.ts
@@ -0,0 +1,13 @@
+export var shareTitle = 
+{
+    sharingRange: "Phạm vi chia sẻ",
+    auth: "Yêu cầu xác thực khi chia sẻ",
+    shareOTP: "Gửi OTP xác nhận",
+    shareOTPSub: "Hệ thống sẽ gửi OTP đến email người dùng được chia sẻ đường dẫn",
+    pwOTP: "Mật khẩu cố định",
+    pwOTPSub: "Người dùng nhập mật khẩu trước khi truy cập vào đường dẫn",
+    noPw: "Không xác thực",
+    noPwSub: "Bất kì ai cũng không cần xác thực khi truy cập vào đường dẫn",
+    copyLink: "Sao chép đường dẫn",
+    shareObject: "Đối tượng được chia sẻ"
+}
