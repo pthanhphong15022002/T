ad545202f78efe4df2e4704f9a1ee7a960f5b41c
vuhiepp07 <vuhiep2k107@gmail.com>
2023-12-15 17:57:46 +0700

    check

---
+++ projects/codx-pr/src/lib/employee-kowds/popup-copy-ekowds/popup-copy-ekowds.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PopupCopyEkowdsComponent } from './popup-copy-ekowds.component';
+
+describe('PopupCopyEkowdsComponent', () => {
+  let component: PopupCopyEkowdsComponent;
+  let fixture: ComponentFixture<PopupCopyEkowdsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PopupCopyEkowdsComponent]
+    });
+    fixture = TestBed.createComponent(PopupCopyEkowdsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
