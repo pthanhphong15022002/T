401c943e52ed7b47e8107ad0976be67508048dc5
Vo Thao <vthao@lacviet.com.vn>
2023-12-28 16:51:43 +0700

    check gom component

--- projects/codx-share/src/lib/components/codx-input-custom-field/codx-fields-detail-temp/popup-custom-field/popup-custom-field.component.ts
+++ projects/codx-share/src/lib/components/codx-input-custom-field/codx-fields-detail-temp/popup-custom-field/popup-custom-field.component.ts
@@ -6,6 +6,7 @@ import {
   DialogRef,
   NotificationsService,
 } from 'codx-core';
+import { CustomFieldService } from '../../custom-field.service';
 
 @Component({
   selector: 'lib-popup-custom-field',
@@ -35,6 +36,7 @@ export class PopupCustomFieldComponent implements OnInit {
     private cache: CacheService,
     private api: ApiHttpService,
     private notiService: NotificationsService,
+    private customFieldSV: CustomFieldService,
     @Optional() dt?: DialogData,
     @Optional() dialog?: DialogRef
   ) {
@@ -185,7 +187,9 @@ export class PopupCustomFieldComponent implements OnInit {
 
       if (!dataFormat.includes('[')) {
         //tinh toán
-        obj.dataValue = this.caculate(dataFormat);
+        // obj.dataValue = this.caculate(dataFormat);
+        obj.dataValue = this.customFieldSV.caculate(dataFormat, this.point);
+
         //tính toan end
         let index = this.fields.findIndex((x) => x.recID == obj.recID);
         if (index != -1) {
@@ -195,134 +199,134 @@ export class PopupCustomFieldComponent implements OnInit {
     });
   }
 
-  caculate(stringMath) {
-    if (stringMath.includes('_')) return stringMath;
-    if (this.isExitOperator(this.arrCheck, stringMath)) {
-      if (this.isExitOperator(this.parenthesis, stringMath)) {
-        //có ngoặc => chưa làm
-      } else if (this.isExitOperator(this.operator, stringMath)) {
-        //chi la phep toan
-        if (this.isExitOperator(this.operatorMulDiv, stringMath)) {
-          // co nhan chia
-          stringMath = this.sumAndMul(stringMath);
-        } else {
-          stringMath = this.sumSub(stringMath);
-        }
-      }
-    }
-    return stringMath;
-  }
+  // caculate(stringMath) {
+  //   if (stringMath.includes('_')) return stringMath;
+  //   if (this.isExitOperator(this.arrCheck, stringMath)) {
+  //     if (this.isExitOperator(this.parenthesis, stringMath)) {
+  //       //có ngoặc => chưa làm
+  //     } else if (this.isExitOperator(this.operator, stringMath)) {
+  //       //chi la phep toan
+  //       if (this.isExitOperator(this.operatorMulDiv, stringMath)) {
+  //         // co nhan chia
+  //         stringMath = this.sumAndMul(stringMath);
+  //       } else {
+  //         stringMath = this.sumSub(stringMath);
+  //       }
+  //     }
+  //   }
+  //   return stringMath;
+  // }
 
-  //phep toan co ban
-  sumSub(stringMath, opera = '+') {
-    if (!stringMath || !this.isExitOperator(this.operatorAddMinus, stringMath))
-      return stringMath;
-    let sum = 0;
-    let num = stringMath[0];
+  // //phep toan co ban
+  // sumSub(stringMath, opera = '+') {
+  //   if (!stringMath || !this.isExitOperator(this.operatorAddMinus, stringMath))
+  //     return stringMath;
+  //   let sum = 0;
+  //   let num = stringMath[0];
 
-    for (var i = 1; i < stringMath.length; i++) {
-      if (this.operatorAddMinus.includes(stringMath[i])) {
-        num = this.converCommaDot(num);
-        if (opera == '+') {
-          sum += Number.parseFloat(num);
-        } else {
-          sum -= Number.parseFloat(num);
-        }
-        num = '';
-        opera = stringMath[i];
-      } else {
-        num += stringMath[i];
-      }
-    }
-    num = this.converCommaDot(num);
-    if (opera == '+') {
-      sum += Number.parseFloat(num);
-    } else {
-      sum -= Number.parseFloat(num);
-    }
-    return sum.toString();
-  }
+  //   for (var i = 1; i < stringMath.length; i++) {
+  //     if (this.operatorAddMinus.includes(stringMath[i])) {
+  //       num = this.converCommaDot(num);
+  //       if (opera == '+') {
+  //         sum += Number.parseFloat(num);
+  //       } else {
+  //         sum -= Number.parseFloat(num);
+  //       }
+  //       num = '';
+  //       opera = stringMath[i];
+  //     } else {
+  //       num += stringMath[i];
+  //     }
+  //   }
+  //   num = this.converCommaDot(num);
+  //   if (opera == '+') {
+  //     sum += Number.parseFloat(num);
+  //   } else {
+  //     sum -= Number.parseFloat(num);
+  //   }
+  //   return sum.toString();
+  // }
 
-  //phep nhan chia
-  multDiv(stringMath, opera = 'x') {
-    if (!stringMath || !this.isExitOperator(this.operatorMulDiv, stringMath))
-      return stringMath;
-    let mul = 1;
-    let num = stringMath[0];
+  // //phep nhan chia
+  // multDiv(stringMath, opera = 'x') {
+  //   if (!stringMath || !this.isExitOperator(this.operatorMulDiv, stringMath))
+  //     return stringMath;
+  //   let mul = 1;
+  //   let num = stringMath[0];
 
-    for (var i = 1; i < stringMath.length; i++) {
-      if (this.operatorMulDiv.includes(stringMath[i])) {
-        num = this.converCommaDot(num);
-        if (opera == 'x') {
-          mul = mul * Number.parseFloat(num);
-        } else {
-          if (Number.parseFloat(num) == 0) return '_'; //ko chia dc cho 0
-          mul = mul / Number.parseFloat(num);
-        }
-        num = '';
-        opera = stringMath[i];
-      } else {
-        num += stringMath[i];
-      }
-    }
-    num = this.converCommaDot(num);
-    if (opera == 'x') {
-      mul = mul * Number.parseFloat(num);
-    } else {
-      if (Number.parseFloat(num) == 0) return '_'; //ko chia dc cho 0
-      mul = mul / Number.parseFloat(num);
-    }
-    return mul.toString();
-  }
+  //   for (var i = 1; i < stringMath.length; i++) {
+  //     if (this.operatorMulDiv.includes(stringMath[i])) {
+  //       num = this.converCommaDot(num);
+  //       if (opera == 'x') {
+  //         mul = mul * Number.parseFloat(num);
+  //       } else {
+  //         if (Number.parseFloat(num) == 0) return '_'; //ko chia dc cho 0
+  //         mul = mul / Number.parseFloat(num);
+  //       }
+  //       num = '';
+  //       opera = stringMath[i];
+  //     } else {
+  //       num += stringMath[i];
+  //     }
+  //   }
+  //   num = this.converCommaDot(num);
+  //   if (opera == 'x') {
+  //     mul = mul * Number.parseFloat(num);
+  //   } else {
+  //     if (Number.parseFloat(num) == 0) return '_'; //ko chia dc cho 0
+  //     mul = mul / Number.parseFloat(num);
+  //   }
+  //   return mul.toString();
+  // }
 
-  // pheptoan + ,-,x,/
-  sumAndMul(stringMath, haveSum = false) {
-    if (!stringMath || !this.isExitOperator(this.operator, stringMath))
-      return stringMath;
-    if (stringMath.includes('+')) {
-      let arrAdd = stringMath.trim().split('+');
-      if (arrAdd?.length > 0) {
-        let arrRes = arrAdd.map((str) => {
-          return this.sumAndMul(str, true);
-        });
-        // stringMath = this.sumAndMul(arrRes.join('+'));
-        return this.sumSub(arrRes.join('+'));
-      }
-    } else if (stringMath.includes('-')) {
-      let arrMunus = stringMath.trim().split('-');
-      if (arrMunus?.length > 0) {
-        let arrRes = arrMunus.map((str) => {
-          return this.sumAndMul(str);
-        });
-        stringMath = arrRes.join('-');
-        if (!haveSum) return this.sumSub(stringMath);
-      }
-    } else stringMath = this.multDiv(stringMath);
-    return stringMath;
-  }
-  //check tồn tại
-  isExitOperator(arrOperator, string) {
-    var check = false;
-    arrOperator.forEach((op) => {
-      if (string.includes(op)) {
-        check = true;
-        return;
-      }
-    });
-    return check;
-  }
-  //check undifine
-  isUndifine(string) {
-    return string.includes('_');
-  }
-  //xác định dấu "." hay "," ngăn cách phần thập phân
-  converCommaDot(num) {
-    if (this.point == ',' && num.includes('.')) {
-      num = num.replaceAll('.', ',');
-    } else if (this.point == '.' && num.includes(','))
-      num = num.replaceAll(',', '.');
-    return num;
-  }
+  // // pheptoan + ,-,x,/
+  // sumAndMul(stringMath, haveSum = false) {
+  //   if (!stringMath || !this.isExitOperator(this.operator, stringMath))
+  //     return stringMath;
+  //   if (stringMath.includes('+')) {
+  //     let arrAdd = stringMath.trim().split('+');
+  //     if (arrAdd?.length > 0) {
+  //       let arrRes = arrAdd.map((str) => {
+  //         return this.sumAndMul(str, true);
+  //       });
+  //       // stringMath = this.sumAndMul(arrRes.join('+'));
+  //       return this.sumSub(arrRes.join('+'));
+  //     }
+  //   } else if (stringMath.includes('-')) {
+  //     let arrMunus = stringMath.trim().split('-');
+  //     if (arrMunus?.length > 0) {
+  //       let arrRes = arrMunus.map((str) => {
+  //         return this.sumAndMul(str);
+  //       });
+  //       stringMath = arrRes.join('-');
+  //       if (!haveSum) return this.sumSub(stringMath);
+  //     }
+  //   } else stringMath = this.multDiv(stringMath);
+  //   return stringMath;
+  // }
+  // //check tồn tại
+  // isExitOperator(arrOperator, string) {
+  //   var check = false;
+  //   arrOperator.forEach((op) => {
+  //     if (string.includes(op)) {
+  //       check = true;
+  //       return;
+  //     }
+  //   });
+  //   return check;
+  // }
+  // //check undifine
+  // isUndifine(string) {
+  //   return string.includes('_');
+  // }
+  // //xác định dấu "." hay "," ngăn cách phần thập phân
+  // converCommaDot(num) {
+  //   if (this.point == ',' && num.includes('.')) {
+  //     num = num.replaceAll('.', ',');
+  //   } else if (this.point == '.' && num.includes(','))
+  //     num = num.replaceAll(',', '.');
+  //   return num;
+  // }
   //Decimal point separation
   decimalPointSeparation() {
     const string1 = '1,23'; //parFloat
@@ -337,10 +341,10 @@ export class PopupCustomFieldComponent implements OnInit {
     }
   }
 
-  agv(arr: Array<number>) {
-    let sum = 0;
-    arr.forEach((n) => (sum += n));
-    return sum / arr.length;
-  }
+  // agv(arr: Array<number>) {
+  //   let sum = 0;
+  //   arr.forEach((n) => (sum += n));
+  //   return sum / arr.length;
+  // }
   //------------------END_CACULATE--------------------//
 }
