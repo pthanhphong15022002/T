fdad57b9f5b4677f1aa3d4c8423b33c785c53bfa
chungthai7596 <bachung7596@gmail.com>
2024-01-09 11:13:06 +0700

    .

--- projects/codx-share/src/lib/codx-share.service.ts
+++ projects/codx-share/src/lib/codx-share.service.ts
@@ -170,6 +170,37 @@ export class CodxShareService {
     this.cachedObservables.set(key, observable);
     return observable;
   }
+  loadValueList(vll: any): Observable<any> {
+    let paras = ['ValueList', vll];
+    let keyRoot = 'ValueList' + vll;
+    let key = JSON.stringify(paras).toLowerCase();
+    if (this.caches.has(keyRoot)) {
+      var c = this.caches.get(keyRoot);
+      if (c && c.has(key)) {
+        return c.get(key);
+      }
+    } else {
+      this.caches.set(keyRoot, new Map<any, any>());
+    }
+
+    if (this.cachedObservables.has(key)) {
+      this.cachedObservables.get(key);
+    }
+    let observable = this.cache.valueList(vll).pipe(
+      map((res) => {
+        if (res) {
+          let c = this.caches.get(keyRoot);
+          c?.set(key, res);
+          return res;
+        }
+        return null;
+      }),
+      share(),
+      finalize(() => this.cachedObservables.delete(key))
+    );
+    this.cachedObservables.set(key, observable);
+    return observable;
+  }
   loadFuncID(functionID: any): Observable<any> {
     let paras = [functionID];
     let keyRoot = 'MFunc' + functionID;
