1af0dc2b8850a4553061580fdf42de944b7fe65f
chungthai7596 <bachung7596@gmail.com>
2024-01-17 09:11:08 +0700

    .

--- projects/codx-bp/src/lib/modeview/properties/base.component.ts
+++ projects/codx-bp/src/lib/modeview/properties/base.component.ts
@@ -1,19 +1,26 @@
 import { ChangeDetectorRef, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from "@angular/core";
+import { ApiHttpService, AuthStore, CallFuncService } from "codx-core";
 import { CodxShareService } from "projects/codx-share/src/public-api";
+import { CodxBpService } from "../../codx-bp.service";
 
 @Component({ template: '' })
 export abstract class BasePropertyComponent
 {
+    user:any;
     @Input() isBack = false;
     @Input() data: any;
     @Output() dataChange = new EventEmitter<any>();
     @Output() back = new EventEmitter<any>();
     constructor(
+      private authstore: AuthStore,
+      public api: ApiHttpService,
       public shareService: CodxShareService,
-      public ref: ChangeDetectorRef
+      public ref: ChangeDetectorRef,
+      public bpService: CodxBpService,
+      public callFuc: CallFuncService
     )
     {
-
+      this.user = this.authstore.get();
     }
 
     changeValue(e:any)
