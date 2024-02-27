a35f2599c425c3876e38111ce6a2e4ebb67e6b6c
nvhao <nvhaohg95@gmail.com>
2024-02-27 15:22:13 +0700

    fix lỗi k hiện thị phân quyền khi xem chi tiết user

--- projects/codx-share/src/lib/components/codx-share-report/codx-share-report.component.ts
+++ projects/codx-share/src/lib/components/codx-share-report/codx-share-report.component.ts
@@ -1,7 +1,13 @@
-import { AfterViewInit, Component, Injector, Input, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Injector,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { WSUIComponent } from 'projects/codx-ws/src/lib/default/wsui.component';
 import { CodxView2Component } from '../codx-view2/codx-view2.component';
-import { FormModel, Util } from 'codx-core';
+import { CacheService, FormModel, Util } from 'codx-core';
 import { isObservable } from 'rxjs';
 import { BookmarkComponent } from 'projects/codx-ws/src/lib/bookmark/bookmark.component';
 import { CodxShareService } from '../../codx-share.service';
@@ -9,265 +15,280 @@ import { CodxShareService } from '../../codx-share.service';
 @Component({
   selector: 'codx-share-report',
   templateUrl: './codx-share-report.component.html',
-  styleUrls: ['./codx-share-report.component.scss']
+  styleUrls: ['./codx-share-report.component.scss'],
 })
+export class CodxShareReportComponent
+  extends WSUIComponent
+  implements AfterViewInit
+{
+  @ViewChild('codxView2') codxView2?: CodxView2Component;
 
-export class CodxShareReportComponent extends WSUIComponent implements AfterViewInit{
+  @Input() listModule: any;
+  @Input() isToolBar: any = true;
 
-  @ViewChild('codxView2') codxView2: CodxView2Component;
-
-  @Input() listModule:any;
-  @Input() isToolBar:any = true;
-
-  viewID = "1";
-  listReport:any;
+  viewID = '1';
+  listReport: any;
   listReports: any;
   listBookMarks = [];
   listGroupReport = [];
   countBookMarks = 0;
-  selectedToolBar = "All";
-  imgDefault = "assets/themes/ws/default/img/Report_Empty.svg";
+  selectedToolBar = 'All';
+  imgDefault = 'assets/themes/ws/default/img/Report_Empty.svg';
   dataModel = new FormModel();
   shareService: CodxShareService;
-  constructor(
-    inject: Injector,
-  ) 
-  {
+  title: string = '';
+  constructor(inject: Injector, private cache: CacheService) {
     super(inject);
     this.shareService = inject.get(CodxShareService);
   }
 
   override onInit(): void {
+    this.cache.functionList(this.funcID).subscribe((f) => {
+      if (f) this.title = f.customName;
+    });
     this.formatListGroupReport();
     this.getModuleByUserID();
     this.getCountBookMark();
-    if(this.listModule) this.getDashboardOrReport("R", this.listModule);
+    if (this.listModule) this.getDashboardOrReport('R', this.listModule);
   }
 
   ngAfterViewInit(): void {
     this.setCSS();
   }
 
-  getCountBookMark()
-  {
+  getCountBookMark() {
     let widthBody = document.body.offsetWidth - 40;
     this.countBookMarks = Math.ceil(widthBody / 260);
   }
 
-  setCSS()
-  {
-    var elems = document.querySelectorAll(".header-fixed");
+  setCSS() {
+    var elems = document.querySelectorAll('.header-fixed');
 
-    [].forEach.call(elems, function(el) {
-        el.classList.remove("toolbar-fixed");
+    [].forEach.call(elems, function (el) {
+      el.classList.remove('toolbar-fixed');
     });
   }
 
-  formatListGroupReport()
-  {
-    var obj = 
-    {
-      functionID : "All",
-      customName : "Tất cả"
-    }
+  formatListGroupReport() {
+    var obj = {
+      functionID: 'All',
+      customName: 'Tất cả',
+    };
     this.listGroupReport.push(obj);
   }
 
-  getModuleByUserID()
-  {
-    if(this.listModule) return;
-    var module = this.codxWsService.loadModuleByUserID(this.userInfo?.userID) as any;
-    if(isObservable(module))
-    {
-      module.subscribe((item:any)=>{
-        if(item) {
-          this.listModule = item.join(";");
-          this.getDashboardOrReport("R", this.listModule);
+  getModuleByUserID() {
+    if (this.listModule) return;
+    var module = this.codxWsService.loadModuleByUserID(
+      this.userInfo?.userID
+    ) as any;
+    if (isObservable(module)) {
+      module.subscribe((item: any) => {
+        if (item) {
+          this.listModule = item.join(';');
+          this.getDashboardOrReport('R', this.listModule);
         }
-      })
-    }
-    else
-    {
-      this.listModule = module.join(";");
-      this.getDashboardOrReport("R", this.listModule);
+      });
+    } else {
+      this.listModule = module.join(';');
+      this.getDashboardOrReport('R', this.listModule);
     }
   }
 
-  getDashboardOrReport(type:any,listModule:any)
-  {
-    var result = this.codxWsService.loadDashboardOrReport(type,listModule.toUpperCase()) as any;
-    if(isObservable(result))
-    {
-      result.subscribe((item:any)=>{
-        if(item) {
+  getDashboardOrReport(type: any, listModule: any) {
+    var result = this.codxWsService.loadDashboardOrReport(
+      type,
+      listModule.toUpperCase()
+    ) as any;
+    if (isObservable(result)) {
+      result.subscribe((item: any) => {
+        if (item) {
           var results = this.formatBookMark(item);
           this.listReport = results;
-          this.listReports = JSON.parse(JSON.stringify(results)) ; //this.formatData(item);
+          this.listReports = JSON.parse(JSON.stringify(results)); //this.formatData(item);
           this.formatData(this.listReport);
         }
-      })
-    }
-    else {
+      });
+    } else {
       var results = this.formatBookMark(result);
-      this.listReport = results; 
+      this.listReport = results;
       this.listReports = JSON.parse(JSON.stringify(results));
       this.formatData(this.listReport);
     } //this.formatData(result);
   }
 
-  formatBookMark(data:any)
-  {
-    data.forEach(element => {
+  formatBookMark(data: any) {
+    data.forEach((element) => {
       element.isBookMark = false;
-      if(element.bookmarks && element.bookmarks.length > 0)
-      {
-        var dt = element.bookmarks.filter(x=>x.objectID == this.userInfo?.userID);
-        if(dt && dt.length > 0) {
+      if (element.bookmarks && element.bookmarks.length > 0) {
+        var dt = element.bookmarks.filter(
+          (x) => x.objectID == this.userInfo?.userID
+        );
+        if (dt && dt.length > 0) {
           this.listBookMarks.push(element);
           element.isBookMark = true;
         }
       }
     });
-    
-    return data
+
+    return data;
   }
 
-  getFuncListByModules(data:any)
-  {
+  getFuncListByModules(data: any) {
     var result = this.codxWsService.loadListFucByListModule(data) as any;
-    if(isObservable(result))
-    {
-      result.subscribe((item:any)=>{
-        if(item) {
+    if (isObservable(result)) {
+      result.subscribe((item: any) => {
+        if (item) {
           this.formatData2(item);
         }
-      })
-    }
-    else this.formatData2(result);
+      });
+    } else this.formatData2(result);
   }
 
-  formatData(data:any)
-  {
-    if(this.funcID.includes("WS")) 
-    {
-      var listModule = data.map(function(item){return item.moduleID});
+  formatData(data: any) {
+    if (this.funcID.includes('WS')) {
+      var listModule = data.map(function (item) {
+        return item.moduleID;
+      });
       listModule = this.removeDuplicates(listModule);
-      this.getFuncListByModules(JSON.stringify(listModule))
-    }
-    else 
-    {
-      let vll = this.shareService.loadValueList("RP001") as any;
+      this.getFuncListByModules(JSON.stringify(listModule));
+    } else {
+      let vll = this.shareService.loadValueList('RP001') as any;
 
-      if(isObservable(vll))
-      {
-        vll.subscribe((item:any)=>{
-          this.formatVll(data,item.datas);
-        })
-      }
-      else this.formatVll(data,vll.datas);
+      if (isObservable(vll)) {
+        vll.subscribe((item: any) => {
+          this.formatVll(data, item.datas);
+        });
+      } else this.formatVll(data, vll.datas);
     }
-    
   }
 
-  formatVll(data:any,vll:any)
-  {
+  formatVll(data: any, vll: any) {
     var listCategory = [];
-    data.forEach(elm => {
-      var text = vll.filter(x=>x.value == elm.category);
-      if(text.length > 0)
-      {
+    data.forEach((elm) => {
+      var text = vll.filter((x) => x.value == elm.category);
+      if (text.length > 0) {
         var name = text[0].text;
-        if(!listCategory.some(x=>x.customName == name))
-        {
-          listCategory.push(
-            {
-              functionID: elm.category,
-              customName: name
-            }
-          )
+        if (!listCategory.some((x) => x.customName == name)) {
+          listCategory.push({
+            functionID: elm.category,
+            customName: name,
+          });
         }
       }
     });
     this.formatData2(listCategory);
   }
 
-  formatData2(data:any)
-  {
+  formatData2(data: any) {
     this.listGroupReport = this.listGroupReport.concat(data);
   }
 
-
-  removeDuplicates(arr:any) {
+  removeDuplicates(arr: any) {
     return [...new Set(arr)];
   }
 
-  selectedChange(data:any)
-  {
-    this.codxService.navigate('', "/" + data.moduleID.toLowerCase() + '/report/detail/' + data.recID);
+  selectedChange(data: any) {
+    this.codxService.navigate(
+      '',
+      '/' + data.moduleID.toLowerCase() + '/report/detail/' + data.recID
+    );
     this.codxWsService.functionID = data.reportID;
     data.functionID = data.reportID;
     this.codxWsService.listBreadCumb.push(data);
   }
 
-  selectedChangeToolBar(data:any)
-  {
+  selectedChangeToolBar(data: any) {
     this.selectedToolBar = data?.functionID;
-    if(this.selectedToolBar == "All") this.listReport = JSON.parse(JSON.stringify(this.listReports));
-    else if(this.funcID.includes("WS")) this.listReport = JSON.parse(JSON.stringify(this.listReports.filter(x=>x.moduleID == this.selectedToolBar)));
-    else this.listReport = JSON.parse(JSON.stringify(this.listReports.filter(x=>x.category == this.selectedToolBar)));
+    if (this.selectedToolBar == 'All')
+      this.listReport = JSON.parse(JSON.stringify(this.listReports));
+    else if (this.funcID.includes('WS'))
+      this.listReport = JSON.parse(
+        JSON.stringify(
+          this.listReports.filter((x) => x.moduleID == this.selectedToolBar)
+        )
+      );
+    else
+      this.listReport = JSON.parse(
+        JSON.stringify(
+          this.listReports.filter((x) => x.category == this.selectedToolBar)
+        )
+      );
   }
 
-  setBookMark(recID:any)
-  {
-    this.api.execSv("rptrp","Codx.RptBusiness.RP","ReportBusiness","BookmarkAsync",recID).subscribe(item=>{
-      if(item)
-      {
-        var className = "opacity-100";
-        var messCode = "OD002";
-        var index = this.listReport.findIndex(x=>x.recID == recID);
-        var index2 = this.listReports.findIndex(x=>x.recID == recID);
-        if(index2 >= 0)  this.listReports[index2].isBookMark = !this.listReports[index2].isBookMark;
-        if(index >= 0) {
+  setBookMark(recID: any) {
+    this.api
+      .execSv(
+        'rptrp',
+        'Codx.RptBusiness.RP',
+        'ReportBusiness',
+        'BookmarkAsync',
+        recID
+      )
+      .subscribe((item) => {
+        if (item) {
+          var className = 'opacity-100';
+          var messCode = 'OD002';
+          var index = this.listReport.findIndex((x) => x.recID == recID);
+          var index2 = this.listReports.findIndex((x) => x.recID == recID);
+          if (index2 >= 0)
+            this.listReports[index2].isBookMark =
+              !this.listReports[index2].isBookMark;
+          if (index >= 0) {
+            this.listReport[index].isBookMark =
+              !this.listReport[index].isBookMark;
 
-          this.listReport[index].isBookMark = !this.listReport[index].isBookMark;
-          
-          if(!this.listReport[index].isBookMark)
-          {
-            className = "opacity-25";
-            messCode = "OD003";
-            this.listBookMarks = this.listBookMarks.filter(x=>x.recID != this.listReport[index].recID);
-            if(this.listReports[index2].bookmarks &&  this.listReports[index2].bookmarks.length > 0)
-            this.listReports[index2].bookmarks = this.listReports[index2].bookmarks.filter(x=>x.objectID != this.userInfo.userID);
+            if (!this.listReport[index].isBookMark) {
+              className = 'opacity-25';
+              messCode = 'OD003';
+              this.listBookMarks = this.listBookMarks.filter(
+                (x) => x.recID != this.listReport[index].recID
+              );
+              if (
+                this.listReports[index2].bookmarks &&
+                this.listReports[index2].bookmarks.length > 0
+              )
+                this.listReports[index2].bookmarks = this.listReports[
+                  index2
+                ].bookmarks.filter((x) => x.objectID != this.userInfo.userID);
+            } else {
+              this.listBookMarks.unshift(this.listReport[index]);
+              if (!this.listReports[index2].bookmarks)
+                this.listReports[index2].bookmarks = [];
+              this.listReports[index2].bookmarks.push({
+                objectID: this.userInfo.userID,
+              });
+            }
+
+            //Bookmark report
+            document
+              .getElementById(
+                'ws-report-bookmark' + this.listReport[index].recID
+              )
+              .classList.add(className);
+
+            //Noti
+            this.notifySvr.notifyCode(messCode, 0, this.userInfo?.userName);
+
+            //Update cache
+            let paras = ['R', this.listModule];
+            let keyRoot = 'WSDR' + 'R' + this.listModule;
+            let key = JSON.stringify(paras).toLowerCase();
+            this.codxWsService.updateCache(keyRoot, key, this.listReports);
           }
-          else  {
-            this.listBookMarks.unshift(this.listReport[index]);
-            if(!this.listReports[index2].bookmarks) this.listReports[index2].bookmarks = [];
-              this.listReports[index2].bookmarks.push({objectID:this.userInfo.userID});
-          }
-
-          //Bookmark report
-          document.getElementById("ws-report-bookmark" + this.listReport[index].recID).classList.add(className);
-          
-          //Noti
-          this.notifySvr.notifyCode(messCode,0,this.userInfo?.userName);
-
-          //Update cache
-          let paras = ["R",this.listModule];
-          let keyRoot = "WSDR" + "R" + this.listModule;
-          let key = JSON.stringify(paras).toLowerCase();
-          this.codxWsService.updateCache(keyRoot,key,this.listReports);
         }
-      }
+      });
+  }
+
+  selectMoreBookmark() {
+    this.callFunc.openForm(BookmarkComponent, '', 900, 700, '', {
+      listGroup: this.listGroupReport,
+      listBookMarks: this.listBookMarks,
+      type: 'R',
     });
   }
 
-  selectMoreBookmark()
-  {
-    this.callFunc.openForm(BookmarkComponent,"",900,700,"",{listGroup:this.listGroupReport,listBookMarks:this.listBookMarks,type:'R'});
-  }
-
-  viewChange(e:any)
-  {
+  viewChange(e: any) {
     this.viewID = e;
   }
 }
