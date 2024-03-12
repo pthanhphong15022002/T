99d50e383dc14ed4304f03ffbd01c639ff22a24d
Phúc <tdnphuck42@gmail.com>
2024-03-12 10:57:50 +0700

    ui form and thu hoi

--- projects/codx-bp/src/lib/bp-tasks/popup-bp-tasks/popup-bp-tasks.component.ts
+++ projects/codx-bp/src/lib/bp-tasks/popup-bp-tasks/popup-bp-tasks.component.ts
@@ -18,6 +18,7 @@ import {
 } from 'codx-core';
 import { AttachmentComponent } from 'projects/codx-common/src/lib/component/attachment/attachment.component';
 import { PopupSignForApprovalComponent } from 'projects/codx-es/src/lib/sign-file/popup-sign-for-approval/popup-sign-for-approval.component';
+import { CodxEmailComponent } from 'projects/codx-share/src/lib/components/codx-email/codx-email.component';
 import { CodxShareService } from 'projects/codx-share/src/public-api';
 import { isObservable } from 'rxjs';
 
@@ -69,88 +70,90 @@ export class PopupBpTasksComponent implements OnInit {
   }
   ngOnInit(): void {
     this.checkList = this.data.checkList ?? [];
-    this.countCheck = this.checkList.filter(x => x.status == '1').length;
+    this.countCheck = this.checkList.filter((x) => x.status == '1').length;
     this.getInfo();
     this.getVll();
 
-    this.api
-      .execSv<any>(
-        'BP',
-        'BP',
-        'ProcessInstancesBusiness',
-        'GetItemsByInstanceIDAsync',
-        [this.data.instanceID]
-      )
-      .subscribe((ins) => {
-        if (ins) {
-          this.dataIns = ins;
-          this.getFile();
-        }
-      });
-
-
-    if (this.subTitle == null) {
-      this.subTitle = this.dataIns?.title;
+    if (this.dataIns == null) {
+      this.api
+        .execSv<any>(
+          'BP',
+          'BP',
+          'ProcessInstancesBusiness',
+          'GetItemsByInstanceIDAsync',
+          [this.data.instanceID]
+        )
+        .subscribe((ins) => {
+          if (ins) {
+            this.dataIns = ins;
+            this.getFile();
+          }
+        });
+    } else {
+      if (this.subTitle == null) {
+        this.subTitle = this.dataIns?.title;
+      }
+      this.getFile();
     }
   }
-  getFile(){
-    if(this.dataIns!=null){
-          this.fileIDs=[];
-          if (this.dataIns?.documentControl?.length > 0) {
-            //let curStepDmc= this.dataIns?.documentControl;
-            let curStepDmc= this.dataIns?.documentControl.filter(x=>x?.stepID == this.data?.stepID );
-            if(curStepDmc?.length>0){
-              curStepDmc?.forEach(dmc=>{
-                if(dmc?.files?.length>0){
-                  dmc?.files?.forEach(file=>{
-                    if(file?.type=="1" || file?.type=="3"){
-                      this.fileIDs.push(file?.fileID);
-                    }
-                  })
+  getFile() {
+    if (this.dataIns != null) {
+      this.fileIDs = [];
+      if (this.dataIns?.documentControl?.length > 0) {
+        //let curStepDmc= this.dataIns?.documentControl;
+        let curStepDmc = this.dataIns?.documentControl.filter(
+          (x) => x?.stepID == this.data?.stepID
+        );
+        if (curStepDmc?.length > 0) {
+          curStepDmc?.forEach((dmc) => {
+            if (dmc?.files?.length > 0) {
+              dmc?.files?.forEach((file) => {
+                if (file?.type == '1' || file?.type == '3') {
+                  this.fileIDs.push(file?.fileID);
                 }
-
-                let curRefStepDmc= this.dataIns?.documentControl.filter(x=>x?.recID == dmc?.refID );
-                if(curRefStepDmc?.length>0){
-                  curRefStepDmc?.forEach(refDmc=>{
-                    if(refDmc?.files?.length>0){
-                      refDmc?.files?.forEach(refFile=>{
-                        if(refFile?.type=="1" || refFile?.type=="3"){
-                          this.fileIDs.push(refFile?.fileID);
-                        }
-                      })
-                    }
-                  })
-                }
-                // let curRefStepDmc= this.dataIns?.documentControl.filter(x=>x?.stepID == dmc.refStepID );
-                // if(curRefStepDmc?.length>0){
-                //   curRefStepDmc?.forEach(refDmc=>{
-                //     if(refDmc?.files?.length>0){
-                //       refDmc?.files?.forEach(refFile=>{
-                //         if(refFile?.type=="1" || refFile?.type=="3"){
-                //           this.fileIDs.push(refFile?.fileID);
-                //         }
-                //       })
-                //     }
-                //   })
-                // }
-              })
-            }
-            if(this.fileIDs?.length>0){
-              this.files=[];
-              this.shareService
-                .getLstFileByID(this.fileIDs)
-                .subscribe((res) => {
-                  if (res) {
-                    this.files = res;
-                    this.detectorRef.detectChanges();
-                  }
-                });
+              });
             }
 
-          }
-
+            let curRefStepDmc = this.dataIns?.documentControl.filter(
+              (x) => x?.recID == dmc?.refID
+            );
+            if (curRefStepDmc?.length > 0) {
+              curRefStepDmc?.forEach((refDmc) => {
+                if (refDmc?.files?.length > 0) {
+                  refDmc?.files?.forEach((refFile) => {
+                    if (refFile?.type == '1' || refFile?.type == '3') {
+                      this.fileIDs.push(refFile?.fileID);
+                    }
+                  });
+                }
+              });
+            }
+            // let curRefStepDmc= this.dataIns?.documentControl.filter(x=>x?.stepID == dmc.refStepID );
+            // if(curRefStepDmc?.length>0){
+            //   curRefStepDmc?.forEach(refDmc=>{
+            //     if(refDmc?.files?.length>0){
+            //       refDmc?.files?.forEach(refFile=>{
+            //         if(refFile?.type=="1" || refFile?.type=="3"){
+            //           this.fileIDs.push(refFile?.fileID);
+            //         }
+            //       })
+            //     }
+            //   })
+            // }
+          });
+        }
+        if (this.fileIDs?.length > 0) {
+          this.files = [];
+          this.shareService.getLstFileByID(this.fileIDs).subscribe((res) => {
+            if (res) {
+              this.files = res;
+              this.detectorRef.detectChanges();
+            }
+          });
         }
       }
+    }
+  }
   getVll() {
     this.cache.valueList('BP001').subscribe((vll) => {
       if (vll && vll?.datas?.length > 0) {
@@ -182,7 +185,7 @@ export class PopupBpTasksComponent implements OnInit {
     } else this.info = info;
   }
 
-  onSave(status = "5") {
+  onSave(status = '5') {
     if (this.checkList?.length > 0) {
       for (let i = 0; i < this.checkList.length; i++) {
         const item = this.checkList[i];
@@ -209,6 +212,32 @@ export class PopupBpTasksComponent implements OnInit {
       });
   }
 
+  sendMail() {
+    let data = {
+      dialog: this.dialog,
+      formGroup: null,
+      templateID: '',
+      showIsTemplate: true,
+      showIsPublish: true,
+      showSendLater: true,
+      files: null,
+      isAddNew: false,
+      notSendMail: true,
+    };
+    let opt = new DialogModel();
+    opt.zIndex = 20000;
+    let popEmail = this.callfc.openForm(
+      CodxEmailComponent,
+      '',
+      800,
+      screen.height,
+      '',
+      data,
+      '',
+      opt
+    );
+  }
+
   valueChange(e) {}
 
   //#region ActivityType = 'Task'
@@ -227,8 +256,11 @@ export class PopupBpTasksComponent implements OnInit {
   }
   valueChangeCb(e, i) {
     this.checkList[i]['status'] = e?.target?.checked ? '1' : '0';
+    this.countCheck = this.checkList.filter((x) => x.status == '1').length;
     this.detectorRef.detectChanges();
   }
+
+  changeRadio(e) {}
   //#endregion
 
   addFile(evt: any) {
@@ -245,44 +277,40 @@ export class PopupBpTasksComponent implements OnInit {
     this.dialog.close(this.dataIns);
   }
 
-
-  return(){
-    this.onSave("2")
+  return() {
+    this.onSave('2');
   }
-  eSign(){
-    if(this.data?.recID){
-          // gọi hàm xử lý xem trình ký
-          let dialogModel = new DialogModel();
-          dialogModel.IsFull = true;
-          var listApproveMF = this.shareService.getMoreFunction(null,"S1");
+  eSign() {
+    if (this.data?.recID) {
+      // gọi hàm xử lý xem trình ký
+      let dialogModel = new DialogModel();
+      dialogModel.IsFull = true;
+      var listApproveMF = this.shareService.getMoreFunction(null, 'S1');
 
-          let dialogApprove = this.callfc.openForm(
-            PopupSignForApprovalComponent,
-            'Thêm mới',
-            700,
-            650,
-            'EST021',
-            {
-              funcID: 'EST021',
-              sfRecID: this.data?.recID,
-              title: this.data?.taskName,
-              status: "3",
-              stepType: "S1",
-              stepNo: "0",
-              modeView:"2",
-              lstMF: listApproveMF,
-            },
-            '',
-            dialogModel
-          );
-          dialogApprove.closed.subscribe((res) => {
-            if (res?.event?.msgCodeError == null && res?.event?.rowCount > 0) {
-              this.onSave();
-            }
-          });
-
-
-
+      let dialogApprove = this.callfc.openForm(
+        PopupSignForApprovalComponent,
+        'Thêm mới',
+        700,
+        650,
+        'EST021',
+        {
+          funcID: 'EST021',
+          sfRecID: this.data?.recID,
+          title: this.data?.taskName,
+          status: '3',
+          stepType: 'S1',
+          stepNo: '0',
+          modeView: '2',
+          lstMF: listApproveMF,
+        },
+        '',
+        dialogModel
+      );
+      dialogApprove.closed.subscribe((res) => {
+        if (res?.event?.msgCodeError == null && res?.event?.rowCount > 0) {
+          this.onSave();
+        }
+      });
     }
   }
 }
