d104dbf41c6e82b30d87398522aa115f59d7790e
LocTran <loctt.cntp@gamil.com>
2024-03-28 18:13:30 +0700

    kế hoạch công tác

---
+++ projects/codx-ep/src/lib/requests/requests-view-detai/requests-view-detai.component.ts
@@ -0,0 +1,50 @@
+import { AfterViewInit, Component, Injector, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { change } from '@syncfusion/ej2-angular-grids';
+import { FormModel, UIDetailComponent } from 'codx-core';
+
+@Component({
+  selector: 'ep-requests-view-detai',
+  templateUrl: './requests-view-detai.component.html',
+  styleUrls: ['./requests-view-detai.component.css']
+})
+export class RequestsViewDetaiComponent extends UIDetailComponent implements OnChanges, AfterViewInit {
+
+
+
+  @Input() formModel:FormModel;
+  data:any;
+  constructor
+  (
+    injector:Injector
+  ) 
+  {
+    super(injector);
+  }
+
+  override onInit(): void {
+    this.loadDataInfo(this.recID,this.funcID);
+  }
+
+  ngAfterViewInit(): void {
+
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes && !changes.recID.firstChange && changes.recID && changes.recID.currentValue !== changes.recID.previousValue)
+    {
+      this.loadDataInfo(this.recID,this.funcID);
+    }
+  }
+
+  loadDataInfo(recID:string,funcID:string){
+    this.api.execSv("EP","EP","RequestBusiness","GetRequestDetailAsync",[recID,funcID])
+    .subscribe((res:any) => {
+      this.data = res;
+      this.detectorRef.detectChanges();
+    })
+  }
+
+  changeDataMF(event:any){
+
+  }
+
+}
